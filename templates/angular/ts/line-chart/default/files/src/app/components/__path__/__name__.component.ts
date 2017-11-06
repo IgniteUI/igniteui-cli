a1ae866b8463dc62333ea8894f6e81fb070beb7d
IvayloG <inj.iganchev@gmail.com>
2017-11-06 12:38:37 +0200

    Adding Combo with more series types.

--- templates/angular/ts/line-chart/default/files/src/app/components/__path__/__name__.component.ts
+++ templates/angular/ts/line-chart/default/files/src/app/components/__path__/__name__.component.ts
@@ -5,10 +5,22 @@ import { data } from "../../assets/line-chart-data";
 @Component({
 	selector: "app-line-chart",
 	template: `
-		<div style="float: left">
-			<ig-data-chart [(options)]="options" widgetId='lineChart'></ig-data-chart>
-		</div>
-		<div id="lineLegend" style="float: left"></div>`
+	<br>
+	<div class="selectionOptions" (change)="selectionChanged($event)" >
+		Series type:
+		<select id="seriesType">
+			<option value="line" selected="selected">Line</option>
+			<option value="stepLine">Step Line</option>
+			<option value="spline">Spline</option>
+			<option value="splineArea">Spline Area</option>
+		</select>
+		<br>
+		<br>
+	</div>
+	<div style="float: left">
+		<ig-data-chart [(options)]="options" widgetId='lineChart'></ig-data-chart>
+	</div>
+	<div id="lineLegend" style="float: left"></div>`
 })
 export class $(ClassName)Component {
 	public options: IgDataChart;
@@ -59,5 +71,40 @@ export class $(ClassName)Component {
 					thickness: 5
 				}]
 		};
+	}
+
+	updateChart(curSelection){
+		//Series require the whole object 
+		this.options = {
+			series: [ 
+				{
+					name: "2005Population",
+					type: curSelection,
+					title: "2005",
+					xAxis: "NameAxis",
+					yAxis: "PopulationAxis",
+					valueMemberPath: "Pop2005",
+					isTransitionInEnabled: true,
+					isHighlightingEnabled: true,
+					thickness: 5
+				},
+				{
+					name: "1995Population",
+					type: curSelection,
+					title: "1995",
+					xAxis: "NameAxis",
+					yAxis: "PopulationAxis",
+					valueMemberPath: "Pop1995",
+					isTransitionInEnabled: true,
+					isHighlightingEnabled: true,
+					thickness: 5
+				}
+			]
+		};
+	}
+	
+	selectionChanged (event: Event){
+		//pass currently selected Option from the Select element
+		this.updateChart((<HTMLSelectElement>event.target).value);
 	}
 }
