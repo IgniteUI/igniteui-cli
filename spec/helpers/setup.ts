4d50464b8ae1ba381eee4f889968e6657083ec12
Damyan Petev <damyan.petev@gmail.com>
2017-11-15 18:36:38 +0200

    test: Add a colorful console spec reporter

---
+++ spec/helpers/setup.ts
@@ -0,0 +1,13 @@
+import { DisplayProcessor, SpecReporter } from "jasmine-spec-reporter";
+import SuiteInfo = jasmine.SuiteInfo;
+
+class CustomProcessor extends DisplayProcessor {
+	public displayJasmineStarted(info: SuiteInfo, log: string): string {
+		return `TypeScript ${log}`;
+	}
+}
+
+jasmine.getEnv().clearReporters();
+jasmine.getEnv().addReporter(new SpecReporter({
+	customProcessors: [CustomProcessor]
+}));
