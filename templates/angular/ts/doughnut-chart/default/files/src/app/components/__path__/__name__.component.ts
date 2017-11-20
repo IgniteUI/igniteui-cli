bf63441e5f42d2ad165f309b5676ea1918a50242
AMarinov <amarinov@infragistics.com>
2017-11-20 15:10:33 +0200

    Adding igDoughnutChart template for Angular

---
+++ templates/angular/ts/doughnut-chart/default/files/src/app/components/__path__/__name__.component.ts
@@ -0,0 +1,37 @@
+import { Component } from "@angular/core";
+import { doughnutData } from "../../assets/doughnut-data";
+
+@Component({
+	selector: "app-doughnut-chart",
+	template: `
+	<div style="float: left">
+	<ig-doughnut-chart [(options)]="doughnutOptions" widgetId='doughnutChart'></ig-doughnut-chart>
+	</div>
+	`
+})
+export class $(ClassName)Component {
+	public doughnutOptions: IgDoughnutChart;
+	public data: any;
+
+	constructor() {
+		this.data = doughnutData;
+		this.doughnutOptions = {
+			width: "550px",
+			height: "550px",
+			series:
+			[{
+				dataSource: this.data,
+				responseDataKey: "data",
+				name: "Pop1990",
+				labelMemberPath: "CountryName",
+				valueMemberPath: "Pop1990",
+				labelsPosition: "bestFit",
+				formatLabel: function (context) {
+					return context.itemLabel + " (" + context.item.Pop1990 + ")";
+				},
+				showTooltip: true,
+				brushes: ["#B284BE", "#5D8AA8", "#C9FFE5", "#7CB9E8", "#F19CBB"]
+			}]	
+		};
+	}
+}
