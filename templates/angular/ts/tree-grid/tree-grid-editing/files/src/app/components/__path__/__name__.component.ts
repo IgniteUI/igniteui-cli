29b3786298c57aa9b78dc4903cfb01b872650d18
AMarinov <amarinov@infragistics.com>
2017-11-30 15:32:40 +0200

    fix: Fixing issue 94 "The output data file is overridden when adding more than a single igTreeGrid in Angular projects"

--- templates/angular/ts/tree-grid/tree-grid-editing/files/src/app/components/__path__/__name__.component.ts
+++ templates/angular/ts/tree-grid/tree-grid-editing/files/src/app/components/__path__/__name__.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { hierarchicalDS } from "../../assets/data";
+import { hierarchicalDS } from "../../assets/data-editing";
 
 @Component({
 	selector: "app-tree-grid",
