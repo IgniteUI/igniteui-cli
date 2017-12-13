a66fe2af8042929467616773d843b019a541c172
IvayloG <inj.iganchev@gmail.com>
2017-12-13 17:05:03 +0200

    chore: revert tslint comment.

--- lib/PromptSession.ts
+++ lib/PromptSession.ts
@@ -21,6 +21,7 @@ export class PromptSession {
 
 		add.templateManager = this.templateManager;
 
+		// tslint:disable:object-literal-sort-keys
 		if (config != null && !config.project.isShowcase) {
 			projLibrary = this.templateManager.getProjectLibrary(config.project.framework, config.project.projectType);
 			await this.chooseActionLoop(projLibrary, config.project.theme);
