36927ca6d139cfb528ea37dacd280cd5848b97c4
IvayloG <inj.iganchev@gmail.com>
2018-01-05 14:35:36 +0200

    feat: Add grid-editing template.

---
+++ templates/angular/ig-ts/grid/grid-editing/files/src/app/components/__path__/__name__.component.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+import { products } from "../../assets/northwindProducts";
+
+@Component({
+	selector: "app-grid",
+	template: `<ig-grid [(options)]="gridOptions" widgetId='grid-editing'></ig-grid>`
+})
+export class $(ClassName)Component {
+	public gridOptions: IgGrid;
+	public products: any[];
+
+	constructor() {
+		this.products = products;
+		this.gridOptions = {
+			columns: [
+				{ headerText: "Product ID", key: "ProductID", dataType: "number" },
+				{ headerText: "Product Name", key: "Name", dataType: "string" },
+				{ headerText: "Product Number", key: "ProductNumber", dataType: "string" }
+			],
+			width: "500px",
+			dataSource: this.products,
+			autoCommit : true,
+			primaryKey: "ProductID",
+			features: $(gridFeatures)
+		};
+	}
+}
---
+++ templates/angular/ig-ts/grid/grid-editing/index.ts
@@ -0,0 +1,57 @@
+import * as fs from "fs-extra";
+import * as path from "path";
+import { GridHelper } from "../../../../../lib/project-utility/GridHelper";
+import { AngularTemplate } from "../../../../../lib/templates/AngularTemplate";
+import { Util } from "../../../../../lib/Util";
+
+class GridEditingTemplate extends AngularTemplate {
+	private gridHelper: GridHelper;
+	private extraConfigurations: ControlExtraConfiguration[];
+	private userExtraConfiguration: {} = {};
+
+	constructor() {
+		super(__dirname);
+		this.id = "grid-editing";
+		this.name = "Grid Editing";
+		this.controlGroup = "Data Grids";
+		this.description = "This is a grid editing template for Angular";
+		this.dependencies = ["igGrid"];
+		this.projectType = "ig-ts";
+		this.extraConfigurations = [];
+		this.hasExtraConfiguration = true;
+		this.listInComponentTemplates = true;
+
+		this.gridHelper = new GridHelper();
+		const featureConfiguration: ControlExtraConfiguration = {
+			choices: ["Sorting", "Filtering"],
+			default: "",
+			key: "features",
+			message: "Select features for the igGrid",
+			type: Enumerations.ControlExtraConfigType.MultiChoice
+		};
+		this.extraConfigurations.push(featureConfiguration);
+	}
+
+	public setExtraConfiguration(extraConfigKeys: {}) {
+		this.userExtraConfiguration = extraConfigKeys;
+	}
+
+	public generateFiles(projectPath: string, name: string, ...options: any[]): Promise<boolean> {
+		const config = {
+			"$(ClassName)": this.className(name),
+			"__name__": this.fileName(name),
+			"__path__": this.folderName(name)
+		};
+		const features = this.gridHelper.generateFeatures(["Updating"].concat(this.userExtraConfiguration["features"]), 3);
+
+		config["$(gridFeatures)"] = features;
+		config["$(componentName)"] = name;
+		const pathsConfig = {};
+		return Util.processTemplates(path.join(__dirname, "files"), projectPath, config, pathsConfig);
+	}
+	public getExtraConfiguration(): ControlExtraConfiguration[] {
+		return this.extraConfigurations;
+	}
+}
+
+module.exports = new GridEditingTemplate();
