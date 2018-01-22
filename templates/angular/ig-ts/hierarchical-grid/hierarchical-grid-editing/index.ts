7473cd8bfe5db2a757cccde54b935d5c1bbde6f8
IvayloG <inj.iganchev@gmail.com>
2018-01-22 14:00:12 +0200

    chore: Synch validateTemplate;

--- templates/angular/ig-ts/hierarchical-grid/hierarchical-grid-editing/index.ts
+++ templates/angular/ig-ts/hierarchical-grid/hierarchical-grid-editing/index.ts
@@ -47,6 +47,10 @@ class HierarchicalGridEditingTemplate extends AngularTemplate {
 		config["$(gridFeatures)"] = features;
 		config["$(componentName)"] = name;
 		const pathsConfig = {};
+		// TODO: Refactor to base
+		if (!Util.validateTemplate(path.join(__dirname, "files"), projectPath, config, pathsConfig)) {
+			return Promise.resolve(false);
+		}
 		return Util.processTemplates(path.join(__dirname, "files"), projectPath, config, pathsConfig);
 	}
 	public getExtraConfiguration(): ControlExtraConfiguration[] {
