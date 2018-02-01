95f55b6867e5e5a0783ba9b58797265e983db6cd
Damyan Petev <damyan.petev@gmail.com>
2018-02-01 10:09:43 +0200

    feat(global-config): intitial config methods and defaults

--- lib/ProjectConfig.ts
+++ lib/ProjectConfig.ts
@@ -1,26 +1,54 @@
 import * as fs from "fs-extra";
+import * as os from "os";
 import * as path from "path";
 
 export class ProjectConfig {
 
 	public static configFile: string = "ignite-ui-cli.json";
 
-	public static getConfig(): Config {
+	/** TODO: Use this to check for existing local project instead of `getConfig` */
+	public static localConfigFile(): boolean {
 		const filePath = path.join(process.cwd(), this.configFile);
-		if (fs.existsSync(filePath)) {
+		return fs.existsSync(filePath);
+	}
+
+	public static getConfig(global: boolean = false): Config {
+		const filePath = path.join(process.cwd(), this.configFile);
+		let config = this.globalDefaults();
+
+		if (!global && fs.existsSync(filePath)) {
 			try {
-				return JSON.parse(fs.readFileSync(filePath, "utf8")) as Config;
+				const localConfig = JSON.parse(fs.readFileSync(filePath, "utf8")) as Config;
+				config = Object.assign(config, localConfig);
 			} catch (error) {
 				throw new Error(`The ${this.configFile} file is not parsed correctly. ` +
 					`The following error has occurred: ${error.message}`);
 			}
 		}
-		return null;
+		return config;
 	}
-	public static setConfig(config: Config) {
-		const filePath = path.join(process.cwd(), this.configFile);
-		if (fs.existsSync(filePath)) {
-			fs.writeJsonSync(filePath, config, { spaces: 4 });
+	public static setConfig(config: Config, global: boolean = false) {
+		const basePath = global ? os.homedir() : process.cwd();
+		const filePath = path.join(basePath, this.configFile);
+		fs.writeJsonSync(filePath, config, { spaces: 4 });
+	}
+
+	public static globalConfig(): Config {
+		const globalConfigPath = path.join(os.homedir(), this.configFile);
+		let globalConfig = {};
+
+		if (fs.existsSync(globalConfigPath)) {
+			globalConfig = require(globalConfigPath);
 		}
+		return globalConfig as Config;
+	}
+
+	private static globalDefaults(): Config {
+		let defaults: Config = require("./config/defaults.json");
+		const globalConfig = this.globalConfig();
+
+		// TODO: `Object.assign` doesn't do deep extend, nested object properties override!
+		defaults = Object.assign(defaults, globalConfig);
+		return defaults;
 	}
 }
