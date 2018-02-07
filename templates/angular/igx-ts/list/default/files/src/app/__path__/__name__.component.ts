1be1fa009496de4df83def09401102f35cbeed28
wnvko <milko.venkov@gmail.com>
2018-02-07 13:31:19 +0200

    feat: Adding angular list template       List template is ready with filtering and toggleable favorite star

--- templates/angular/igx-ts/list/default/files/src/app/__path__/__name__.component.ts
+++ templates/angular/igx-ts/list/default/files/src/app/__path__/__name__.component.ts
@@ -1,16 +1,5 @@
-import { Component, ElementRef, ViewChild } from "@angular/core";
-import {
-	IgxDialog,
-	IgxDialogModule,
-	IgxFilterModule,
-	IgxFilterOptions,
-	IgxInput,
-	IgxList,
-	IgxListItem,
-	IgxListModule,
-	IgxListPanState,
-	IgxRippleModule
-} from 'igniteui-angular/main';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { IgxFilterOptions, IgxListItem } from 'igniteui-angular/main';
 
 @Component({
 	selector: 'app-home',
@@ -20,77 +9,53 @@ import {
 })
 export class $(ClassName)Component implements OnInit {
 	title = '$(name)';
-	public navItems: object[] = [
+	public searchContact: string;
+	public contacts = [
 		{
-			avatar: "assets/1.jpg",
-			favorite: true,
-			key: "1",
-			link: "#",
+			isFavorite: false,
+			name: "Terrance Orta",
 			phone: "770-504-2217",
-			text: "Terrance Orta"
-		}, {
-			avatar: "assets/2.jpg",
-			favorite: false,
-			key: "2",
-			link: "#",
+			photo: "https://randomuser.me/api/portraits/men/27.jpg"
+		},
+		{
+			isFavorite: true,
+			name: "Richard Mahoney",
 			phone: "423-676-2869",
-			text: "Richard Mahoney"
-		}, {
-			avatar: "assets/3.jpg",
-			favorite: false,
-			key: "3",
-			link: "#",
+			photo: "https://randomuser.me/api/portraits/men/1.jpg"
+		},
+		{
+			isFavorite: false,
+			name: "Donna Price",
 			phone: "859-496-2817",
-			text: "Donna Price"
-		}, {
-			avatar: "assets/4.jpg",
-			favorite: false,
-			key: "4",
-			link: "#",
+			photo: "https://randomuser.me/api/portraits/women/50.jpg"
+		},
+		{
+			isFavorite: false,
+			name: "Lisa Landers",
 			phone: "901-747-3428",
-			text: "Lisa Landers"
-		}, {
-			avatar: "assets/12.jpg",
-			favorite: true,
-			key: "5",
-			link: "#",
+			photo: "https://randomuser.me/api/portraits/women/3.jpg"
+		},
+		{
+			isFavorite: true,
+			name: "Dorothy H. Spencer",
 			phone: "573-394-9254",
-			text: "Dorothy H. Spencer"
-		}, {
-			avatar: "assets/13.jpg",
-			favorite: false,
-			key: "6",
-			link: "#",
-			phone: "323-668-1482",
-			text: "Stephanie May"
-		}, {
-			avatar: "assets/14.jpg",
-			favorite: false,
-			key: "7",
-			link: "#",
-			phone: "401-661-3742",
-			text: "Marianne Taylor"
-		}, {
-			avatar: "assets/15.jpg",
-			favorite: true,
-			key: "8",
-			link: "#",
-			phone: "662-374-2920",
-			text: "Tammie Alvarez"
-		}, {
-			avatar: "assets/16.jpg",
-			favorite: true,
-			key: "9",
-			link: "#",
-			phone: "240-455-2267",
-			text: "Charlotte Flores"
-		}, {
-			avatar: "assets/17.jpg",
-			favorite: false,
-			key: "10",
-			link: "#",
-			phone: "724-742-0979",
-			text: "Ward Riley"
+			photo: "https://randomuser.me/api/portraits/women/67.jpg"
 		}
 	];
+
+	constructor() { }
+
+	ngOnInit() { }
+
+	public toggleFavorite(item: IgxListItem) {
+		const contact = this.contacts[item.index - 1];
+		contact.isFavorite = !contact.isFavorite;
+	}
+
+	get filterContacts() {
+		const fo = new IgxFilterOptions();
+		fo.key = "name";
+		fo.inputValue = this.searchContact;
+		return fo;
+	}
 }
--- templates/angular/igx-ts/projects/empty/files/src/app/app.module.ts
+++ templates/angular/igx-ts/projects/empty/files/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+	FormsModule,
     AppRoutingModule,
     IgxButtonModule
   ],
