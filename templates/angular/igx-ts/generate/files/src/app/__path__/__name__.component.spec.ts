75fe4ef9dc07697a916085cdc54b2e045ae19378
wnvko <milko.venkov@gmail.com>
2018-02-19 13:22:46 +0200

    feat: Add generate command Adding some files for angular

---
+++ templates/angular/igx-ts/generate/files/src/app/__path__/__name__.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { $(ClassName)Component } from './$(name).component';
+
+describe('$(ClassName)Component', () => {
+  let component: $(ClassName)Component;
+  let fixture: ComponentFixture<$(ClassName)Component>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ $(ClassName)Component ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent($(ClassName)Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ templates/angular/igx-ts/generate/files/src/app/__path__/__name__.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: "$(ClassName)",
+	templateUrl: './$(ClassName).component.html',
+	styleUrls: ['./$(ClassName).component.css'],
+})
+export class $(ClassName)Component implements {
+	title = '$(name)';
+	constructor() { }
+}
