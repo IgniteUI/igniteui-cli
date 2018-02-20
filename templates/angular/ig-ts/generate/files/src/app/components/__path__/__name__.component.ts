d9d4fca2f74778a950bf49857de474bf1c58423e
wnvko <milko.venkov@gmail.com>
2018-02-20 10:58:25 +0200

    feat: Add generate command Angular wrapper add some more files

---
+++ templates/angular/ig-ts/generate/files/src/app/components/__path__/__name__.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "$(filePrefix)",
+	template: "<h1>$(description)</h1>"
+})
+export class $(ClassName)Component {
+
+	constructor() {}
+}
