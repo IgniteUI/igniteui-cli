8f26621a3234684cb4031c242a52395de5e963ce
AMarinov <amarinov@infragistics.com>
2018-02-21 16:53:07 +0200

    Adding more unit tests

--- spec/unit/BaseProjectLibrary-spec.ts
+++ spec/unit/BaseProjectLibrary-spec.ts
@@ -101,7 +101,7 @@ describe("Unit - Base project library ", () => {
 		done();
 	});
 
-	fit("gets template by id.", async done => {
+	it("gets template by id and name.", async done => {
 		spyOn(Util, "getDirectoryNames").and.returnValues(["grid", "chart"], ["awesome"]);
 
 		// spy on require(), https://coderwall.com/p/ck7w6g/spying-on-require-with-jasmine
@@ -131,10 +131,101 @@ describe("Unit - Base project library ", () => {
 		});
 
 		const library = new BaseProjectLibrary(__dirname);
+
+		//get templates by id
 		expect(library.getTemplateById("gridTemplate")).toBeTruthy();
 		expect(library.getTemplateById("gridtemplate")).toBeFalsy();
 		expect(library.getTemplateById("awesome")).toBeTruthy();
 		expect(library.getTemplateById("chartTemplate")).toBe(library.templates[1]);
+
+		//get templates by name
+		expect(library.getTemplateByName("gridTemplateName")).toBeTruthy();
+		expect(library.getTemplateByName("gridtemplatename")).toBeFalsy();
+		expect(library.getTemplateByName("awesomeCustomName")).toBeTruthy();
+		expect(library.getTemplateByName("chartTemplateName")).toBe(library.templates[1]);
+
+		done();
+	});
+
+	it("registers a template successfully.", async done => {
+		spyOn(Util, "getDirectoryNames").and.returnValues(["grid", "chart"], ["awesome"]);
+
+		// spy on require(), https://coderwall.com/p/ck7w6g/spying-on-require-with-jasmine
+		spyOn(require("module"), "_load").and.callFake((modulePath: string) => {
+			if (modulePath.startsWith(path.join(__dirname, "custom-templates"))) {
+				const folder = path.basename(modulePath);
+				// tslint:disable-next-line:no-object-literal-type-assertion
+				return {
+					id: folder,
+					name: folder + "CustomName",
+					group: folder + "CustomGroup"
+				};
+			}
+			if (modulePath.startsWith(__dirname)) {
+				const folder = path.basename(modulePath);
+				// tslint:disable-next-line:no-object-literal-type-assertion
+				return {
+					templates: {
+						id: folder + "Template",
+						name: folder + "TemplateName",
+						group: folder + "TemplateGroup",
+						components: { name: folder + "ComponentName"}
+					}
+				};
+			} else {
+				fail("unexpected require");
+			}
+		});
+
+		const library = new BaseProjectLibrary(__dirname);
+
+		const mockTemplate = library.templates[0];
+		mockTemplate.components = ["newComponent"];
+		mockTemplate.name = "newName";
+		library.registerTemplate(mockTemplate);
+
+		expect(library.getTemplateById("gridTemplate")).toBeTruthy();
+		expect(library.getTemplateByName("newName")).toBeTruthy();
+		expect(library.getComponentByName("newComponent")).toBeTruthy();
+		expect(library.templates.length).toEqual(3);
+		done();
+	});
+
+	it("gets [custom] component by name.", async done => {
+		spyOn(Util, "getDirectoryNames").and.returnValues(["grid", "chart"], ["awesome"]);
+
+		// spy on require(), https://coderwall.com/p/ck7w6g/spying-on-require-with-jasmine
+		spyOn(require("module"), "_load").and.callFake((modulePath: string) => {
+			if (modulePath.startsWith(path.join(__dirname, "custom-templates"))) {
+				const folder = path.basename(modulePath);
+				// tslint:disable-next-line:no-object-literal-type-assertion
+				return {
+					id: folder,
+					name: folder + "CustomName",
+					group: folder + "CustomGroup"
+				};
+			}
+			if (modulePath.startsWith(__dirname)) {
+				const folder = path.basename(modulePath);
+				// tslint:disable-next-line:no-object-literal-type-assertion
+				return {
+						name: folder + "Name",
+						group: folder + "Group"
+				};
+			} else {
+				fail("unexpected require");
+			}
+		});
+
+		const library = new BaseProjectLibrary(__dirname);
+
+		//get components by name
+		expect(library.getComponentByName("chartName")).toBeTruthy();
+		expect(library.getComponentByName("chartname")).toBeFalsy();
+		expect(library.getComponentByName("awesomeCustomName")).toBeFalsy();
+		expect(library.getCustomTemplateByName("awesomeCustomName")).toBeTruthy();
+		expect(library.getComponentByName("gridName")).toBe(library.components[0]);
+
 		done();
 	});
 });
