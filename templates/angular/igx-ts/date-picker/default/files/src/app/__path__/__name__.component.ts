d0eb6dee1d123b717d6c061d64d29620b67935e5
Damyan Petev <damyan.petev@gmail.com>
2018-02-26 19:41:23 +0200

    refactor(igx-ts): move sass to scss and component-specific files

--- templates/angular/igx-ts/date-picker/default/files/src/app/__path__/__name__.component.ts
+++ templates/angular/igx-ts/date-picker/default/files/src/app/__path__/__name__.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 @Component({
 	selector: "$(filePrefix)",
 	templateUrl: './$(filePrefix).component.html',
+	styleUrls: ['./$(filePrefix).component.css'],
 	encapsulation: ViewEncapsulation.None,
 })
 export class $(ClassName)Component implements OnInit {
