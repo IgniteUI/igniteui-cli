a02bb6eb2af6f54abef5108331c5e2f1fef8c50f
Damyan Petev <damyan.petev@gmail.com>
2018-02-27 16:36:58 +0200

    chore(igx-carousel): selector per tslint, update test imports, formatting

--- templates/angular/igx-ts/carousel/default/files/src/app/__path__/__name__.component.spec.ts
+++ templates/angular/igx-ts/carousel/default/files/src/app/__path__/__name__.component.spec.ts
@@ -1,27 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { $(ClassName)Component } from './$(filePrefix).component';
-import { IgxCarousel, IgxCarouselModule, IgxSlide } from 'igniteui-angular/main';
+import { IgxCarouselModule } from 'igniteui-angular/main';
 
 describe('$(ClassName)Component', () => {
-	let component: $(ClassName)Component;
-	let fixture: ComponentFixture<$(ClassName)Component>;
+  let component: $(ClassName)Component;
+  let fixture: ComponentFixture<$(ClassName)Component>;
 
-	beforeEach(async(() => {
-		TestBed.configureTestingModule({
-			declarations: [$(ClassName)Component],
-			imports: [IgxCarouselModule]
-		})
-			.compileComponents();
-	}));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [$(ClassName)Component],
+      imports: [IgxCarouselModule]
+    })
+      .compileComponents();
+  }));
 
-	beforeEach(() => {
-		fixture = TestBed.createComponent($(ClassName)Component);
-		component = fixture.componentInstance;
-		fixture.detectChanges();
-	});
+  beforeEach(() => {
+    fixture = TestBed.createComponent($(ClassName)Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-	it("should create", () => {
-		expect(component).toBeTruthy();
-	});
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
