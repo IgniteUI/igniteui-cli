7d1a188aa79ac4135e337eef2514533dd054f898
wnvko <milko.venkov@gmail.com>
2018-02-27 09:48:05 +0200

    test: Unit test for generate command - 100% coverage now.

--- spec/unit/generate-spec.ts
+++ spec/unit/generate-spec.ts
@@ -1,7 +1,8 @@
 import * as fs from "fs";
 import * as os from "os";
 import * as path from "path";
-import cli = require("../../lib/cli");
+import { default as config } from "../../lib/commands/config";
+import { default as generateCmd } from "../../lib/commands/generate";
 import { ProjectConfig } from "../../lib/ProjectConfig";
 import { Util } from "../../lib/Util";
 import { deleteAll } from "../helpers/utils";
@@ -20,6 +21,9 @@ describe("Unit - Generate command", () => {
 		// ~/.global/ homedir for global files:
 		fs.mkdirSync(`.global`);
 		spyOn(os, "homedir").and.returnValue(path.join(process.cwd(), ".global"));
+
+		spyOn(Util, "error");
+		spyOn(Util, "log");
 	});
 
 	afterEach(() => {
@@ -28,21 +32,38 @@ describe("Unit - Generate command", () => {
 		fs.rmdirSync(`./output/${testFolder}`);
 	});
 
-	it("Should generate template with default values", async done => {
-		spyOn(Util, "error");
-		spyOn(Util, "log");
+	it("Should generate template pass with correct values", async done => {
+		spyOn(Util, "isAlphanumericExt").and.returnValue(true);
+		spyOn(Util, "directoryExists").and.returnValue(false);
 		spyOn(Util, "processTemplates").and.returnValue(new Promise<boolean>((res, rej) => { res(true); }));
-		spyOn(ProjectConfig, "globalConfig").and.returnValue({});
 
-		await cli.run(["generate", "template"]);
+		const projectLib = {
+			generateTemplateFolderPath: "somePath"
+		};
+		generateCmd.templateManager = jasmine.createSpyObj("TemplateManager", {
+			getFrameworkById: {},
+			getProjectLibrary: projectLib
+		});
+
+		spyOn(config, "addHandler").and.stub();
+
+		await generateCmd.template({ name: "custom-template", framework: "jquery", type: "js" });
 
 		expect(Util.error).toHaveBeenCalledTimes(0);
-		expect(Util.log).toHaveBeenCalledTimes(2);
-		expect(Util.log).toHaveBeenCalledWith("Property \"customTemplates\" updated.");
+
+		expect(Util.isAlphanumericExt).toHaveBeenCalledTimes(1);
+		expect(Util.isAlphanumericExt).toHaveBeenCalledWith("custom-template");
+
+		const outDir = path.join(process.cwd(), "custom-template");
+		expect(Util.directoryExists).toHaveBeenCalledTimes(1);
+		expect(Util.directoryExists).toHaveBeenCalledWith(outDir);
+
+		expect(Util.log).toHaveBeenCalledTimes(1);
 		expect(Util.log).toHaveBeenCalledWith("Template generated successfully");
+
 		expect(Util.processTemplates).toHaveBeenCalledWith(
-			jasmine.any(String),
-			jasmine.any(String),
+			projectLib.generateTemplateFolderPath,
+			outDir,
 			{
 				"$(templateFramework)": "jquery",
 				"$(templateName)": "custom-template",
@@ -50,30 +71,124 @@ describe("Unit - Generate command", () => {
 			},
 			null);
 
+		const addHandlerExpectedParameter = { property: "customTemplates", value: "path:" + outDir, global: true };
+		expect(config.addHandler).toHaveBeenCalledTimes(1);
+		expect(config.addHandler).toHaveBeenCalledWith(addHandlerExpectedParameter);
+
 		done();
 	});
 
-	it("Should generate template with custom values", async done => {
-		spyOn(Util, "error");
-		spyOn(Util, "log");
-		spyOn(Util, "processTemplates").and.returnValue(new Promise<boolean>((res, rej) => { res(true); }));
-		spyOn(ProjectConfig, "globalConfig").and.returnValue({});
+	it("Logs error for wrong name", async done => {
+		spyOn(Util, "isAlphanumericExt").and.returnValue(false);
 
-		await cli.run(["generate", "template", "some-name", "-f=angular", "-t=igx-ts"]);
+		await generateCmd.template({ name: "123wrongName", framework: "jquery", type: "js" });
+
+		expect(Util.error).toHaveBeenCalledTimes(1);
+		expect(Util.error).toHaveBeenCalledWith(
+			"Name '123wrongName' is not valid. Name should start with a letter and can also contain numbers, dashes and spaces.",
+			"red");
+
+		expect(Util.log).not.toHaveBeenCalled();
+
+		done();
+	});
+
+	it("Logs error for existing folder", async done => {
+		spyOn(Util, "isAlphanumericExt").and.returnValue(true);
+		spyOn(Util, "directoryExists").and.returnValue(true);
+
+		spyOn(config, "addHandler").and.stub();
+
+		await generateCmd.template({ name: "custom-template", framework: "jquery", type: "js" });
+
+		expect(Util.error).toHaveBeenCalledTimes(1);
+		expect(Util.error).toHaveBeenCalledWith("Folder 'custom-template' already exists!", "red");
+
+		expect(Util.log).not.toHaveBeenCalled();
+
+		done();
+	});
+
+	it("Logs error for wrong framework", async done => {
+		spyOn(Util, "isAlphanumericExt").and.returnValue(true);
+		spyOn(Util, "directoryExists").and.returnValue(false);
+
+		generateCmd.templateManager = jasmine.createSpyObj("TemplateManager", {
+			getFrameworkById: undefined
+		});
+
+		await generateCmd.template({ name: "custom-template", framework: "wrongFramework", type: "js" });
+
+		expect(Util.error).toHaveBeenCalledTimes(1);
+		expect(Util.error).toHaveBeenCalledWith("Framework not supported", "red");
+
+		expect(Util.log).not.toHaveBeenCalled();
+
+		done();
+	});
+
+	it("Logs error for wrong type", async done => {
+		spyOn(Util, "isAlphanumericExt").and.returnValue(true);
+		spyOn(Util, "directoryExists").and.returnValue(false);
+
+		generateCmd.templateManager = jasmine.createSpyObj("TemplateManager", {
+			getFrameworkById: {},
+			getProjectLibrary: undefined
+		});
+
+		await generateCmd.template({ name: "custom-template", framework: "jquery", type: "wrongType" });
+
+		expect(Util.error).toHaveBeenCalledTimes(1);
+		expect(Util.error).toHaveBeenCalledWith("Project type 'wrongType' not found in framework 'jquery'");
+
+		expect(Util.log).not.toHaveBeenCalled();
+
+		done();
+	});
+
+	it("Should log if generate template fail", async done => {
+		spyOn(Util, "isAlphanumericExt").and.returnValue(true);
+		spyOn(Util, "directoryExists").and.returnValue(false);
+		spyOn(Util, "processTemplates").and.returnValue(new Promise<boolean>((res, rej) => { res(false); }));
+
+		generateCmd.templateManager = jasmine.createSpyObj("TemplateManager", {
+			getFrameworkById: {},
+			getProjectLibrary: {}
+		});
+
+		await generateCmd.template({ name: "custom-template", framework: "jquery", type: "js" });
+
+		expect(Util.error).toHaveBeenCalledTimes(1);
+		expect(Util.error).toHaveBeenCalledWith("Template generation failed!", "red");
+
+		expect(Util.log).not.toHaveBeenCalled();
+
+		done();
+	});
+
+	it("Should not add path to global config if skip-config is true", async done => {
+		spyOn(Util, "isAlphanumericExt").and.returnValue(true);
+		spyOn(Util, "directoryExists").and.returnValue(false);
+		spyOn(Util, "processTemplates").and.returnValue(new Promise<boolean>((res, rej) => { res(true); }));
+
+		const projectLib = {
+			generateTemplateFolderPath: "somePath"
+		};
+		generateCmd.templateManager = jasmine.createSpyObj("TemplateManager", {
+			getFrameworkById: {},
+			getProjectLibrary: projectLib
+		});
+
+		spyOn(config, "addHandler").and.stub();
+
+		await generateCmd.template({ name: "custom-template", framework: "jquery", type: "js", skipConfig: true });
 
 		expect(Util.error).toHaveBeenCalledTimes(0);
-		expect(Util.log).toHaveBeenCalledTimes(2);
-		expect(Util.log).toHaveBeenCalledWith("Property \"customTemplates\" updated.");
+
+		expect(Util.log).toHaveBeenCalledTimes(1);
 		expect(Util.log).toHaveBeenCalledWith("Template generated successfully");
-		expect(Util.processTemplates).toHaveBeenCalledWith(
-			jasmine.any(String),
-			jasmine.any(String),
-			{
-				"$(templateFramework)": "angular",
-				"$(templateName)": "some-name",
-				"$(templateType)": "igx-ts"
-			},
-			null);
+
+		expect(config.addHandler).not.toHaveBeenCalled();
 
 		done();
 	});
