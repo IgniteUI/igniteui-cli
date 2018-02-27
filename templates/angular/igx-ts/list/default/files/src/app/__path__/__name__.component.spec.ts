985fe3deb01829de4e2fa70fc0f93f6b5b23d811
Damyan Petev <damyan.petev@gmail.com>
2018-02-27 16:31:18 +0200

    chore(igx-list): selector per tslint, update test imports, formatting

--- templates/angular/igx-ts/list/default/files/src/app/__path__/__name__.component.spec.ts
+++ templates/angular/igx-ts/list/default/files/src/app/__path__/__name__.component.spec.ts
@@ -1,27 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
 import { $(ClassName)Component } from './$(filePrefix).component';
-import { IgxComponentsModule } from 'igniteui-angular/main';
+import { IgxListModule, IgxAvatarModule, IgxIconModule, IgxFilterModule } from 'igniteui-angular/main';
 
 describe('$(ClassName)Component', () => {
-	let component: $(ClassName)Component;
-	let fixture: ComponentFixture<$(ClassName)Component>;
+  let component: $(ClassName)Component;
+  let fixture: ComponentFixture<$(ClassName)Component>;
 
-	beforeEach(async(() => {
-		TestBed.configureTestingModule({
-			declarations: [$(ClassName)Component],
-			imports: [IgxListModule]
-		})
-			.compileComponents();
-	}));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [$(ClassName)Component],
+      imports: [ FormsModule, IgxListModule, IgxAvatarModule, IgxIconModule, IgxFilterModule ]
+    })
+      .compileComponents();
+  }));
 
-	beforeEach(() => {
-		fixture = TestBed.createComponent($(ClassName)Component);
-		component = fixture.componentInstance;
-		fixture.detectChanges();
-	});
+  beforeEach(() => {
+    fixture = TestBed.createComponent($(ClassName)Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-	it('should create', () => {
-		expect(component).toBeTruthy();
-	});
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
