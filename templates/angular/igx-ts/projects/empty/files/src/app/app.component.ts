3118c3545b50284482ff4f76df656a467d1eae7e
Damyan Petev <damyan.petev@gmail.com>
2018-02-27 15:40:19 +0200

    chore(igx-ts): fix app shell test, lint

--- templates/angular/igx-ts/projects/empty/files/src/app/app.component.ts
+++ templates/angular/igx-ts/projects/empty/files/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { routes } from "./app-routing.module";
+import { routes } from './app-routing.module';
 
 
 @Component({
@@ -8,21 +8,21 @@ import { routes } from "./app-routing.module";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	public topNavLinks: Array<{
-		path: string,
-		name: string
-	}> = [];
-	/**
-	 *
-	 */
-	constructor() {
-		for (const route of routes) {
-			if (route.path && route.data && route.path.indexOf("*") === -1) {
-				this.topNavLinks.push({
-					name: route.data.text,
-					path: "/" + route.path
-				});
-			}
-		}
-	}
- }
+  public topNavLinks: Array<{
+    path: string,
+    name: string
+  }> = [];
+  /**
+   *
+   */
+  constructor() {
+    for (const route of routes) {
+      if (route.path && route.data && route.path.indexOf('*') === -1) {
+        this.topNavLinks.push({
+          name: route.data.text,
+          path: '/' + route.path
+        });
+      }
+    }
+  }
+}
