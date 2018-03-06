154030c5c19dfb52946cf0bb7cbaf3a2eeede1bb
IvayloG <inj.iganchev@gmail.com>
2018-03-06 14:19:14 +0200

    fix: Await for test command. Cleanup.

--- spec/unit/test-spec.ts
+++ spec/unit/test-spec.ts
@@ -1,12 +1,11 @@
 
-import * as path from "path";
 import { default as testCmd } from "../../lib/commands/test";
 import { Util } from "../../lib/Util";
 
 describe("Unit - Test command", () => {
 	it("Run tests for the current project", async done => {
 		spyOn(Util, "exec");
-		testCmd.execute({});
+		await testCmd.execute({});
 		expect (Util.exec).toHaveBeenCalledWith("npm test");
 
 		done();
