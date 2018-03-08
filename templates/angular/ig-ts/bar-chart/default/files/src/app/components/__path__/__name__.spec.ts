95209688f143f853572d9f3403f464d2530fb548
IvayloG <inj.iganchev@gmail.com>
2018-03-08 18:54:45 +0200

    feat: Initial angular-wrappers test config and BarChart test.

---
+++ templates/angular/ig-ts/bar-chart/default/files/src/app/components/__path__/__name__.spec.ts
@@ -0,0 +1,29 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BarChartComponent } from './bar-chart.component';
+import { IgniteUIModule } from 'igniteui-angular-wrappers';
+
+declare var $;
+
+describe('BarChartComponent', () => {
+  let component: BarChartComponent;
+  let fixture: ComponentFixture<BarChartComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [BarChartComponent],
+      imports: [IgniteUIModule]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BarChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
