168a6e7aac8e5e61686aeeef1043d07e5b8ddb5a
IvayloG <inj.iganchev@gmail.com>
2018-03-13 15:29:55 +0200

    feat: Initial jQuery test configuration for Karma and QUnit.

---
+++ templates/jquery/js/projects/empty/files/test/test-template.spec.js
@@ -0,0 +1,7 @@
+//Illustrative test-template 
+QUnit.test('Test Name', function(assert) {
+    // Setup the various states of the code you want to test and assert conditions.
+    assert.equal(1, 1, '1 === 1');
+    assert.ok(1, '1 is truthy');
+    assert.ok(true, 'true is truthy');
+});
