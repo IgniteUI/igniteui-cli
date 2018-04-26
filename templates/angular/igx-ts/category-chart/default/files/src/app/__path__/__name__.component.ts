2832304e6b10a63f74793e1098f6a8f5c0b40c84
Damyan Petev <damyan.petev@gmail.com>
2018-04-26 19:27:59 +0300

    chore(igniteui-angular): replace chart templates quotes for lint

--- templates/angular/igx-ts/category-chart/default/files/src/app/__path__/__name__.component.ts
+++ templates/angular/igx-ts/category-chart/default/files/src/app/__path__/__name__.component.ts
@@ -6,14 +6,14 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./$(filePrefix).component.css']
 })
 export class $(ClassName)Component {
-  public chartType: string = "Auto";
+  public chartType: string = 'Auto';
 
   data = [
-    { "CountryName": "China", "Pop1995": 1216, "Pop2005": 1297, },
-    { "CountryName": "India", "Pop1995": 920, "Pop2005": 1090, },
-    { "CountryName": "United States", "Pop1995": 266, "Pop2005": 295, },
-    { "CountryName": "Indonesia", "Pop1995": 197, "Pop2005": 229, },
-    { "CountryName": "Brazil", "Pop1995": 161, "Pop2005": 186, }
+    { 'CountryName': 'China', 'Pop1995': 1216, 'Pop2005': 1297, },
+    { 'CountryName': 'India', 'Pop1995': 920, 'Pop2005': 1090, },
+    { 'CountryName': 'United States', 'Pop1995': 266, 'Pop2005': 295, },
+    { 'CountryName': 'Indonesia', 'Pop1995': 197, 'Pop2005': 229, },
+    { 'CountryName': 'Brazil', 'Pop1995': 161, 'Pop2005': 186, }
   ];
 
 }
