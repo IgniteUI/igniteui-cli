f878c3787017b5e50ef635666e01e4b5db293404
IvayloG <inj.iganchev@gmail.com>
2018-05-16 16:33:45 +0300

    feat: Update to ng6.

--- templates/angular/igx-ts/projects/empty/files/src/app/app.component.ts
+++ templates/angular/igx-ts/projects/empty/files/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NavigationStart, Router } from '@angular/router';
-import 'rxjs/add/operator/filter';
+import { filter } from 'rxjs/operators';
 
 import { routes } from './app-routing.module';
 
@@ -31,8 +31,9 @@ export class AppComponent implements OnInit {
   }
 
   public ngOnInit(): void {
-    this.router.events
-      .filter((x) => x instanceof NavigationStart)
+    this.router.events.pipe(
+      filter((x) => x instanceof NavigationStart)
+    )
       .subscribe((event: NavigationStart) => {
           if (event.url !== '/' && !this.navdrawer.pin) {
               // Close drawer when selecting a view on mobile (unpinned)
