a38ebb1a9513a42208abf43f9f3f63da3e06f52c
IvayloG <inj.iganchev@gmail.com>
2018-06-07 14:17:41 +0300

    feat: Add search functionality and rename to crm-grid. #287

---
+++ templates/angular/igx-ts/custom-templates/crm-grid/files/src/app/__path__/__name__.component.ts
@@ -0,0 +1,157 @@
+  import {
+	Component,
+	OnInit,
+	AfterViewInit,
+	QueryList,
+	ViewChild,
+	ChangeDetectorRef
+} from '@angular/core';
+
+	import { IgxColumnComponent } from 'igniteui-angular/grid/column.component';
+	import { IgxDateSummaryOperand, IgxNumberSummaryOperand, IgxSummaryResult } from 'igniteui-angular/grid/grid-summary';
+	import { IgxGridComponent } from 'igniteui-angular/grid/grid.component';
+	import { IgxToggleDirective, STRING_FILTERS } from 'igniteui-angular/main';
+	import { IgxExcelExporterOptions, IgxExcelExporterService } from 'igniteui-angular/services';
+	import { data } from './data';
+
+	
+@Component({
+	selector: 'app-$(filePrefix)',
+	templateUrl: './$(filePrefix).component.html',
+	styleUrls: ['./$(filePrefix).component.scss']
+	})
+export class $(ClassName)Component implements OnInit, AfterViewInit {
+
+	@ViewChild('grid1', { read: IgxGridComponent })
+	public grid1: IgxGridComponent;
+
+	@ViewChild('toggleRefHiding') public toggleHiding: IgxToggleDirective;
+	@ViewChild('toggleRefPinning') public togglePinning: IgxToggleDirective;
+
+	public localData: any[];
+	public dealsSummary = DealsSummary;
+	public earliestSummary = EarliestSummary;
+	public soonSummary = SoonSummary;
+
+	public cols: QueryList<IgxColumnComponent>;
+	public hiddenColsLength: number;
+	public pinnedColsLength: number;
+
+	public searchText = '';
+	public caseSensitive = false;
+
+	constructor(private excelExporterService: IgxExcelExporterService, private cdr: ChangeDetectorRef) { }
+
+	public ngOnInit() {
+			this.localData = data;
+	}
+
+	public ngAfterViewInit() {
+			this.cols = this.grid1.columnList;
+			this.hiddenColsLength = this.cols.filter((col) => col.hidden).length;
+			this.pinnedColsLength = this.cols.filter((col) => col.pinned).length;
+			this.cdr.detectChanges();
+	}
+
+	public toggleVisibility(col: IgxColumnComponent) {
+			if (col.hidden) {
+				this.hiddenColsLength--;
+			} else {
+				this.hiddenColsLength++;
+			}
+			col.hidden = !col.hidden;
+	}
+
+	public togglePin(col: IgxColumnComponent, evt) {
+			if (col.pinned) {
+				this.grid1.unpinColumn(col.field);
+				this.pinnedColsLength--;
+			} else {
+				if (this.grid1.pinColumn(col.field)) {
+					this.pinnedColsLength++;
+				} else {
+					// if pinning fails uncheck the checkbox
+					evt.checkbox.checked = false;
+				}
+			}
+	}
+
+	public exportData() {
+			this.excelExporterService.exportData(this.localData, new IgxExcelExporterOptions('Report'));
+	}
+
+	public formatDate(val: Date) {
+			return new Intl.DateTimeFormat('en-US').format(val);
+	}
+
+	public searchKeyDown(ev) {
+			if (ev.key === 'Enter' || ev.key === 'ArrowDown' || ev.key === 'ArrowRight') {
+				ev.preventDefault();
+				this.grid1.findNext(this.searchText, this.caseSensitive);
+			} else if (ev.key === 'ArrowUp' || ev.key === 'ArrowLeft') {
+				ev.preventDefault();
+				this.grid1.findPrev(this.searchText, this.caseSensitive);
+			}
+	}
+
+	public updateSearch() {
+			this.caseSensitive = !this.caseSensitive;
+			this.grid1.findNext(this.searchText, this.caseSensitive);
+	}
+
+	public clearSearch() {
+			this.searchText = '';
+			this.grid1.clearSearch();
+	}
+}
+
+class DealsSummary extends IgxNumberSummaryOperand {
+	constructor() {
+		super();
+	}
+
+	public operate(summaries?: any[]): IgxSummaryResult[] {
+		const result = super.operate(summaries).filter((obj) => {
+			if (obj.key === 'average' || obj.key === 'sum') {
+				const summaryResult = obj.summaryResult;
+				// apply formatting to float numbers
+				if (Number(summaryResult) === summaryResult && summaryResult % 1 !== 0) {
+					obj.summaryResult = summaryResult.toFixed(2);
+				}
+				return obj;
+			}
+		});
+		return result;
+	}
+}
+
+class EarliestSummary extends IgxDateSummaryOperand {
+	constructor() {
+		super();
+	}
+
+	public operate(summaries?: any[]): IgxSummaryResult[] {
+		const result = super.operate(summaries).filter((obj) => {
+			if (obj.key === 'earliest') {
+				return obj;
+			}
+		});
+		return result;
+	}
+}
+
+class SoonSummary extends IgxDateSummaryOperand {
+	constructor() {
+		super();
+	}
+
+	public operate(summaries?: any[]): IgxSummaryResult[] {
+		const result = super.operate(summaries).filter((obj) => {
+			if (obj.key === 'latest') {
+				obj.label = 'Soon';
+				return obj;
+			}
+		});
+		return result;
+	}
+}
---
+++ templates/angular/igx-ts/custom-templates/crm-grid/index.ts
@@ -0,0 +1,34 @@
+import { IgniteUIForAngularTemplate } from "../../../../../lib/templates/IgniteUIForAngularTemplate";
+
+class IgxGridCRMTemplate extends IgniteUIForAngularTemplate {
+	constructor() {
+		super(__dirname);
+		this.id = "crm-grid";
+		this.projectType = "igx-ts";
+		this.listInComponentTemplates = false;
+		this.listInCustomTemplates = true;
+		this.name = "CRM Grid";
+		this.description = "CRM IgxGrid";
+		this.dependencies = [
+			{ import: "IgxGridModule", from: "igniteui-angular/main", root: true },
+			{ provide: "IgxExcelExporterService", from: "igniteui-angular/services" },
+			{
+				from: "igniteui-angular/main",
+				import: [
+					"IgxAvatarModule",
+					"IgxBadgeModule",
+					"IgxButtonModule",
+					"IgxIconModule",
+					"IgxInputGroupModule",
+					"IgxProgressBarModule",
+					"IgxRippleModule",
+					"IgxSwitchModule",
+					"IgxToggleModule",
+					"IgxCheckboxModule"
+				]
+			}
+		];
+		this.packages = [ "jszip@3.1.5" ];
+	}
+}
+module.exports = new IgxGridCRMTemplate();
