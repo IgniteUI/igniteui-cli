87b364a4e80c1dfab3ba57124fe2cfd73a66d02e
IvayloG <inj.iganchev@gmail.com>
2018-06-11 11:48:12 +0300

    fix: Grid id is unnecessary and causes memory leak

--- templates/angular/igx-ts/custom-templates/awesome-grid/files/src/app/__path__/__name__.component.ts
+++ templates/angular/igx-ts/custom-templates/awesome-grid/files/src/app/__path__/__name__.component.ts
@@ -6,18 +6,18 @@ import {
   OnDestroy,
   OnInit,
   ViewChild
-  } from '@angular/core';
-  import { IgxNumberSummaryOperand, IgxSummaryResult} from 'igniteui-angular/grid/grid-summary';
-  import { IgxGridComponent } from 'igniteui-angular/grid/grid.component';
-  import { STRING_FILTERS } from 'igniteui-angular/main';
-  import { athletesData } from './services/data';
+} from '@angular/core';
+import { IgxNumberSummaryOperand, IgxSummaryResult } from 'igniteui-angular/grid/grid-summary';
+import { IgxGridComponent } from 'igniteui-angular/grid/grid.component';
+import { STRING_FILTERS } from 'igniteui-angular/main';
+import { athletesData } from './services/data';
 
-  @Component({
+@Component({
   selector: 'app-$(filePrefix)',
   templateUrl: './$(filePrefix).component.html',
   styleUrls: ['./$(filePrefix).component.scss']
-  })
-  export class $(ClassName)Component implements OnInit, OnDestroy {
+})
+export class $(ClassName)Component implements OnInit, OnDestroy {
 
   @ViewChild('grid1', { read: IgxGridComponent })
   public grid1: IgxGridComponent;
@@ -32,15 +32,15 @@ import {
 
   get live() {
     return this._live;
-    }
+  }
   set live(val) {
     this._live = val;
     if (this._live) {
       this._timer = setInterval(() => this.ticker(), 3000);
-      } else {
+    } else {
       clearInterval(this._timer);
-      }
     }
+  }
 
   get hideAthleteNumber() {
     return this.windowWidth && this.windowWidth < 960;
@@ -51,7 +51,7 @@ import {
   }
 
 
-  constructor(private zone: NgZone) {}
+  constructor(private zone: NgZone) { }
 
   public ngOnInit() {
     this.localData = athletesData;
@@ -74,8 +74,8 @@ import {
   }
 
   public cellSelection(evt) {
-      const cell = evt.cell;
-      this.grid1.selectRows([cell.row.rowID], true);
+    const cell = evt.cell;
+    this.grid1.selectRows([cell.row.rowID], true);
   }
 
   public getIconType(cell) {
@@ -122,20 +122,20 @@ import {
 
   private updateData() {
     this.localData.map((rec) => {
-        let val = this.generateRandomNumber(-1, 1);
-        switch (val) {
+      let val = this.generateRandomNumber(-1, 1);
+      switch (val) {
         case -1:
-            val = 0;
-            break;
+          val = 0;
+          break;
         case 0:
-            val = 1;
-            break;
+          val = 1;
+          break;
         case 1:
-            val = 3;
-            break;
-        }
+          val = 3;
+          break;
+      }
 
-        rec.TrackProgress += val;
+      rec.TrackProgress += val;
     });
     const unsortedData = this.localData.slice(0);
 
@@ -143,21 +143,21 @@ import {
     this.localData = this.localData.slice(0);
 
     unsortedData.forEach((element, index) => {
-        this.localData.some((elem, ind) => {
-            if (element.Id === elem.Id) {
-                const position = index - ind;
+      this.localData.some((elem, ind) => {
+        if (element.Id === elem.Id) {
+          const position = index - ind;
 
-                if (position < 0) {
-                    elem.Position = 'down';
-                } else if (position === 0) {
-                    elem.Position = 'current';
-                } else {
-                    elem.Position = 'up';
-                }
+          if (position < 0) {
+            elem.Position = 'down';
+          } else if (position === 0) {
+            elem.Position = 'current';
+          } else {
+            elem.Position = 'up';
+          }
 
-                return true;
-            }
-        });
+          return true;
+        }
+      });
     });
 
     if (this.localData[0].TrackProgress >= 100) {
@@ -169,45 +169,45 @@ import {
 
 class CustomTopSpeedSummary extends IgxNumberSummaryOperand {
 
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    public operate(data?: any[]): IgxSummaryResult[] {
-        const result = [];
-        result.push({
-            key: 'average',
-            label: 'average',
-            summaryResult: this.average(data).toFixed(2)
-        });
+  public operate(data?: any[]): IgxSummaryResult[] {
+    const result = [];
+    result.push({
+      key: 'average',
+      label: 'average',
+      summaryResult: this.average(data).toFixed(2)
+    });
 
-        return result;
-    }
+    return result;
+  }
 }
 
 export class CustomBPMSummary extends IgxNumberSummaryOperand {
 
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    public operate(data?: any[]): IgxSummaryResult[] {
-        const result = [];
-        result.push(
-            {
-                key: 'min',
-                label: 'min',
-                summaryResult: this.min(data)
-            }, {
-                key: 'max',
-                label: 'max',
-                summaryResult: this.max(data)
-            }, {
-                key: 'average',
-                label: 'average',
-                summaryResult: this.average(data).toFixed(2)
-            });
+  public operate(data?: any[]): IgxSummaryResult[] {
+    const result = [];
+    result.push(
+      {
+        key: 'min',
+        label: 'min',
+        summaryResult: this.min(data)
+      }, {
+        key: 'max',
+        label: 'max',
+        summaryResult: this.max(data)
+      }, {
+        key: 'average',
+        label: 'average',
+        summaryResult: this.average(data).toFixed(2)
+      });
 
-        return result;
-    }
+    return result;
+  }
 }
