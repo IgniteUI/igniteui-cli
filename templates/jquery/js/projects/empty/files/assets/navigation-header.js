9846b115fdd0726e32b7390be102cca27507ce5f
AMarinov <amarinov@infragistics.com>
2018-07-19 12:30:45 +0300

    Adding "active" class to the selected menu item

--- templates/jquery/js/projects/empty/files/assets/navigation-header.js
+++ templates/jquery/js/projects/empty/files/assets/navigation-header.js
@@ -1,12 +1,25 @@
 /// <reference path="../ignite-cli-views.js" />
 
 $(function () {
-	var menu = "<ul id='list' class='nav-menu'><li class='nav-menu-item'><a href='/'>$(name)</a></li></ul>"
+	var menu = "<ul id='list' class='nav-menu'></ul>"
 	$('body').prepend(menu);
 	var result = "";
+
+	var path = window.location.pathname;
+	path = path.slice(1);
+
+
+	result += "<li class='nav-menu-item" + (path ? "" : " active") + "'><a href='/'>Home</a></li>"
+	
 	for (var i = 0; i < igniteCLIViews.length; i++) {
+		var navMenuClass = "nav-menu-item";
+		
 		var view = igniteCLIViews[i];
-		result += "<li class=\"nav-menu-item\"><a href='/" + view.path + "'>" + view.name + "</a></li>"
+		if(view.path === path) {
+			navMenuClass += " active"; 
+		}
+		result += "<li class=\"" + navMenuClass +"\"><a href='/" + view.path + "'>" + view.name + "</a></li>"
 	}
+
 	result += "<li class=\"nav-menu-item-logo\"><div>Ignite UI CLI</div></li>"; $("#list").append(result);
 });
