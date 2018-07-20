64ea42c90ee068d038860d2110085679ace1bdc1
AMarinov <amarinov@infragistics.com>
2018-07-20 14:16:57 +0300

    Fixing more eslint issues

--- templates/react/es6/projects/empty/files/client/components/Toolbar/index.js
+++ templates/react/es6/projects/empty/files/client/components/Toolbar/index.js
@@ -3,18 +3,18 @@ import { Link } from 'react-router/es6';
 import './toolbar.scss';
 import data from '../../pages/routesTemplate.js';
 
-var links =[];
+var links = [];
 for (var i = 0; i < data.length; i++) {
-    links.push(
-      <Link activeClassName="active" to={data[i].path} key={data[i].text}>
-            {data[i].text}
-	  </Link>
-    )
+  links.push(
+	  <Link activeClassName="active" to={data[i].path} key={data[i].text}>
+          {data[i].text}
+      </Link>
+  );
 }
 
 export default () => (
-  <nav className="navbar" role="navigation">
-      <li className="nav-menu-item-logo"><div>Ignite UI CLI</div></li>
+  <nav className="navbar">
+        <li className="nav-menu-item-logo"><div>Ignite UI CLI</div></li>
     {links}
   </nav>
 );
