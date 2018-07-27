b16cc91fc84abc329365e6f8e91e632d01f2c1dc
Boris <jackofdiamond596@gmail.com>
2018-07-27 15:22:16 +0300

    feat(cli-react): Added styling to the hierarchical grid component.

--- templates/react/es6/hierarchical-grid/hierarchical-grid-export/files/client/components/__path__/index.js
+++ templates/react/es6/hierarchical-grid/hierarchical-grid-export/files/client/components/__path__/index.js
@@ -47,9 +47,9 @@ export default class $(ClassName) extends Component {
 		return (
 			<div className="App">
 				<div className="App-header">
-					<h2>$(description)</h2>
+					<h2 style={{textAlign: "center"}}>$(description)</h2>
 				</div>
-				<div>
+				<div style={{marginLeft: "36vw"}}>
 					<IgCombo
 						id="combo"
 						width="200px"
