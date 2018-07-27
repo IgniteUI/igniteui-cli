1e76710f8b7c163a28c46d1b16bc798627647779
Boris <jackofdiamond596@gmail.com>
2018-07-27 14:15:34 +0300

    feat(cli-react): Added styling for the header and toolbar. Placed the title of the project on the header and toolbar.

--- templates/react/es6/projects/empty/files/client/pages/Home/index.js
+++ templates/react/es6/projects/empty/files/client/pages/Home/index.js
@@ -3,8 +3,7 @@ import Header from 'components/Header';
 
 export default () => (
   <article className="home">
-    <Header title="Home" className="header-dark" />
-    <h2>$(description): $(name)</h2>
+    <Header title="$(description): $(name)" className="header-main" />
     <img src="https://static.infragistics.com/marketing/Website/products/ignite/Ignite-header-apps-960.png" alt="Ignite UI" height="402" width="402" />
   </article>
 );
