71d3535eb7251753d2e85ee015f6934a99d6129b
Damyan Petev <damyan.petev@gmail.com>
2018-08-08 19:45:16 +0300

    feat: add a ComponentGroup interface to describe groups in step by step

---
+++ lib/types/ComponentGroup.d.ts
@@ -0,0 +1,9 @@
+/**
+ * A set of Templates for a common component
+ */
+declare interface ComponentGroup {
+	/** Group name, e.g. Charts or Grids  */
+	name: string;
+	/** Description the group to show in Step by step mode */
+	description: string;
+}
