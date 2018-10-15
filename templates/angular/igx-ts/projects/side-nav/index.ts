mahesh7502/ignite-grid

feat(projects): Make templates selectable in step-by-step

templates/angular/igx-ts/projects/side-nav/index.ts
@@ -1,11 +1,11 @@
 import * as path from "path";
 import { Util } from "../../../../../lib/Util";
-import { BaseIgxProject } from "../base";
+import { BaseIgxProject } from "../_base";
 
-class EmptyJsBlocksProject extends BaseIgxProject implements ProjectTemplate {
-	public id: string = "angular";
-	public name = "empty";
-	public description = "Default empty angular project structure for Ignite UI for Angular";
+export class EmptyJsBlocksProject extends BaseIgxProject implements ProjectTemplate {
+	public id: string = "side-nav";
+	public name = "Default side navigation";
+	public description = "Project structure with side navigation drawer";
 	public dependencies: string[] = [];
 	public framework: string = "angular";
 	public projectType: string = "igx-ts";

IgniteUI/igniteui-cli

templates/angular/igx-ts/projects/side-nav/index.ts
@@ -1,11 +1,11 @@
 import * as path from "path";
 import { Util } from "../../../../../lib/Util";
-import { BaseIgxProject } from "../base";
+import { BaseIgxProject } from "../_base";
 
-class SideNavProject extends BaseIgxProject implements ProjectTemplate {
-	public id: string = "angular";
-	public name = "empty";
-	public description = "Default empty angular project structure for Ignite UI for Angular";
+export class SideNavProject extends BaseIgxProject implements ProjectTemplate {
+	public id: string = "side-nav";
+	public name = "Default side navigation";
+	public description = "Project structure with side navigation drawer";
 	public dependencies: string[] = [];
 	public framework: string = "angular";
 	public projectType: string = "igx-ts";