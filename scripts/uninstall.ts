c861e281a9ce18fce0735047c1c0a22527140929
wnvko <milko.venkov@gmail.com>
2018-10-25 15:33:04 +0300

    fix(*): remove GA from non-global install and uninstall

--- scripts/uninstall.ts
+++ scripts/uninstall.ts
@@ -1,11 +1,13 @@
 import { GoogleAnalytics } from "../lib/GoogleAnalytics";
 
 function uninstall() {
-	GoogleAnalytics.post({
-		t: "screenview",
-		// tslint:disable-next-line:object-literal-sort-keys
-		cd: `uninstall global: ${!!process.env.npm_config_global}`
-	});
+	if (!!process.env.npm_config_global) {
+		GoogleAnalytics.post({
+			t: "screenview",
+			// tslint:disable-next-line:object-literal-sort-keys
+			cd: `uninstall`
+		});
+		}
 }
 
 uninstall();
