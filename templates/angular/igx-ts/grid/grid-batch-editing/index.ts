81ef18bd50fc22225366e704c53914d62951db2e
AMarinov <amarinov@infragistics.com>
2018-11-01 11:52:30 +0200

    Updating the Batch editing template

--- templates/angular/igx-ts/grid/grid-batch-editing/index.ts
+++ templates/angular/igx-ts/grid/grid-batch-editing/index.ts
@@ -1,19 +1,19 @@
 import { IgniteUIForAngularTemplate } from "../../../../../lib/templates/IgniteUIForAngularTemplate";
 
-class IgxGridBatchUpdatingTemplate extends IgniteUIForAngularTemplate {
+class IgxGridBatchEditingTemplate extends IgniteUIForAngularTemplate {
 	constructor() {
 		super(__dirname);
 		this.components = ["Grid"];
 		this.controlGroup = "Grids & Lists";
 		this.listInComponentTemplates = true;
-		this.id = "grid-batch-updating";
+		this.id = "grid-batch-editing";
 		this.projectType = "igx-ts";
-		this.name = "Grid Batch Updating";
-		this.description = "Sample IgxGrid with batch updating";
+		this.name = "Grid Batch Editing";
+		this.description = "Sample IgxGrid with batch editing";
 		this.dependencies = [
 			{ import: "IgxGridModule", from: "igniteui-angular", root: true },
 			{ import: "IgxIconModule", from: "igniteui-angular" }
 		];
 	}
 }
-module.exports = new IgxGridBatchUpdatingTemplate();
+module.exports = new IgxGridBatchEditingTemplate();
