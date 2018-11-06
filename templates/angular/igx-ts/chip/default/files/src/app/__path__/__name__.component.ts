c9743e73fa9cf15411c76a3fab3425e01711905c
Boris <jackofdiamond596@gmail.com>
2018-11-06 15:45:09 +0200

    chip-sample: added styling

--- templates/angular/igx-ts/chip/default/files/src/app/__path__/__name__.component.ts
+++ templates/angular/igx-ts/chip/default/files/src/app/__path__/__name__.component.ts
@@ -1,61 +1,63 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
-import { CloseScrollStrategy, ConnectedPositioningStrategy, IBaseChipEventArgs, IgxChipsAreaComponent, HorizontalAlignment,
-	IgxDropDownComponent, VerticalAlignment } from 'igniteui-angular';
+import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import {
+    CloseScrollStrategy, ConnectedPositioningStrategy, IBaseChipEventArgs, IgxChipsAreaComponent, HorizontalAlignment,
+    IgxDropDownComponent, VerticalAlignment
+} from 'igniteui-angular';
 
 @Component({
-  selector: 'app-$(filePrefix)',
-  templateUrl: './$(filePrefix).component.html',
-  styleUrls: ['./$(filePrefix).component.css']
+    selector: 'app-$(filePrefix)',
+    templateUrl: './$(filePrefix).component.html',
+    styleUrls: ['./$(filePrefix).component.css']
 })
 
 export class $(ClassName)Component {
 
-	public dropDownList = [
+    public dropDownList = [
         {
-			id: "1",
+            id: "1",
             name: "Bug fixing for smart devices"
         },
         {
-			id: "2",
+            id: "2",
             name: "The True Purpose of Design Systems"
         },
         {
-			id: "3",
+            id: "3",
             name: "The IoT is dead, long live the internet"
         },
         {
-			id: "4",
+            id: "4",
             name: "Micro frontends in practice"
         },
         {
-			id: "5",
+            id: "5",
             name: "Lessons about API testing"
         },
         {
-			id: "6",
+            id: "6",
             name: "Scaling big with Apache Kafka"
         },
         {
-			id: "7",
+            id: "7",
             name: "Last minute performance testing"
         },
         {
-			id: "8",
+            id: "8",
             name: "Sales Skillz for IT People"
         }
-	];
+    ];
 
-	public chipList = [ ];
-	
-	@ViewChild("chipsArea", { read: IgxChipsAreaComponent })
-	public chipsArea: IgxChipsAreaComponent;
-	
-	@ViewChild(IgxDropDownComponent)
-	public igxDropDown: IgxDropDownComponent;
-	
-	private dropDownOpened = false;
+    public chipList = [];
 
-	private _positionSettings = {
+    @ViewChild("chipsArea", { read: IgxChipsAreaComponent })
+    public chipsArea: IgxChipsAreaComponent;
+
+    @ViewChild(IgxDropDownComponent)
+    public igxDropDown: IgxDropDownComponent;
+    
+    private dropDownOpened = false;
+
+    private _positionSettings = {
         horizontalStartPoint: HorizontalAlignment.Left,
         verticalStartPoint: VerticalAlignment.Bottom
     };
@@ -67,21 +69,21 @@ export class $(ClassName)Component {
         scrollStrategy: new CloseScrollStrategy()
     };
 
-  constructor(public cdr: ChangeDetectorRef) { }
+    constructor(public cdr: ChangeDetectorRef) { }
 
-	public chipRemoved(event: IBaseChipEventArgs) {
-		this.chipList = this.chipList.filter((item) => {
-			return item.id !== event.owner.id;
-		});
-		this.cdr.detectChanges();
-	}
+    public chipRemoved(event: IBaseChipEventArgs) {
+        this.chipList = this.chipList.filter((item) => {
+            return item.id !== event.owner.id;
+        });
+        this.cdr.detectChanges();
+    }
 
-	public toggleDropDown(ev) {
-		this._overlaySettings.positionStrategy.settings.target = ev.target;
-		this.igxDropDown.toggle(this._overlaySettings);
-	}
+    public toggleDropDown(ev) {
+        this._overlaySettings.positionStrategy.settings.target = ev.target;
+        this.igxDropDown.toggle(this._overlaySettings);
+    }
 
-	public onDropDownOpen() {
+    public onDropDownOpen() {
         this.dropDownOpened = true;
     }
 
@@ -90,25 +92,12 @@ export class $(ClassName)Component {
     }
 
     public itemSelection(ev) {
-		if (
-            this.chipList.find(
-                (val) => val.name === ev.newSelection.elementRef.nativeElement.textContent
-            ) !== undefined
-        ) {
-            return;
-        }
-        let i;
-        for (i = 0; i < this.dropDownList.length; i++) {
-            if (
-                ev.newSelection.elementRef.nativeElement.textContent ===
-                this.dropDownList[i].name
-            ) {
-                this.chipList.push({
-					id: this.dropDownList[i].id,
-                    name: this.dropDownList[i].name
-                });
-                this.igxDropDown.close();
-            }
+        const item = this.dropDownList.find((val) => val.name === ev.newSelection.elementRef.nativeElement.textContent);
+        if (this.chipList.find((val) => val.name === item.name) === undefined) {
+            this.chipList.push({
+                id: item.id,
+                name: item.name
+            });
         }
     }
 }
