2e338d9337c057df7440b5c0ab285ec94aab9213
AMarinov <amarinov@infragistics.com>
2018-11-06 15:14:48 +0200

    Adding the igxTooltip template

---
+++ templates/angular/igx-ts/tooltip/default/files/src/app/__path__/__name__.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+	selector: 'app-$(filePrefix)',
+	templateUrl: './$(filePrefix).component.html',
+	styleUrls: ['./$(filePrefix).component.css']
+})
+
+export class $(ClassName)Component implements OnInit {
+	
+	constructor() { }
+
+	public ngOnInit() {
+	}
+}
