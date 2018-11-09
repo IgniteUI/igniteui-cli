c35df4ad4c443186287024f5440355e11e5299e8
bazal4o <todor.paskalev@gmail.com>
2018-11-09 17:20:00 +0200

    chore(treegrid): Update group priority

--- templates/angular/igx-ts/tree-grid/index.ts
+++ templates/angular/igx-ts/tree-grid/index.ts
@@ -9,8 +9,8 @@ class IgxGridComponent extends BaseComponent {
 		super(__dirname);
 		this.name  = "Tree Grid";
 		this.group = "Grids & Lists";
-		this.description = "pick from tree grids: custom or with summaries";
-		this.groupPriority = 11;
+		this.description = "pick from tree grid templates";
+		this.groupPriority = 9;
 	}
 }
 module.exports = new IgxGridComponent();
