984501833dd6bc4ec533db15bf3b754499837ffd
Boris <jackofdiamond596@gmail.com>
2018-11-14 14:45:36 +0200

    chore(cli): Removed redundant dependency.

--- schematics/ng-add/index.ts
+++ schematics/ng-add/index.ts
@@ -2,23 +2,49 @@
 import { chain, Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
 // tslint:disable-next-line:no-submodule-imports
 import { NodePackageInstallTask } from "@angular-devkit/schematics/tasks";
-// tslint:disable-next-line:no-implicit-dependencies
-import { addPackageJsonDependency, NodeDependency, NodeDependencyType } from "schematics-utilities";
+
+interface IDependency {
+	name: string;
+	version: string;
+}
 
 function addPackageJsonDependencies(): Rule {
 	return (tree: Tree, _context: SchematicContext) => {
-		const dependencies: NodeDependency[] = [
-			{ type: NodeDependencyType.Default, name: "igniteui-angular", version: "^6.2.0" },
-			{ type: NodeDependencyType.Default, name: "web-animations-js", version: "^2.3.1" }
+		const dependencies: IDependency[] = [
+			{ name: "igniteui-angular", version: "^6.2.0" },
+			{ name: "web-animations-js", version: "^2.3.1" }
 		];
 
 		dependencies.forEach(d => {
-			addPackageJsonDependency(tree, d);
-			_context.logger.log("info", `Added ${d.name} into ${d.type} - Version: ${d.version}`);
+			addPackageJsonDependency(tree, "igniteui-angular", "^6.2.0");
+			_context.logger.log("info", `Added ${d.name} to dependencies - Version: ${d.version}`);
 		});
 
 		return tree;
 	};
+}
+
+function addPackageJsonDependency(tree: Tree, pkg: string, version: string): Tree {
+	if (tree.exists("package.json")) {
+		const sourceText = tree.read("package.json")!.toString();
+		const json = JSON.parse(sourceText);
+
+		if (!json.dependencies) {
+			json.dependencies = {};
+		}
+
+		if (!json.dependencies[pkg]) {
+			json.dependencies[pkg] = version;
+			json.dependencies =
+				Object.keys(json.dependencies)
+					.sort()
+					.reduce((result, key) => (result[key] = json.dependencies[key]) && result, {});
+		}
+
+		tree.overwrite("package.json", JSON.stringify(json, null, 2));
+	}
+
+	return tree;
 }
 
 function installPackageJsonDependencies(): Rule {
