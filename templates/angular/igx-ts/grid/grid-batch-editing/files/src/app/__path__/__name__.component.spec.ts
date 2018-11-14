4d155e49e4fd13e0d63c0ea57b38480fd884a555
AMarinov <amarinov@infragistics.com>
2018-11-14 14:28:29 +0200

    Adressing more comments

--- templates/angular/igx-ts/grid/grid-batch-editing/files/src/app/__path__/__name__.component.spec.ts
+++ templates/angular/igx-ts/grid/grid-batch-editing/files/src/app/__path__/__name__.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { $(ClassName)Component } from './$(filePrefix).component';
+import { GridWithTransactionsComponent } from './grid-transaction.component';
 import { IgxDialogModule, IgxGridModule, IgxFocusModule, IgxButtonModule } from 'igniteui-angular';
 
 describe('$(ClassName)Component', () => {
