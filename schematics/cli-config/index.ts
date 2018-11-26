ad5e01103edc302fb099124ffb6e25d582ed3e63
Boris <jackofdiamond596@gmail.com>
2018-11-26 17:17:14 +0200

    feat(schematics): Included default styles

--- schematics/cli-config/index.ts
+++ schematics/cli-config/index.ts
@@ -1,17 +1,31 @@
 // tslint:disable-next-line:no-submodule-imports
 import { WorkspaceSchema } from "@angular-devkit/core/src/workspace";
 // tslint:disable-next-line:ordered-imports
-import { chain, Rule, Tree, SchematicsException } from "@angular-devkit/schematics";
+import { chain, Rule, Tree, SchematicsException, contentTemplate } from "@angular-devkit/schematics";
 // tslint:disable-next-line:no-submodule-imports
 import { getWorkspace } from "@schematics/angular/utility/config";
 import { Util } from "../../lib/Util";
 import { addTypography } from "../../migrations/update-3/index";
+
+const cssImport = "node_modules/igniteui-angular/styles/igniteui-angular.css";
+const sassImports =
+	`
+@import "~igniteui-angular/lib/core/styles/themes/index";
+// Uncomment the following lines if you want to add a custom palette:
+// $primary: #731963 !default;
+// $secondary: #ce5712 !default;
+// $app-palette: igx-palette($primary, $secondary);
+
+@include igx-core();
+@include igx-theme($default-theme-palette);
+`;
 
 function createCliConfig(): Rule {
 	return (tree: Tree) => {
 		tree.create("ignite-ui-cli.json", JSON.stringify(GetCliConfig(tree), null, 2) + "\n");
 		addTypography(tree);
 		addFontsToIndexHtml(tree);
+		importDefaultTheme(tree);
 
 		return tree;
 	};
@@ -33,12 +47,108 @@ function GetCliConfig(tree: Tree): Config {
 
 		return cliConfig;
 	} catch (e) {
-		switch (e) {
-			case typeof e === typeof SchematicsException:
-				throw new SchematicsException("angular.json was not found in the project's root.");
-			default:
-				throw new Error(e.message);
+		if (e.toString().includes("Could not find (undefined)")) {
+			throw new SchematicsException("angular.json was not found in the project's root");
 		}
+
+		throw new Error(e.message);
+	}
+}
+
+function importDefaultTheme(tree: Tree): Tree {
+	if (tree.exists("/src/styles.sass")) {
+		importDefaultThemeSass(tree);
+	} else if (tree.exists("/src/styles.scss")) {
+		importDefaultThemeScss(tree);
+	} else if (tree.exists("/src/styles.css")) {
+		importDefaultThemeCss(tree);
+	}
+
+	return tree;
+}
+
+function importDefaultThemeScss(tree: Tree): Tree {
+	const targetFile = "/src/styles.scss";
+	let content = tree.read(targetFile).toString();
+
+	if (!content.includes(sassImports)) {
+		content += sassImports;
+	}
+
+	tree.overwrite(targetFile, content);
+	return tree;
+}
+
+function importDefaultThemeSass(tree: Tree): Tree {
+	const targetFile = "/src/styles.sass";
+	let content = tree.read(targetFile).toString();
+
+	if (!content.includes(sassImports)) {
+		content += sassImports;
+	}
+
+	tree.overwrite(targetFile, content);
+	return tree;
+}
+
+function importDefaultThemeCss(tree: Tree): Tree {
+	const targetFile = "/angular.json";
+	const workspace = getWorkspace(tree);
+	const importedStylesToBuild = importDefaultThemeToBuildStyles(workspace);
+	const importedStylesToTest = importDefaultThemeToTestStyles(workspace);
+
+	if (importedStylesToBuild || importedStylesToTest) {
+		tree.overwrite(targetFile, JSON.stringify(workspace, null, 2) + "\n");
+	}
+
+	return tree;
+}
+
+function importDefaultThemeToBuildStyles(workspace: WorkspaceSchema) {
+	const projectName = workspace.defaultProject;
+	if (projectName) {
+		if (!workspace.projects[projectName].architect) {
+			workspace.projects[projectName].architect = {};
+		}
+		if (!workspace.projects[projectName].architect.build) {
+			workspace.projects[projectName].architect.build = {};
+		}
+		if (!workspace.projects[projectName].architect.build.options) {
+			workspace.projects[projectName].architect.build.options = {};
+		}
+		if (!workspace.projects[projectName].architect.build.options.styles) {
+			workspace.projects[projectName].architect.build.options.styles = [];
+		}
+		if (!workspace.projects[projectName].architect.build.options.styles.includes(cssImport)) {
+			workspace.projects[projectName].architect.build.options.styles.push(cssImport);
+			return true;
+		}
+
+		return false;
+	}
+}
+
+function importDefaultThemeToTestStyles(workspace: WorkspaceSchema) {
+	const projectName = workspace.defaultProject;
+	if (projectName) {
+		if (!workspace.projects[projectName].architect) {
+			workspace.projects[projectName].architect = {};
+		}
+		if (!workspace.projects[projectName].architect.test) {
+			workspace.projects[projectName].architect.test = {};
+		}
+		if (!workspace.projects[projectName].architect.test.options) {
+			workspace.projects[projectName].architect.test.options = {};
+		}
+		if (!workspace.projects[projectName].architect.test.options.styles) {
+			workspace.projects[projectName].architect.test.options.styles = [];
+		}
+		if (!workspace.projects[projectName].architect.test.options.styles.includes(cssImport)) {
+			workspace.projects[projectName].architect.test.options.styles.push(cssImport);
+			return true;
+		}
+
+		return false;
 	}
 }
 
