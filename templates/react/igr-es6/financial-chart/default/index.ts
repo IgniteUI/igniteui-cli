e91cc8178516a978762d30ccd3a676ba79a67961
ViktorSlavov <viktorslavov91@gmail.com>
2019-02-13 11:53:05 +0200

    feat(igr): add financial chart template to igr-es6 templates

---
+++ templates/react/igr-es6/financial-chart/default/index.ts
@@ -0,0 +1,18 @@
+import { IgniteUIForReactTemplate } from "../../../../../lib/templates/IgniteUIForReactTemplate";
+
+class IgrFinancialChartTemplate extends IgniteUIForReactTemplate {
+	constructor() {
+		super(__dirname);
+		this.components = ["Financial Chart"];
+		this.controlGroup = "Charts";
+		// set to true once bug with chart destoy is fixed
+		this.listInComponentTemplates = false;
+		this.id = "financial-chart";
+		this.projectType = "igr-es6";
+		this.name = "Financial Chart";
+		this.description = `charting component that makes it easy to visualize financial data by
+							using a simple and intuitive API.`;
+		this.packages = ["igniteui-react-charts@^16.6.4"]; // TODO: read version from igniteui-react-core in package.json
+	}
+}
+module.exports = new IgrFinancialChartTemplate();
