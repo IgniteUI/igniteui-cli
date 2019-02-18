83014ee2ae233e30d9b97c7f4a707d6afb41e601
Boris <jackofdiamond596@gmail.com>
2019-02-18 17:03:36 +0200

    feat(*): Template the autocomplete sample to include groups as well.

---
+++ templates/angular/igx-ts/autocomplete/simple-autocomplete/files/src/app/__path__/towns-data.ts
@@ -0,0 +1,478 @@
+export const townsSimple = [
+	"Sofia", "Plovdiv", "Varna", "Burgas", "Ruse", "Stara Zagora", "Pleven", "Dobrich", "Sliven", "Shumen", 
+	"Pernik", "Haskovo", "Yambol", "Pazardzhik", "Blagoevgrad", "Veliko Tarnovo", "Vratsa", "Gabrovo", "Asenovgrad", 
+	"Vidin", "Kazanlak", "Kyustendil", "Kardzhali", "Montana", "Dimitrovgrad", "Targovishte", "Lovech", "Silistra", "Dupnitsa", 
+	"Svishtov", "Razgrad", "Gorna Oryahovitsa", "Smolyan", "Petrich", "Sandanski", "Samokov", "Sevlievo", "Lom", 
+	"Karlovo", "Velingrad", "Nova Zagora", "Troyan", "Aytos", "Botevgrad", "Gotse Delchev", "Peshtera", "Harmanli", "Karnobat", 
+	"Svilengrad", "Panagyurishte", "Chirpan", "Popovo", "Rakovski", "Radomir", "Novi Iskar", "Kozloduy", "Parvomay", "Berkovitsa", 
+	"Cherven Bryag", "Pomorie", "Ihtiman", "Radnevo", "Provadiya", "Novi Pazar", "Razlog", "Byala Slatina", "Nesebar", "Balchik", 
+	"Kostinbrod", "Stamboliyski", "Kavarna", "Knezha", "Pavlikeni", "Mezdra", "Etropole", "Levski", "Teteven", "Elhovo", "Bankya", 
+	"Tryavna", "Lukovit", "Tutrakan", "Sredets", "Sopot", "Byala", "Veliki Preslav", "Isperih", "Belene", "Omurtag", "Bansko", "Krichim", 
+	"Galabovo", "Devnya", "Septemvri", "Rakitovo", "Lyaskovets", "Svoge", "Aksakovo", "Kubrat", "Dryanovo", "Beloslav", "Pirdop", "Lyubimets", 
+	"Momchilgrad", "Slivnitsa", "Hisarya", "Zlatograd", "Kostenets", "Devin", "General Toshevo", "Simeonovgrad", "Simitli", "Elin Pelin", 
+	"Dolni Chiflik", "Tervel", "Dulovo", "Varshets", "Kotel", "Madan", "Straldzha", "Saedinenie", "Bobov Dol", "Tsarevo", "Kuklen", "Tvarditsa", 
+	"Yakoruda", "Elena", "Topolovgrad", "Bozhurishte", "Chepelare", "Oryahovo", "Sozopol", "Belogradchik", "Perushtitsa", "Zlatitsa", "Strazhitsa", 
+	"Krumovgrad", "Kameno", "Dalgopol", "Vetovo", "Suvorovo", "Dolni Dabnik", "Dolna Banya", "Pravets", "Nedelino", "Polski Trambesh", "Trastenik", 
+	"Bratsigovo", "Koynare", "Godech", "Slavyanovo", "Dve Mogili", "Kostandovo", "Debelets", "Strelcha", "Sapareva Banya", "Ignatievo", "Smyadovo", 
+	"Breznik", "Sveti Vlas", "Nikopol", "Shivachevo", "Belovo", "Tsar Kaloyan", "Ivaylovgrad", "Valchedram", "Marten", "Glodzhevo", "Sarnitsa", 
+	"Letnitsa", "Varbitsa", "Iskar", "Ardino", "Shabla", "Rudozem", "Vetren", "Kresna", "Banya", "Batak", "Maglizh", "Valchi Dol", "Gulyantsi", 
+	"Dragoman", "Zavet", "Kran", "Miziya", "Primorsko", "Sungurlare", "Dolna Mitropoliya", "Krivodol", "Kula", "Kalofer", "Slivo Pole", 
+	"Kaspichan", "Apriltsi", "Belitsa", "Roman", "Dzhebel", "Dolna Oryahovitsa", "Buhovo", "Gurkovo", "Pavel Banya", "Nikolaevo", "Yablanitsa", 
+	"Kableshkovo", "Opaka", "Rila", "Ugarchin", "Dunavtsi", "Dobrinishte", "Hadzhidimovo", "Bregovo", "Byala Cherkva", "Zlataritsa", 
+	"Kocherinovo", "Dospat", "Tran", "Sadovo", "Laki", "Koprivshtitsa", "Malko Tarnovo", "Loznitsa", "Obzor", "Kilifarevo", "Borovo", 
+	"Batanovtsi", "Chernomorets", "Aheloy", "Byala", "Pordim", "Suhindol", "Merichleri", "Glavinitsa", "Chiprovtsi", "Kermen", "Brezovo", 
+	"Plachkovtsi", "Zemen", "Balgarovo", "Alfatar", "Boychinovtsi", "Gramada", "Senovo", "Momin Prohod", "Kaolinovo", "Shipka", "Antonovo", 
+	"Ahtopol", "Boboshevo", "Bolyarovo", "Brusartsi", "Klisura", "Dimovo", "Kiten", "Pliska", "Madzharovo", "Melnik"
+];
+
+export const townsGrouped = {
+	regions: [
+		{
+			name: "Blagoevgrad",
+			towns: [
+				"Bansko",
+				"Belitsa",
+				"Blagoevgrad (city)",
+				"Garmen",
+				"Gotse Delchev",
+				"Hadzhidimovo",
+				"Kresna",
+				"Petrich",
+				"Razlog",
+				"Sandanski",
+				"Satovcha",
+				"Simitli",
+				"Strumyani",
+				"Yakoruda"
+			]
+		},
+		{
+			name: "Burgas",
+			towns: [
+				"Aytos",
+				"Burgas (city)",
+				"Kameno",
+				"Karnobat",
+				"Malko Tarnovo",
+				"Nesebar",
+				"Pomorie",
+				"Primorsko",
+				"Ruen",
+				"Sozopol",
+				"Sredets",
+				"Sungurlare",
+				"Tsarevo"
+			]
+		},
+		{
+			name: "Dobrich",
+			towns: [
+				"Balchik",
+				"Dobrich (city)",
+				"Dobrichka (rural)",
+				"General Toshevo",
+				"Kavarna",
+				"Krushari",
+				"Shabla",
+				"Tervel"
+			]
+		},
+		{
+			name: "Gabrovo",
+			towns: [
+				"Dryanovo",
+				"Gabrovo (city)",
+				"Sevlievo",
+				"Tryavna"
+			]
+		},
+		{
+			name: "Haskovo",
+			towns: [
+				"Dimitrovgrad",
+				"Harmanli",
+				"Haskovo (city)",
+				"Ivaylovgrad",
+				"Lyubimets",
+				"Madzharovo",
+				"Mineralni bani",
+				"Simeonovgrad",
+				"Stambolovo",
+				"Svilengrad",
+				"Topolovgrad"
+			]
+		},
+		{
+			name: "Kardzhali",
+			towns: [
+				"Ardino",
+				"Chernoochene",
+				"Dzhebel",
+				"Kardzhali (city)",
+				"Kirkovo",
+				"Krumovgrad",
+				"Momchilgrad"
+			]
+		},
+		{
+			name: "Kyustendil",
+			towns: [
+				"Boboshevo",
+				"Bobov dol",
+				"Dupnitsa",
+				"Kocherinovo",
+				"Kyustendil (city)",
+				"Nevestino",
+				"Nevestino",
+				"Rila",
+				"Sapareva banya",
+				"Treklyanod"
+			]
+		},
+		{
+			name: "Lovech",
+			towns: [
+				"Apriltsi",
+				"Letnitsa",
+				"Lovech (city)",
+				"Lukovit",
+				"Teteven",
+				"Troyan",
+				"Ugarchin",
+				"Yablanitsa"
+			]
+		},
+		{
+			name: "Montana",
+			towns: [
+				"Berkovitsa",
+				"Boychinovtsi",
+				"Brusartsi",
+				"Chiprovtsi",
+				"Georgi Damyanovo",
+				"Lom",
+				"Medkovets",
+				"Montana (city)",
+				"Valchedram",
+				"Varshets",
+				"Yakimovo"
+			]
+		},
+		{
+			name: "Pazardzhik",
+			towns: [
+				"Pazardzhik (city)",
+				"Velingrad",
+				"Septemvri",
+				"Panagyurishte",
+				"Peshtera",
+				"Rakitovo",
+				"Bratsigovo",
+				"Belovo",
+				"Batak",
+				"Lesichovo",
+				"Strelcha",
+				"Sarnitsa"
+			]
+		},
+		{
+			name: "Pernik",
+			towns: [
+				"Breaznik",
+				"Pernik (city)",
+				"Kovachevci",
+				"Radomir",
+				"Tran",
+				"Zemen"
+			]
+		},
+		{
+			name: "Pleven",
+			towns: [
+				"Belene",
+				"Gulyantsi",
+				"Dolna Mitropoliya",
+				"Dolni Dabnik",
+				"Levski",
+				"Nikopol",
+				"Iskar",
+				"Pleven (city)",
+				"Pordim",
+				"Cherven Bryag",
+				"Knezha"
+			]
+		},
+		{
+			name: "Plovdiv",
+			towns: [
+				"Asenovgrad",
+				"Brezovo",
+				"Hisarya",
+				"Kaloyanovo",
+				"Kaloyanovo",
+				"Karlovo",
+				"Krichim",
+				"Kuklen",
+				"Laki",
+				"Maritsa (Plovdiv rural)",
+				"Perushtitsa",
+				"Plovdiv (city)",
+				"Parvomay",
+				"Rakovski",
+				"Rodopi (Plovdiv rural)",
+				"Sadovo",
+				"Sopot",
+				"Stamboliyski",
+				"Saedinenie"
+			]
+		},
+		{
+			name: "Razgrad",
+			towns: [
+				"Isperih",
+				"Kubrat",
+				"Loznitsa",
+				"Razgrad (city)",
+				"Samuil",
+				"Samuil",
+				"Tsar Kaloyan",
+				"Zavet"
+			]
+		},
+		{
+			name: "Ruse",
+			towns: [
+				"Borovo",
+				"Byala",
+				"Vetovo",
+				"Dve Mogili",
+				"Ivanovo",
+				"Ivanovo",
+				"Ruse (city)",
+				"Slivo Pole",
+				"Tsenovo"
+			]
+		},
+		{
+			name: "Shumen",
+			towns: [
+				"Venets",
+				"Venets",
+				"Varbitsa",
+				"Hitrino",
+				"Kaolinovo",
+				"Kaspichan",
+				"Nikola Kozlevo",
+				"Novi Pazar",
+				"Veliki Preslav",
+				"Smyadovo",
+				"Shumen"
+			]
+		},
+		{
+			name: "Silistra",
+			towns: [
+				"Alfatar",
+				"Glavinitsa",
+				"Dulovo",
+				"Kaynardzha",
+				"Silistra (city)",
+				"Sitovo",
+				"Tutrakan"
+			]
+		},
+		{
+			name: "Sliven",
+			towns: [
+				"Kotel",
+				"Nova Zagora",
+				"Sliven (city)",
+				"Tvarditsa"
+			]
+		},
+		{
+			name: "Smolyan",
+			towns: [
+				"Banite",
+				"Borino",
+				"Chepelare",
+				"Devin",
+				"Dospat",
+				"Madan",
+				"Nedelino",
+				"Rudozem",
+				"Smolyan (city)",
+				"Zlatograd"
+			]
+		},
+		{
+			name: "Sofia City",
+			towns: [
+				"Balsha",
+				"Bistritsa",
+				"Busmantsi",
+				"Chepintsi",
+				"Dobroslavtsi",
+				"Dolni Bogrov",
+				"Dolni Pasarel",
+				"German",
+				"Gorni Bogrov",
+				"Ivanyane",
+				"Jeleznitsa",
+				"Jelyava",
+				"Jiten",
+				"Kazichene",
+				"Klisura",
+				"Kokalyane",
+				"Krivina",
+				"Kubratovo",
+				"Katina",
+				"Lokorsko",
+				"Lozen",
+				"Malo Buchino",
+				"Marchaevo",
+				"Mirovyane",
+				"Mramor",
+				"Negovan",
+				"Pancharevo",
+				"Plana",
+				"Podgumer",
+				"Svetovrachene",
+				"Vladaya",
+				"Voluyak",
+				"Voynegovtsi",
+				"Yana"
+			]
+		},
+		{
+			name: "Sofia",
+			towns: [
+				"Anton",
+				"Anton",
+				"Botevgrad",
+				"Bozhurishte",
+				"Chavdar",
+				"Chavdar",
+				"Chelopech",
+				"Dolna Banya",
+				"Dragoman",
+				"Elin Pelin",
+				"Etropole",
+				"Godech",
+				"Gorna Malina",
+				"Ihtiman",
+				"Koprivshtitsa",
+				"Kostenets",
+				"Kostinbrod",
+				"Mirkovo",
+				"Pirdop",
+				"Pravets",
+				"Samokov",
+				"Slivnitsa",
+				"Svoge",
+				"Zlatitsa"
+			]
+		},
+		{
+			name: "Stara Zagora",
+			towns: [
+				"Bratya",
+				"Chirpan",
+				"Gurkovo",
+				"Galabovo",
+				"Kazanlak",
+				"Maglizh",
+				"Nikolaevo",
+				"Opan",
+				"Pavel Banya",
+				"Radnevo",
+				"Stara Zagora (city)"
+			]
+		},
+		{
+			name: "Targovishte",
+			towns: [
+				"Targovishte (city)",
+				"Popovo",
+				"Omurtag",
+				"Antonovo",
+				"Opaka"
+			]
+		},
+		{
+			name: "Varna",
+			towns: [
+				"Avren",
+				"Avren",
+				"Aksakovo",
+				"Beloslav",
+				"Byala",
+				"Varna (city)",
+				"Vetrino",
+				"Valchi Dol",
+				"Dolni Chiflik",
+				"Devnya",
+				"Dalgopol",
+				"Provadiya",
+				"Suvorovo"
+			]
+		},
+		{
+			name: "Veliko Tarnovo",
+			towns: [
+				"Elena",
+				"Gorna Oryahovitsa",
+				"Lyaskovets",
+				"Pavlikeni",
+				"Polski Trambesh",
+				"Strazhitsa",
+				"Suhindol",
+				"Svishtov",
+				"Veliko Tarnovo (city)",
+				"Zlataritsa"
+			]
+		},
+		{
+			name: "Vidin",
+			towns: [
+				"Belogradchik",
+				"Boynitsa",
+				"Bregovo",
+				"Vidin (city)",
+				"Gramada",
+				"Dimovo",
+				"Kula",
+				"Makresh",
+				"Novo Selo",
+				"Novo Selo",
+				"Ruzhintsi",
+				"Chuprene"
+			]
+		},
+		{
+			name: "Vratsa",
+			towns: [
+				"Borovan",
+				"Byala Slatina",
+				"Vratsa",
+				"Kozloduy",
+				"Krivodol",
+				"Mezdra",
+				"Mizia",
+				"Oryahovo",
+				"Roman",
+				"Hayredin"
+			]
+		},
+		{
+			name: "Yambol",
+			towns: [
+				"Bolyarovo",
+				"Elhovo",
+				"Strandzha",
+				"Tundzha (rural)",
+				"Yambol (city)"
+			]
+		}
+	]
+};
--- templates/angular/igx-ts/autocomplete/simple-autocomplete/index.ts
+++ templates/angular/igx-ts/autocomplete/simple-autocomplete/index.ts
@@ -1,6 +1,8 @@
 import { IgniteUIForAngularTemplate } from "../../../../../lib/templates/IgniteUIForAngularTemplate";
 
 class IgxAutocompleteTemplate extends IgniteUIForAngularTemplate {
+	private userExtraConfiguration: {} = {};
+
 	constructor() {
 		super(__dirname);
 		this.components = ["Autocomplete"];
@@ -15,8 +17,63 @@ class IgxAutocompleteTemplate extends IgniteUIForAngularTemplate {
 			from: "igniteui-angular"
 		}, {
 			declare: ["$(ClassName)PipeStartsWith"],
-			from: "./../pipes/__name__.pipe.ts"
+			from: "./src/app/__path__/__name__.component.ts"
 		}];
+		this.hasExtraConfiguration = true;
+	}
+
+	public setExtraConfiguration(extraConfigKeys: {}) {
+		this.userExtraConfiguration = extraConfigKeys;
+	}
+
+	public getExtraConfiguration(): ControlExtraConfiguration[] {
+		return [{
+			choices: ["Simple Autocomplete", "Autocomplete With Groups"],
+			default: "Simple Autocomplete",
+			key: "autocompleteOptions",
+			message: "Select what the type of IgxAutocomplete you want",
+			type: Enumerations.ControlExtraConfigType.Choice
+		}];
+	}
+
+	public generateFiles(projectPath: string, name: string, ...options: any[]): Promise<boolean> {
+		const selection = this.userExtraConfiguration["autocompleteOptions"];
+		const extraConfig = {
+			"$(autocomplete)": {}
+		};
+		switch (selection) {
+			case "Simple Autocomplete":
+				extraConfig["$(autocomplete)"] = this.getSimpleAutoCompleteTmp();
+				break;
+			case "Autocomplete With Groups":
+				extraConfig["$(autocomplete)"] = this.getAutocompleteWithGroupsTmp();
+				break;
+			default:
+				throw new Error("Unexpected input.");
+		}
+
+		return super.generateFiles(projectPath, name, { extraConfig });
+	}
+
+	private getSimpleAutoCompleteTmp() {
+		const template =
+			`<igx-drop-down #townsPanel maxHeight="300px">
+				<igx-drop-down-item *ngFor="let town of townsSimple | $(camelCaseName)StartsWith:townSelected" [value]="town">
+					{{town}}
+				</igx-drop-down-item>
+			</igx-drop-down>`;
+		return template;
+	}
+	private getAutocompleteWithGroupsTmp() {
+		const template =
+			`<igx-drop-down #townsPanel maxHeight="300px">
+				<igx-drop-down-item-group *ngFor="let region of townsGrouped.regions" [label]="region.name">
+					<igx-drop-down-item *ngFor="let town of region.towns" [value]="town">
+						{{town}}
+					</igx-drop-down-item>
+				</igx-drop-down-item-group>
+			</igx-drop-down>`;
+		return template;
 	}
 }
 module.exports = new IgxAutocompleteTemplate();
