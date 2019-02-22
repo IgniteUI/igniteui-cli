7181604aacd012dd74a9a83ff0f5e6151dd2fffd
Boris <jackofdiamond596@gmail.com>
2019-02-22 17:05:45 +0200

    chore(select): Use tab for indentation; alphabetize imports

--- templates/angular/igx-ts/select/select-in-form/files/src/app/__path__/__name__.component.ts
+++ templates/angular/igx-ts/select/select-in-form/files/src/app/__path__/__name__.component.ts
@@ -2,16 +2,16 @@ import { Component, ViewChild } from "@angular/core";
 import { IgxSelectComponent } from "igniteui-angular";
 
 @Component({
-    selector: "app-$(filePrefix)",
-    styleUrls: ["$(filePrefix).component.scss"],
-    templateUrl: "$(filePrefix).component.html"
+	selector: "app-$(filePrefix)",
+	styleUrls: ["$(filePrefix).component.scss"],
+	templateUrl: "$(filePrefix).component.html"
 })
 export class $(ClassName)Component {
-    @ViewChild(IgxSelectComponent)
-    public igxSelect: IgxSelectComponent;
+	@ViewChild(IgxSelectComponent)
+	public igxSelect: IgxSelectComponent;
 
-    public items: string[] = ["Orange", "Apple", "Banana", "Mango"];
-    public value: string;
+	public items: string[] = ["Orange", "Apple", "Banana", "Mango"];
+	public value: string;
 
 	public onSubmit() {
 		this.value = this.igxSelect.value;
