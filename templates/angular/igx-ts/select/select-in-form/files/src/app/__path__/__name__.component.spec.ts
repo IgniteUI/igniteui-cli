254dfec71bb7b249e026e67cde7f9cfe1b992617
Boris <jackofdiamond596@gmail.com>
2019-02-28 17:02:17 +0200

    chore(select): Fix indentation in spec file.

--- templates/angular/igx-ts/select/select-in-form/files/src/app/__path__/__name__.component.spec.ts
+++ templates/angular/igx-ts/select/select-in-form/files/src/app/__path__/__name__.component.spec.ts
@@ -1,34 +1,40 @@
-import { FormsModule } from "@angular/forms";
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from '@angular/forms';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-import { $(ClassName)Component } from "./$(filePrefix).component";
-import { IgxSelectModule, IgxToggleModule } from "igniteui-angular";
+import { $(ClassName)Component } from './$(filePrefix).component';
+import {
+    IgxSelectModule, IgxToggleModule, IgxDropDownModule, IgxFilterModule, IgxButtonModule, IgxToastModule
+} from 'igniteui-angular';
 
-describe("$(ClassName)Component", () => {
-	let component: $(ClassName)Component;
-	let fixture: ComponentFixture<$(ClassName)Component>;
+describe('$(ClassName)Component', () => {
+    let component: $(ClassName)Component;
+    let fixture: ComponentFixture<$(ClassName)Component >;
 
-	beforeEach(async(() => {
-		TestBed.configureTestingModule({
-			declarations: [$(ClassName)Component],
-			imports: [
-				FormsModule,
-				IgxSelectModule,
-				IgxToggleModule,
-				NoopAnimationsModule,
-			]
-		})
-			.compileComponents();
-	}));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [$(ClassName)Component],
+            imports: [
+                FormsModule,
+                IgxSelectModule,
+                IgxToggleModule,
+                IgxDropDownModule,
+                IgxFilterModule,
+                IgxButtonModule,
+                IgxToastModule,
+                NoopAnimationsModule,
+            ]
+        })
+            .compileComponents();
+    }));
 
-	beforeEach(() => {
-		fixture = TestBed.createComponent($(ClassName)Component);
-		component = fixture.componentInstance;
-		fixture.detectChanges();
-	});
+    beforeEach(() => {
+        fixture = TestBed.createComponent($(ClassName)Component);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-	it("should create", () => {
-		expect(component).toBeTruthy();
-	});
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
