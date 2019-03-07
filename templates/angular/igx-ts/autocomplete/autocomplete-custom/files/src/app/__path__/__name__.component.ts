792fbb292f1abca1af017e7d3bfdc19859dc9034
Boris <jackofdiamond596@gmail.com>
2019-03-07 11:48:05 +0200

    chore(autocomplete): Remove template with simple groups.

--- templates/angular/igx-ts/autocomplete/autocomplete-custom/files/src/app/__path__/__name__.component.ts
+++ templates/angular/igx-ts/autocomplete/autocomplete-custom/files/src/app/__path__/__name__.component.ts
@@ -1,5 +1,5 @@
 import { Component, Pipe, PipeTransform } from '@angular/core';
-import { $(LocalData) } from './towns-data';
+import { Towns } from './towns-data';
 
 @Component({
     selector: 'app-$(filePrefix)',
@@ -11,7 +11,7 @@ export class $(ClassName)Component {
     public townSelected;
 
     constructor() {
-        this.towns = $(LocalData);
+        this.towns = Towns;
     }
 }
 
@@ -19,20 +19,5 @@ export class $(ClassName)Component {
 export class $(ClassName)PipeStartsWith implements PipeTransform {
     public transform(collection: any[], term = '') {
         return collection.filter((item) => item.toString().toLowerCase().startsWith(term.trim().toLowerCase()));
-    }
-}
-
-@Pipe({ name: '$(camelCaseName)RegionContains' })
-export class $(ClassName)RegionContains implements PipeTransform {
-    transform(regions: any[], term = '') {
-        return this.filterRegions(regions, term);
-    }
-
-    private filterRegions(regions: any[], term: string) {
-        return regions.filter((region: any) => this.filterTowns(region.towns, term.trim().toLowerCase()).length > 0);
-    }
-
-    private filterTowns(towns: string[], term: string) {
-        return towns.filter((town: string) => town.toLowerCase().startsWith(term))
     }
 }
--- templates/angular/igx-ts/autocomplete/autocomplete-custom/index.ts
+++ templates/angular/igx-ts/autocomplete/autocomplete-custom/index.ts
@@ -1,8 +1,6 @@
 import { IgniteUIForAngularTemplate } from "../../../../../lib/templates/IgniteUIForAngularTemplate";
 
 class IgxAutocompleteTemplate extends IgniteUIForAngularTemplate {
-	private userExtraConfiguration: {} = {};
-
 	constructor() {
 		super(__dirname);
 		this.components = ["Autocomplete"];
@@ -10,77 +8,15 @@ class IgxAutocompleteTemplate extends IgniteUIForAngularTemplate {
 		this.listInComponentTemplates = true;
 		this.id = "autocomplete";
 		this.projectType = "igx-ts";
-		this.name = "Customizable Autocomplete";
-		this.description = "Simple or Grouped IgxAutocomplete";
+		this.name = "Simple Autocomplete";
+		this.description = "Simple IgxAutocomplete";
 		this.dependencies = [{
 			import: ["IgxAutocompleteModule", "IgxDropDownModule", "IgxInputGroupModule"],
 			from: "igniteui-angular"
 		}, {
 			declare: ["$(ClassName)PipeStartsWith"],
 			from: "./src/app/__path__/__name__.component.ts"
-		}, {
-			declare: ["$(ClassName)RegionContains"],
-			from: "./src/app/__path__/__name__.component.ts"
-		}];
-		this.hasExtraConfiguration = true;
-	}
-
-	public setExtraConfiguration(extraConfigKeys: {}) {
-		this.userExtraConfiguration = extraConfigKeys;
-	}
-
-	public getExtraConfiguration(): ControlExtraConfiguration[] {
-		return [{
-			choices: ["Simple Autocomplete", "Autocomplete With Groups"],
-			default: "Simple Autocomplete",
-			key: "autocompleteOptions",
-			message: "Select the type of IgxAutocomplete you prefer",
-			type: Enumerations.ControlExtraConfigType.Choice
-		}];
-	}
-
-	public generateFiles(projectPath: string, name: string, ...options: any[]): Promise<boolean> {
-		const selection = this.userExtraConfiguration["autocompleteOptions"];
-		const extraConfig = {
-			"$(autocomplete)": "",
-			"$(LocalData)": ""
-		};
-		switch (selection) {
-			case "Simple Autocomplete":
-				extraConfig["$(autocomplete)"] = this.getSimpleAutoCompleteTmp();
-				extraConfig["$(LocalData)"] = "townsSimple";
-				break;
-			case "Autocomplete With Groups":
-				extraConfig["$(autocomplete)"] = this.getAutocompleteWithGroupsTmp();
-				extraConfig["$(LocalData)"] = "townsGrouped";
-				break;
-			default:
-				throw new Error("Unexpected input.");
 		}
-
-		return super.generateFiles(projectPath, name, { extraConfig });
-	}
-
-	private getSimpleAutoCompleteTmp() {
-		const template =
-			`<igx-drop-down #townsPanel maxHeight="300px">
-				<igx-drop-down-item *ngFor="let town of towns | $(camelCaseName)StartsWith:townSelected" [value]="town">
-					{{town}}
-				</igx-drop-down-item>
-			</igx-drop-down>`;
-		return template;
-	}
-	private getAutocompleteWithGroupsTmp() {
-		const template =
-			`<igx-drop-down #townsPanel maxHeight="300px">
-				<igx-drop-down-item-group *ngFor="let region of towns.regions |
-					$(camelCaseName)RegionContains:townSelected" [label]="region.name">
-					<igx-drop-down-item *ngFor="let town of region.towns | $(camelCaseName)StartsWith:townSelected" [value]="town">
-						{{town}}
-					</igx-drop-down-item>
-				</igx-drop-down-item-group>
-			</igx-drop-down>`;
-		return template;
 	}
 }
 module.exports = new IgxAutocompleteTemplate();
