e6b521375b3b60725e3e8d258c160055a6283b68
NikolayAlipiev <nikolay.alipiev@gmail.com>
2019-03-08 11:31:23 +0200

    fix(autocomplete): fix tests' imports #450

--- templates/angular/igx-ts/autocomplete/autocomplete-extended/files/src/app/__path__/__name__.component.spec.ts
+++ templates/angular/igx-ts/autocomplete/autocomplete-extended/files/src/app/__path__/__name__.component.spec.ts
@@ -1,7 +1,8 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
-import { $(ClassName)Component } from "./$(filePrefix).component";
+import { $(ClassName)Component, $(ClassName)PipeStartsWith, $(ClassName)RegionContains } from "./$(filePrefix).component";
 import { IgxDropDownModule, IgxAutocompleteModule, IgxInputGroupModule, IgxToastModule } from "igniteui-angular";
+import { FormsModule } from '@angular/forms';
 
 describe("$(ClassName)Component", () => {
     let component: $(ClassName)Component;
@@ -9,8 +10,8 @@ describe("$(ClassName)Component", () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [$(ClassName)Component],
-            imports: [IgxDropDownModule, IgxAutocompleteModule, NoopAnimationsModule, IgxInputGroupModule, IgxToastModule]
+            declarations: [$(ClassName)Component, $(ClassName)PipeStartsWith, $(ClassName)RegionContains],
+            imports: [FormsModule, IgxDropDownModule, IgxAutocompleteModule, NoopAnimationsModule, IgxInputGroupModule, IgxToastModule]
         })
             .compileComponents();
     }));
