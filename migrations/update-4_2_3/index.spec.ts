3d3f4689c6869095cb3f4b594a1a7d5bcd4aca87
Damyan Petev <damyan.petev@gmail.com>
2019-07-22 19:38:04 +0300

    chore(migration): Awesome Grid extra paginator style for 4.2.3

---
+++ migrations/update-4_2_3/index.spec.ts
@@ -0,0 +1,73 @@
+import * as path from "path";
+
+// tslint:disable:no-implicit-dependencies
+import { EmptyTree } from "@angular-devkit/schematics";
+// tslint:disable-next-line:no-submodule-imports
+import { SchematicTestRunner, UnitTestTree } from "@angular-devkit/schematics/testing";
+
+describe("Update 4.2.3", () => {
+	let appTree: UnitTestTree;
+	const schematicRunner = new SchematicTestRunner("ig-migrate", path.join(__dirname, "../migration-collection.json"));
+	const configJson = {
+		defaultProject: "testProj",
+		projects: {
+			testProj: {
+				sourceRoot: "/src"
+			}
+		}
+	};
+
+	beforeEach(() => {
+		appTree = new UnitTestTree(new EmptyTree());
+		appTree.create("/angular.json", JSON.stringify(configJson));
+	});
+
+	it("should add Awesome Grid extra paginator style", done => {
+		const stylesFile = "/src/app/awesome-grid/awesome-grid.component.scss";
+		appTree.create(stylesFile,
+`@import '~igniteui-angular/lib/core/styles/themes/index';
+
+:host ::ng-deep {
+
+  .grid__wrapper {
+    @include igx-grid($grid-sample-theme);
+  }
+
+  .igx-paginator>* {
+    margin: 0 .3125rem;
+    display: flex;
+    align-items: center;
+  }
+}
+`
+		);
+
+		schematicRunner.runSchematic("migration-04", {}, appTree);
+		expect(appTree.readContent(stylesFile).replace(/\r\n/g, "\n"))
+			.toEqual(
+`@import '~igniteui-angular/lib/core/styles/themes/index';
+
+:host ::ng-deep {
+
+  .grid__wrapper {
+    @include igx-grid($grid-sample-theme);
+  }
+
+  .igx-paginator {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0.3125rem 0;
+  }
+
+  .igx-paginator>* {
+    margin: 0 .3125rem;
+    display: flex;
+    align-items: center;
+  }
+}
+`.replace(/\r\n/g, "\n")
+			);
+		done();
+	});
+});
