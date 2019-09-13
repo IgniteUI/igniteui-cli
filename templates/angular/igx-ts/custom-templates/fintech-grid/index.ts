a1745a52acdce2ebac8ed348d29048e08f8e3cec
ViktorSlavov <viktorslavov91@gmail.com>
2019-09-13 11:18:15 +0300

    feat(igx-ts): update fintech grid template

--- templates/angular/igx-ts/custom-templates/fintech-grid/index.ts
+++ templates/angular/igx-ts/custom-templates/fintech-grid/index.ts
@@ -10,19 +10,25 @@ class IgxFinTechGridTemplate extends IgniteUIForAngularTemplate {
 		this.name = "FinTech Grid";
 		this.description = "Grid with simulated high-frequency data feed";
 		this.dependencies = [
-			{ import: "IgxGridModule", from: "igniteui-angular" },
+			{ import: "IgxCategoryChartModule", from: "igniteui-angular-charts/ES5/igx-category-chart-module" },
 			{ provide: "IgxExcelExporterService", from: "igniteui-angular" },
 			{
 				import: [
-					"IgxCheckboxModule",
+					"IgxButtonGroupModule",
 					"IgxButtonModule",
+					"IgxDialogModule",
+					"IgxGridModule",
+					"IgxIconModule",
+					"IgxRippleModule",
+					"IgxSliderModule",
 					"IgxSwitchModule",
-					"IgxSliderModule"
+					"IgxToggleModule"
 				],
 				from: "igniteui-angular"
 			},
 			{ import: "FormsModule", from: "@angular/forms" }
 		];
+		this.packages = ["tslib@^1.7.1", "igniteui-angular-core@~8.0.0", "igniteui-angular-charts@~8.0.0"];
 	}
 }
 module.exports = new IgxFinTechGridTemplate();
