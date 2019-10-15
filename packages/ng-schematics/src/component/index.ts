75a9bfd410de1c5ac96ff4605e0f17982aa78597
ViktorSlavov <viktorslavov91@gmail.com>
2019-10-15 17:41:24 +0300

    refactor(schematics): split component in multiple functions, add skipRoute & module support

--- packages/ng-schematics/src/component/index.ts
+++ packages/ng-schematics/src/component/index.ts
@@ -9,77 +9,97 @@ import { SchematicsPromptSession } from "../prompt/SchematicsPromptSession";
 import { SchematicsTemplateManager } from "../SchematicsTemplateManager";
 import { ComponentOptions, TemplateOptions } from "./schema";
 
+function registerInProject(
+	templateOpts: TemplateOptions,
+	skipRoute: boolean,
+	modulePath?: string): Rule {
+	return (host: Tree, _context: SchematicContext) => {
+		templateOpts.templateInst.virtFs = new NgTreeFileSystem(host);
+		templateOpts.templateInst.registerInProject("", templateOpts.name, { skipRoute, modulePath });
+	};
+}
+
+function updatePackageJSON(
+	templateOpts: TemplateOptions,
+	packages = new Map<string, string>(),
+	shouldUpdate = false
+): Rule {
+	return (host: Tree, _context: SchematicContext) => {
+		for (const packageName of templateOpts.templateInst.packages) {
+			const packageMatch = packageName.split(/@(?=[^\/]+$)/);
+			packages.set(packageMatch[0], packageMatch[1] || "*");
+		}
+		if (packages.size) {
+			const packageJSON = JSON.parse((host.read("package.json") || "").toString());
+			packages.forEach((version, name) => {
+				if (!packageJSON.dependencies[name]) {
+					packageJSON.dependencies[name] = version;
+					shouldUpdate = true;
+				}
+			});
+			if (shouldUpdate) {
+				host.overwrite("package.json", JSON.stringify(packageJSON, null, 2));
+			}
+		}
+	};
+}
+
+function addComponent(options: TemplateOptions, skipRoute = false, modulePath?: string): Rule {
+	return async (_tree, _context) => {
+		const config = options.templateInst.generateConfig(options.name, {});
+		return chain([...options.templateInst.templatePaths.map(templatePath =>
+			mergeWith(
+				apply(url(Util.relativePath(__filename, templatePath, true)), [
+					template(config)
+				]
+				), MergeStrategy.Overwrite)
+		), registerInProject(options, skipRoute, modulePath)]);
+	};
+}
+export function singleComponent(
+	templateOptions: TemplateOptions, skipRoute: boolean, missingPackages?: Map<string, string>
+) {
+	return async (_tree: Tree, _context: SchematicContext) => {
+		return chain([
+			addComponent(templateOptions, skipRoute),
+			updatePackageJSON(templateOptions, missingPackages)
+		]);
+	};
+}
 export function component(options: ComponentOptions): Rule {
 	return async (_tree: Tree, context: SchematicContext) => {
 
-		// TODO: validate template name + nameFromPath, id, modulePath, skipRoute
 		const addedComponents: TemplateOptions[] = [];
-		// if schematic was not called by ng-new
-		if (!options.templateInst) {
-			const templateManager = new SchematicsTemplateManager();
-			const projLib = templateManager.getProjectLibrary("angular", "igx-ts");
-			if (!options.template || !options.name) {
-				const prompt = new SchematicsPromptSession(templateManager, addedComponents);
-				await prompt.chooseActionLoop(projLib);
-			} else {
-				if (!projLib.hasTemplate(options.template)) {
-					throw new SchematicsException(`template with id '${options.template}' not found`);
-				}
-				options.templateInst = projLib.getTemplateById(options.template) as IgniteUIForAngularTemplate;
+		const templateManager = new SchematicsTemplateManager();
+		const projLib = templateManager.getProjectLibrary("angular", "igx-ts");
+		let prompt: SchematicsPromptSession;
+		if (!options.template || !options.name) {
+			prompt = new SchematicsPromptSession(templateManager, addedComponents);
+			await prompt.chooseActionLoop(projLib);
+		} else {
+			if (!projLib.hasTemplate(options.template)) {
+				throw new SchematicsException(`template with id '${options.template}' not found`);
 			}
-		}
-		// if schematic was called by ng-new OR both name and template were passed
-		if (options.templateInst) {
+			options.templateInst = projLib.getTemplateById(options.template) as IgniteUIForAngularTemplate;
 			addedComponents.push(options as TemplateOptions);
 		}
-
 		const missingPackages: Map<string, string> = new Map();
+		const shouldUpdate = false;
 		// TODO: reuse component schematic?
 		return chain([
 			...addedComponents.map(templateOpts => {
-				const config = templateOpts.templateInst.generateConfig(templateOpts.name, {});
 				context.logger.info(`Generating ${templateOpts.templateInst.name} with name: ${templateOpts.name}`);
 				return chain([
-					...templateOpts.templateInst.templatePaths.map(templatePath =>
-						mergeWith(
-							apply(url(Util.relativePath(__filename, templatePath, true)), [
-								template(config)
-							]
-							), MergeStrategy.Overwrite)
-					),
-					(host: Tree, _context: SchematicContext) => {
-						if (templateOpts.templateInst) {
-							for (const packageName of templateOpts.templateInst.packages) {
-								const packageMatch = packageName.split(/@(?=[^\/]+$)/);
-								missingPackages.set(packageMatch[0], packageMatch[1] || "*");
-							}
-							templateOpts.templateInst.virtFs = new NgTreeFileSystem(host);
-							templateOpts.templateInst.registerInProject("", templateOpts.name, { skipRoute: options.skipRoute });
-						}
-					}
+					addComponent(templateOpts, options.skipRoute, options.module),
+					updatePackageJSON(templateOpts, missingPackages, shouldUpdate)
 				]);
 			}),
-			(host: Tree, _context: SchematicContext) => {
-				const installChain = [];
-				if (options.skipInstall) {
-					return;
-				}
-				if (missingPackages.size) {
-					const packageJSON = JSON.parse((host.read("package.json") || "").toString());
-					let shouldInstall = false;
-					missingPackages.forEach((version, name) => {
-						if (!packageJSON.dependencies[name]) {
-							packageJSON.dependencies[name] = version;
-							shouldInstall = true;
-						}
-					});
-					if (shouldInstall) {
-						host.overwrite("package.json", JSON.stringify(packageJSON, null, 2));
-						const installTask = context.addTask(new NodePackageInstallTask());
-						installChain.push(installTask);
-					}
-				}
-				context.addTask(new RunSchematicTask("start", {}), installChain);
+			(_host: Tree, _context: SchematicContext) => {
+				if (!prompt) { return; }
+				context.addTask(
+					new RunSchematicTask("start", {}),
+					shouldUpdate ? [_context.addTask(new NodePackageInstallTask())] : []
+				);
 			}
 		]);
 	};
