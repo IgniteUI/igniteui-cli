d781f097c9118419dcda1b4f784548b2981ffb8c
Viktor Slavov <viktorslavov91@gmail.com>
2019-10-18 09:49:20 +0300

    refactor(schematics): split component in multiple functions, add Route & module opts (#575)

--- packages/ng-schematics/src/component/index_spec.ts
+++ packages/ng-schematics/src/component/index_spec.ts
@@ -1,6 +1,7 @@
 import { Tree } from "@angular-devkit/schematics";
 import { SchematicTestRunner } from "@angular-devkit/schematics/testing";
 import * as path from "path";
+import { SchematicsTemplateManager } from "../SchematicsTemplateManager";
 
 const collectionPath = path.join(__dirname, "../collection.json");
 
@@ -13,11 +14,20 @@ describe("component",  () => {
 			registerInProject: jasmine.createSpy(),
 			templatePaths: []
 		};
+		const mockLib = {
+			getTemplateById: jasmine.createSpy().and.returnValue(mockInst),
+			hasTemplate: jasmine.createSpy().and.returnValue(true)
+		};
+		const projLibSpy = spyOn(SchematicsTemplateManager.prototype, "getProjectLibrary");
+		projLibSpy.and.returnValue(mockLib);
 		const tree = runner.runSchematicAsync("component",
 			{ name: "my-combo", template: "combo", templateInst: mockInst, skipRoute: false }, Tree.empty());
 		tree.subscribe(state => {
 			expect(mockInst.generateConfig).toHaveBeenCalledWith("my-combo", {});
-			expect(mockInst.registerInProject).toHaveBeenCalledWith("", "my-combo", { skipRoute: false });
+			expect(mockInst.registerInProject).toHaveBeenCalledWith("", "my-combo", { skipRoute: false, modulePath: undefined });
+			expect(projLibSpy).toHaveBeenCalledWith("angular", "igx-ts");
+			expect(mockLib.hasTemplate).toHaveBeenCalledWith("combo");
+			expect(mockLib.getTemplateById).toHaveBeenCalledWith("combo");
 			expect(state.files).toEqual([]);
 			done();
 		});
