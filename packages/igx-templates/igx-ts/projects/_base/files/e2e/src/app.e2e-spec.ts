428c3bdc0927920f445333df5f107c73c72742ae
ViktorSlavov <viktorslavov91@gmail.com>
2019-11-01 13:13:42 +0200

    chore(schematics): update e2e tests w/ promises

--- packages/igx-templates/igx-ts/projects/_base/files/e2e/src/app.e2e-spec.ts
+++ packages/igx-templates/igx-ts/projects/_base/files/e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser, logging } from 'protractor';
 
 describe('<%=name%> App', () => {
   let page: AppPage;
@@ -15,5 +16,13 @@ describe('<%=name%> App', () => {
   it('should display welcome message', () => {
     page.navigateTo();
     expect(page.getWelcomeText()).toEqual('Welcome to Ignite UI for Angular!');
+  });
+
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
   });
 });
--- packages/igx-templates/igx-ts/projects/_base/files/e2e/src/app.po.ts
+++ packages/igx-templates/igx-ts/projects/_base/files/e2e/src/app.po.ts
@@ -2,14 +2,14 @@ import { browser, by, element } from 'protractor';
 
 export class AppPage {
   navigateTo() {
-    return browser.get('/');
+    return browser.get(browser.baseUrl) as Promise<any>;
   }
 
   getNavText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('app-root h1')).getText() as Promise<string>;
   }
 
   getWelcomeText() {
-    return element(by.css('app-home h1')).getText();
+    return element(by.css('app-home h1')).getText() as Promise<string>;
   }
 }
