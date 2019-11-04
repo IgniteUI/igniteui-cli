1654b561f7104e5d5fd3aa9d2a3eb10291c86d33
plamenamiteva <plamenamiteva@abv.bg>
2019-11-04 15:59:06 +0200

    chore(cli): change message text #580

--- packages/cli/lib/commands/add.ts
+++ packages/cli/lib/commands/add.ts
@@ -83,7 +83,7 @@ command = {
 		}
 
 		if (framework.id === "angular" && frameworkLibrary.projectType === "igx-ts") {
-			Util.warn("Psst! Did you know you can also use our schematics package to modify your Angular project directly?", "yellow");
+			Util.warn("Psst! Did you know you can also use our schematics package to modify your Angular project directly, using the ng cli?", "yellow");
 			Util.warn("Read more at: https://www.infragistics.com/products/ignite-ui-angular/angular/components/general/getting_started.html.", "yellow");
 		}
 
