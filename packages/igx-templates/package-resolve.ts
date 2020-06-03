3ab63f9104faaaebd2c32e0ae1d24fbe05188b87
Viktor Slavov <viktorslavov91@gmail.com>
2020-06-03 14:47:24 +0300

    refactor(upgrade): handle multiple licensed packages, #741 (#743)

--- packages/igx-templates/package-resolve.ts
+++ packages/igx-templates/package-resolve.ts
@@ -5,7 +5,24 @@ export const FEED_PACKAGE = "@infragistics/igniteui-angular";
 export const NPM_DOCK_MANAGER = "igniteui-dockmanager";
 export const FEED_DOCK_MANAGER = "@infragistics/igniteui-dockmanager";
 
-export function resolvePackage() {
+export interface PackageDefinition {
+	trial: string;
+	licensed: string;
+}
+
+// packages that have both a trial AND licensed definition
+export const UPGRADEABLE_PACKAGES: PackageDefinition[] = [
+	{
+		trial: NPM_PACKAGE,
+		licensed: FEED_PACKAGE
+	},
+	{
+		trial: NPM_DOCK_MANAGER,
+		licensed: FEED_DOCK_MANAGER
+	}
+];
+
+export function resolveIgxPackage() {
 	const fs = App.container.get<IFileSystem>(FS_TOKEN);
 
 	// read project package JSON
@@ -18,4 +35,20 @@ export function resolvePackage() {
 	}
 
 	return NPM_PACKAGE;
+}
+
+export function getUpgradeablePackages(): PackageDefinition[] {
+	const fs = App.container.get<IFileSystem>(FS_TOKEN);
+	const upgradeable: PackageDefinition[] = [];
+
+	if (fs.fileExists("./package.json")) {
+		const packageJson = JSON.parse(fs.readFile("./package.json"));
+		const dependencies = packageJson["dependencies"];
+		for (const packageEntry of UPGRADEABLE_PACKAGES) {
+			if (dependencies[packageEntry.trial]) {
+				upgradeable.push(packageEntry);
+			}
+		}
+	}
+	return upgradeable;
 }
