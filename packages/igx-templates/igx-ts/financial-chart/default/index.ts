d064dff9238801485db56d329a5d410f3710f2a0
valadzhov <valadzhov@infragistics.com>
2020-10-08 15:33:22 +0300

    chore(dv-components): Update to 10.1.3. #768

--- packages/igx-templates/igx-ts/financial-chart/default/index.ts
+++ packages/igx-templates/igx-ts/financial-chart/default/index.ts
@@ -13,7 +13,7 @@ class IgxFinancialChartTemplate extends IgniteUIForAngularTemplate {
 		this.dependencies = [
 			{ import: "IgxFinancialChartModule", from: "igniteui-angular-charts" }
 		];
-		this.packages = ["igniteui-angular-core@~9.1.2", "igniteui-angular-charts@~9.1.2"];
+		this.packages = ["igniteui-angular-core@~10.1.3", "igniteui-angular-charts@~10.1.3"];
 	}
 }
 module.exports = new IgxFinancialChartTemplate();
