5ac63d42c17d4d8d53398fd010668be368e0c1f1
IvayloG <inj.iganchev@gmail.com>
2020-10-09 10:15:04 +0300

    chore(deploy): Address comments

--- packages/igx-templates/igx-ts/projects/_base/files/src/app/error-routing/error-routing.module.ts
+++ packages/igx-templates/igx-ts/projects/_base/files/src/app/error-routing/error-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, ErrorHandler, Provider, isDevMode } from '@angular/core';
+import { NgModule, ErrorHandler, Provider } from '@angular/core';
 import { PageNotFoundComponent } from './not-found/not-found.component';
 import { UncaughtErrorComponent } from './error/uncaught-error.component';
 import { GlobalErrorHandlerService } from './error/global-error-handler.service';
