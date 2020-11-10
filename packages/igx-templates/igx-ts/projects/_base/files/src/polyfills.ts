7a7c2f316b523b4057f5912360f736e050496fd2
NikolayAlipiev <nikolay.alipiev@gmail.com>
2020-11-10 14:26:40 +0200

    chore(igx-ts, ig-ts): add core-js module and fix path

--- packages/igx-templates/igx-ts/projects/_base/files/src/polyfills.ts
+++ packages/igx-templates/igx-ts/projects/_base/files/src/polyfills.ts
@@ -71,3 +71,5 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * Need to import at least one locale-data with intl.
  */
 // import 'intl/locale-data/jsonp/en';
+
+import 'core-js/es/array'; // for Array.includes()
