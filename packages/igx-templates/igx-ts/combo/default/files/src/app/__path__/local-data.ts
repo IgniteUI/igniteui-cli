fdecfffc948ed619be566d44d6b1871027f57d2b
wnvko <milko.venkov@gmail.com>
2021-07-27 12:04:05 +0300

    refactor: fix combo templates

--- packages/igx-templates/igx-ts/combo/default/files/src/app/__path__/local-data.ts
+++ packages/igx-templates/igx-ts/combo/default/files/src/app/__path__/local-data.ts
@@ -1,33 +1,38 @@
-const division: { [key: string]: string[] } = {
-    'New England 01': ['Connecticut', 'Maine', 'Massachusetts'],
-    'New England 02': ['New Hampshire', 'Rhode Island', 'Vermont'],
-    'Mid-Atlantic': ['New Jersey', 'New York', 'Pennsylvania'],
-    'East North Central 02': ['Michigan', 'Ohio', 'Wisconsin'],
-    'East North Central 01': ['Illinois', 'Indiana'],
-    'West North Central 01': ['Missouri', 'Nebraska', 'North Dakota', 'South Dakota'],
-    'West North Central 02': ['Iowa', 'Kansas', 'Minnesota'],
-    'South Atlantic 01': ['Delaware', 'Florida', 'Georgia', 'Maryland'],
-    'South Atlantic 02': ['North Carolina', 'South Carolina', 'Virginia',
-        'District of Columbia', 'West Virginia'],
-    'South Atlantic 03': ['District of Columbia', 'West Virginia'],
-    'East South Central 01': ['Alabama', 'Kentucky'],
-    'East South Central 02': ['Mississippi', 'Tennessee'],
-    'West South Central': ['Arkansas', 'Louisiana', 'Oklahoma', 'Texas'],
-    // tslint:disable-next-line:object-literal-key-quotes
-    'Mountain': ['Arizona', 'Colorado', 'Idaho', 'Montana', 'Nevada', 'New Mexico', 'Utah', 'Wyoming'],
-    'Pacific 01': ['Alaska', 'California'],
-    'Pacific 02': ['Hawaii', 'Oregon', 'Washington']
-};
-
-const localData: any[] = [];
-const keys = Object.keys(division);
-for (const key of keys) {
-    division[key].map((e) => {
-        localData.push({
-            field: e,
-            region: key.substring(0, key.length - 3)
-        });
-    });
+interface Division {
+    name: string;
+    states: string[];
 }
+
+export interface State {
+    name: string;
+    region: string;
+}
+
+const divisions: Division[] = [
+    { name: 'New England 01', states: ['Connecticut', 'Maine', 'Massachusetts'] },
+    { name: 'New England 02', states: ['New Hampshire', 'Rhode Island', 'Vermont'] },
+    { name: 'Mid-Atlantic', states: ['New Jersey', 'New York', 'Pennsylvania'] },
+    { name: 'East North Central 02', states: ['Michigan', 'Ohio', 'Wisconsin'] },
+    { name: 'East North Central 01', states: ['Illinois', 'Indiana'] },
+    { name: 'West North Central 01', states: ['Missouri', 'Nebraska', 'North Dakota', 'South Dakota'] },
+    { name: 'West North Central 02', states: ['Iowa', 'Kansas', 'Minnesota'] },
+    { name: 'South Atlantic 01', states: ['Delaware', 'Florida', 'Georgia', 'Maryland'] },
+    { name: 'South Atlantic 02', states: ['North Carolina', 'South Carolina', 'Virginia', 'District of Columbia', 'West Virginia'] },
+    { name: 'South Atlantic 03', states: ['District of Columbia', 'West Virginia'] },
+    { name: 'East South Central 01', states: ['Alabama', 'Kentucky'] },
+    { name: 'East South Central 02', states: ['Mississippi', 'Tennessee'] },
+    { name: 'West South Central', states: ['Arkansas', 'Louisiana', 'Oklahoma', 'Texas'] },
+    { name: 'Mountain', states: ['Arizona', 'Colorado', 'Idaho', 'Montana', 'Nevada', 'New Mexico', 'Utah', 'Wyoming'] },
+    { name: 'Pacific 01', states: ['Alaska', 'California'] },
+    { name: 'Pacific 02', states: ['Hawaii', 'Oregon', 'Washington'] }
+];
+
+const localData: State[] = [];
+divisions
+    .map(d => d.states
+        .map(s => localData.push({
+            name: s,
+            region: d.name.replace(/[\d]+/, '').trim()
+        })));
 
 export { localData };
