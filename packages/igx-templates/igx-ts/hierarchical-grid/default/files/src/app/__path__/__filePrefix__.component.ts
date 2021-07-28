fa8a114ca5c35f89e02aae9949457da07e4a3876
ViktorSlavov <viktorslavov91@gmail.com>
2021-07-28 11:22:11 +0300

    chore(igx-templates): update hgrid templates

--- packages/igx-templates/igx-ts/hierarchical-grid/default/files/src/app/__path__/__filePrefix__.component.ts
+++ packages/igx-templates/igx-ts/hierarchical-grid/default/files/src/app/__path__/__filePrefix__.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { SINGERS } from './data';
+import { Component } from '@angular/core';
+import { ARTISTS, Artist } from './data';
 
 @Component({
   selector: 'app-<%=filePrefix%>',
   templateUrl: './<%=filePrefix%>.component.html',
   styleUrls: ['./<%=filePrefix%>.component.scss']
 })
-export class <%=ClassName%>Component implements OnInit {
-  public localData: any[] = [];
-
-  public ngOnInit(): void {
-    this.localData = SINGERS;
-  }
+export class <%=ClassName%>Component {
+  public localData: Artist[] = SINGERS;
 }
