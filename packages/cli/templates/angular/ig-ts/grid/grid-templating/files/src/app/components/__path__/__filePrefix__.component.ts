b770d4df3b1fe910ab9b21bc4b3d66cbd06c0547
iganchev <iganchev@infragistics.com>
2021-07-30 12:06:56 +0300

    chore(igx-ts): Update routerLing bindings
    fix(ig-ts): Fix proj navigation
    
    fix(ig-ts): Template syntax -  build errors

--- packages/cli/templates/angular/ig-ts/grid/grid-templating/files/src/app/components/__path__/__filePrefix__.component.ts
+++ packages/cli/templates/angular/ig-ts/grid/grid-templating/files/src/app/components/__path__/__filePrefix__.component.ts
@@ -23,7 +23,7 @@ export class $(ClassName)Component {
 			dataSourceType: "json",
 			columns: [
 				{ headerText: "Product Name", key: "ProductName", width: 300 },
-				{ headerText: "Unit Price", key: "UnitPrice", type: 'number', width: 150, template: "$ ${UnitPrice} {{if parseInt(${UnitPrice}) >= 19 }}<img width="10" height="15" src= 'https://www.igniteui.com/images/samples/templating-engine/colTemplateWithConditionalCell/arrowUp.gif' />	{{else}} <img width="10" height="15" src= 'https://www.igniteui.com/images/samples/templating-engine/colTemplateWithConditionalCell/arrowDown.gif' /> {{/if}}" },
+				{ headerText: "Unit Price", key: "UnitPrice", type: 'number', width: 150, template: "$ ${UnitPrice} {{if parseInt(${UnitPrice}) >= 19 }}<img width='10' height='15' src= 'https://www.igniteui.com/images/samples/templating-engine/colTemplateWithConditionalCell/arrowUp.gif' />	{{else}} <img width='10' height='15' src= 'https://www.igniteui.com/images/samples/templating-engine/colTemplateWithConditionalCell/arrowDown.gif' /> {{/if}}" },
 				{ headerText: "Units In Stock", key: "UnitsInStock", width: 125 },
 				{ headerText: "Units On Order", key: "UnitsOnOrder", width: 125 },
 				{ headerText: " ", key: "DeltaPrice", hidden: true }
