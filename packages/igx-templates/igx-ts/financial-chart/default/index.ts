5da9cc212e2c3565611880155a1b40f67ccca17f
plamenamiteva <plamenamiteva@abv.bg>
2021-08-30 17:40:12 +0300

    chore: update dv packages#859

--- packages/igx-templates/igx-ts/financial-chart/default/index.ts
+++ packages/igx-templates/igx-ts/financial-chart/default/index.ts
@@ -12,7 +12,7 @@ class IgxFinancialChartTemplate extends IgniteUIForAngularTemplate {
 		this.dependencies = [
 			{ import: "IgxFinancialChartModule", from: "igniteui-angular-charts" }
 		];
-		this.packages = ["igniteui-angular-core@~11.2.1", "igniteui-angular-charts@~11.2.1"];
+		this.packages = ["igniteui-angular-core@~12.1.0", "igniteui-angular-charts@~12.1.0"];
 	}
 }
 module.exports = new IgxFinancialChartTemplate();
