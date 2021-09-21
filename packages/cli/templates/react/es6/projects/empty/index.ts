f650597c4001ecff4a136ad68a6b64e686d12151
iganchev <iganchev@infragistics.com>
2021-09-21 12:43:33 +0300

    feat(ig-es6): Update project igniteui dependencies
    - Use igniteuiResources.js file

--- packages/cli/templates/react/es6/projects/empty/index.ts
+++ packages/cli/templates/react/es6/projects/empty/index.ts
@@ -21,19 +21,6 @@ class EmptyProject implements ProjectTemplate {
 		throw new Error("Method not implemented.");
 	}
 	public async upgradeIgniteUIPackages(projectPath: string, packagePath: string): Promise<boolean> {
-		// HERE IT USED TO REPLACE THE FREE WITH FULL IGNITEUI PACKAGES< MODING THE PATHS inside the WEBPACK.config.ts
-		// TODO: TEST THIS FUNCTIONALITY AND FIX PACKAGES PATHS AND REGISTERING UPDATED PACKAGE
-		// const filePath = path.join(projectPath, this._updateFile);
-		// let configFile = fs.readFileSync(filePath, "utf8");
-		// //comment out core + lob redirects
-		// configFile = configFile.replace(`"ignite-ui/js/infragistics.core.js$"`, `//$&`);
-		// configFile = configFile.replace(`"ignite-ui/js/infragistics.lob.js$"`, `//$&`);
-		// fs.writeFileSync(filePath, configFile);
-		console.log("PASS DUMMY upgradeIgniteUIPackages"); // AT THIS POINT $(igniteImports) is still not replaced in App.js
-
-		// TODO Obtain App.js and update the string where required
-		// 1) ignite-ui/  replace with @infragistics/ignite-ui-full/en/
-		// 2) -lite replace with ""
 		return true;
 	}
 	public generateConfig(name: string, theme: string, ...options: any[]): { [key: string]: any } {
