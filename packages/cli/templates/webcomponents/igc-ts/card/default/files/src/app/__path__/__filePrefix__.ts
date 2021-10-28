13f55a058ffd3aaf242da3a1eeab7336182a064c
valadzhov <valadzhov@infragistics.com>
2021-10-28 15:24:53 +0300

    Card component initial commit.

---
+++ packages/cli/templates/webcomponents/igc-ts/card/default/files/src/app/__path__/__filePrefix__.ts
@@ -0,0 +1,15 @@
+import { html, LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import 'igniteui-webcomponents';
+
+@customElement('lit-$(path)')
+export default class $(ClassName) extends LitElement {
+  render() {
+    return html`
+        <igc-button
+            size='large'>
+            Large button
+        </igc-button>
+    `;
+  }
+}
