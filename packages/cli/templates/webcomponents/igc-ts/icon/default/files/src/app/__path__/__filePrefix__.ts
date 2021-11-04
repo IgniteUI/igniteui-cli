868bb55c843956311d0fd9e5cc18d457584989ad
Alexander M <niron_hsc@yahoo.com>
2021-11-04 14:48:34 +0200

    Improving icon and icon-button views

--- packages/cli/templates/webcomponents/igc-ts/icon/default/files/src/app/__path__/__filePrefix__.ts
+++ packages/cli/templates/webcomponents/igc-ts/icon/default/files/src/app/__path__/__filePrefix__.ts
@@ -1,12 +1,44 @@
-import 'igniteui-webcomponents';
+import "igniteui-webcomponents/src/components/icon/icon";
+import {
+  registerIcon,
+  registerIconFromText,
+  } from "igniteui-webcomponents/src/components/icon/icon.registry";
 
-export default class $(ClassName) extends HTMLElement {
+const searchIcon =
+'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>';
+registerIconFromText('search', searchIcon, 'material');
+
+registerIcon(
+  "build",
+  "https://unpkg.com/material-design-icons@3.0.1/action/svg/production/ic_build_24px.svg",
+  "material"
+  );
+
+export default class Icon extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <igc-icon name="github" slot="suffix">
-      </igc-icon>
+      <div>
+        <label>Search icon: </label>
+        <igc-icon
+          name='search'
+          collection='material'
+          size='large'
+          mirrored = false
+        >
+        </igc-icon>
+      </div>
+      <div>
+        <label>Build icon: </label>
+        <igc-icon
+          name='build'
+          collection='material'
+          size='large'
+          mirrored = false
+        >
+        </igc-icon>
+      </div>
     `;
   }
 }
 
-customElements.define('app-$(path)', $(ClassName));
+customElements.define('app-icon', Icon);
