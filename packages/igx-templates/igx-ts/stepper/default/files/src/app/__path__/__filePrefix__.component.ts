d32c0d61cd2d5b1e75b0d9506b5ecabfd5dcb883
Teodosia Hristodorova <52423497+teodosiah@users.noreply.github.com>
2021-11-09 22:29:40 +0200

    feat(igx-templates): add igx-stepper template (#927)

---
+++ packages/igx-templates/igx-ts/stepper/default/files/src/app/__path__/__filePrefix__.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-<%=filePrefix%>',
+	templateUrl: './<%=filePrefix%>.component.html',
+	styleUrls: ['./<%=filePrefix%>.component.scss']
+})
+export class <%=ClassName%>Component {
+    public orientation = 'horizontal';
+    public stepperOrientations: any[] = [
+		{
+			label: 'Horizontal', orientation: 'horizontal',
+			selected: this.orientation === 'horizontal', togglable: true
+		},
+		{
+			label: 'Vertical', orientation: 'vertical',
+			selected: this.orientation === 'vertical', togglable: true
+		}
+	];
+
+    public toggleOrientation(event): void {
+		this.orientation = this.stepperOrientations[event.index].orientation;
+	}
+}
