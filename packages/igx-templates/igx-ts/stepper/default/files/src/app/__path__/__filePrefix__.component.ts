6a288865093be9ff8e019aef7a70ed6e49448365
iganchev <iganchev@infragistics.com>
2021-11-10 10:59:29 +0200

    chore(lint): Format stepper component files

--- packages/igx-templates/igx-ts/stepper/default/files/src/app/__path__/__filePrefix__.component.ts
+++ packages/igx-templates/igx-ts/stepper/default/files/src/app/__path__/__filePrefix__.component.ts
@@ -1,24 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector: 'app-<%=filePrefix%>',
-	templateUrl: './<%=filePrefix%>.component.html',
-	styleUrls: ['./<%=filePrefix%>.component.scss']
+  selector: 'app-<%=filePrefix%>',
+  templateUrl: './<%=filePrefix%>.component.html',
+  styleUrls: ['./<%=filePrefix%>.component.scss']
 })
-export class <%=ClassName%>Component {
+export class <%=ClassName%> Component {
     public orientation = 'horizontal';
     public stepperOrientations: any[] = [
-		{
-			label: 'Horizontal', orientation: 'horizontal',
-			selected: this.orientation === 'horizontal', togglable: true
-		},
-		{
-			label: 'Vertical', orientation: 'vertical',
-			selected: this.orientation === 'vertical', togglable: true
-		}
-	];
+    {
+      label: 'Horizontal', orientation: 'horizontal',
+      selected: this.orientation === 'horizontal', togglable: true
+    },
+    {
+      label: 'Vertical', orientation: 'vertical',
+      selected: this.orientation === 'vertical', togglable: true
+    }
+  ];
 
     public toggleOrientation(event): void {
-		this.orientation = this.stepperOrientations[event.index].orientation;
-	}
+    this.orientation = this.stepperOrientations[event.index].orientation;
+  }
 }
