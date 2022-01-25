bd0c681c29aae5192d6a3cd9d0fa68913e4e6824
Viktor Aladzhov <valadzhov@infragistics.com>
2022-01-25 19:28:21 +0200

    Adding Slider web component (#986)
    * Adding Slider component

---
+++ packages/cli/templates/webcomponents/igc-ts/slider/index.ts
@@ -0,0 +1,13 @@
+import { BaseComponent } from "@igniteui/cli-core";
+class IgcSliderComponent extends BaseComponent {
+	/**
+	 *
+	 */
+	constructor() {
+		super(__dirname);
+		this.name  = "Slider";
+		this.group = "Interactions";
+		this.description = `Basic slider component`;
+	}
+}
+module.exports = new IgcSliderComponent();
