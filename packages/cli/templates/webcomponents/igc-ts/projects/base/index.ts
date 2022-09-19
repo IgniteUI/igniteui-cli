7574ab2b0221687a6ace6f627ec55caa3945236f
Milko Venkov <milko.venkov@gmail.com>
2022-09-20 00:50:39 +0300

    feat(wc): add base template for Web Components #988 (#1058)

--- packages/cli/templates/webcomponents/igc-ts/projects/base/index.ts
+++ packages/cli/templates/webcomponents/igc-ts/projects/base/index.ts
@@ -1,11 +1,11 @@
 import { ProjectTemplate } from "@igniteui/cli-core";
 import * as path from "path";
-import { BaseWithHomeIgcProject } from "../_base_with_home";
+import { BaseIgcProject } from "../_base";
 
-export class EmptyPageTemplate extends BaseWithHomeIgcProject implements ProjectTemplate {
-	public id: string = "empty";
-	public name = "Empty Project";
-	public description = "Project structure with routing and a home page";
+export class BasePageTemplate extends BaseIgcProject implements ProjectTemplate {
+	public id: string = "base";
+	public name = "Base Project";
+	public description = "Project structure with routing";
 	public framework: string = "webcomponents";
 	public projectType: string = "igc-ts";
 	public dependencies: string[];
@@ -15,4 +15,4 @@ export class EmptyPageTemplate extends BaseWithHomeIgcProject implements Project
 		return [...super.templatePaths, path.join(__dirname, "files")];
 	}
 }
-export default new EmptyPageTemplate();
+export default new BasePageTemplate();
