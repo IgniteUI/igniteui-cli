92e3ac11a2d49332b8a852def8e93e70acb36fda
IBarakov <ibarakov@infragistics.com>
2023-04-21 14:32:49 +0300

    add base template for angular and add placeholder in base wc yml

---
+++ packages/igx-templates/igx-ts/projects/base/index.ts
@@ -0,0 +1,18 @@
+import { ProjectTemplate } from "@igniteui/cli-core";
+import * as path from "path";
+import { BaseIgxProject } from "../_base";
+
+export class BasePageTemplate extends BaseIgxProject implements ProjectTemplate {
+	public id: string = "base";
+	public name = "Base Project";
+	public description = "Project structure with routing";
+	public dependencies: string[] = [];
+	public framework: string = "angular";
+	public projectType: string = "igx-ts";
+	public hasExtraConfiguration: boolean = false;
+
+	public get templatePaths(): string[] {
+		return [...super.templatePaths, path.join(__dirname, "files")];
+	}
+}
+export default new BasePageTemplate();
