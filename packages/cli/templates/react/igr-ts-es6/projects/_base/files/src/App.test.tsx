53b96188f24231c73cdb665202c9af167fa17997
Maria Tsvyatkova <mtsvyatkova@infragistics.com>
2023-09-15 02:15:44 +0300

    fix(igr-ts-es6): configure tests

--- packages/cli/templates/react/igr-ts-es6/projects/_base/files/src/App.test.tsx
+++ packages/cli/templates/react/igr-ts-es6/projects/_base/files/src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders without crashing', () => {
+    render(<App />);
+    const home = screen.getByRole('link');
+    expect(home).toBeTruthy();
 });
--- packages/cli/templates/react/igr-ts-es6/projects/_base/files/vite.config.ts
+++ packages/cli/templates/react/igr-ts-es6/projects/_base/files/vite.config.ts
@@ -1,7 +1,16 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+	globals: true,
+	environment: 'jsdom',
+	setupFiles: ['./src/setupTests.ts']
+  },
+  resolve: {
+	mainFields: ['module'],
+  }
 })
