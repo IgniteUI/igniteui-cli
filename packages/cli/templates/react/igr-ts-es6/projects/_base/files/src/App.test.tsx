66dcf4bd7702d7be5462d73a1767ecd468a400f5
Maria Tsvyatkova <mtsvyatkova@infragistics.com>
2023-09-18 18:29:02 +0300

    chore(*): make app test more generic

--- packages/cli/templates/react/igr-ts-es6/projects/_base/files/src/App.test.tsx
+++ packages/cli/templates/react/igr-ts-es6/projects/_base/files/src/App.test.tsx
@@ -1,9 +1,8 @@
 import { expect, test } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders without crashing', () => {
-    render(<App />);
-    const home = screen.getByRole('link');
-    expect(home).toBeTruthy();
+    const wrapper = render(<App />);
+    expect(wrapper).toBeTruthy();
 });
