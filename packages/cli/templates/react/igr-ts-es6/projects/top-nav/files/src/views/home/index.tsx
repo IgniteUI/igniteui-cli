f22c14dc60ca436b084e2bf9f097ff0848f80d21
IBarakov <ibarakov@infragistics.com>
2023-09-20 15:00:20 +0300

    feat(igr-ts-es6): add view to top-nav

---
+++ packages/cli/templates/react/igr-ts-es6/projects/top-nav/files/src/views/home/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import styles from './style.module.css';
+
+export default function Home() {
+	return (
+		<header className={styles.header}>
+			<img src={logo} className={styles.logo} alt="logo" />
+			<p>
+			Welcome to Ignite UI for React!
+			</p>
+			<a
+			className={styles.link}
+			href="https://www.infragistics.com/products/ignite-ui-react"
+			target="_blank"
+			rel="noopener noreferrer"
+			>
+			Learn More
+			</a>
+		</header>
+	)
+}
