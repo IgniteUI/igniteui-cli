7f6d432847849c3b6df8e076a76afca11023cc81
IBarakov <ibarakov@infragistics.com>
2023-09-27 14:30:38 +0300

    feat(react): add base template

---
+++ packages/cli/templates/react/igr-ts-es6/projects/_base/files/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
+
+export default function App() {
+
+	return (
+    <div className="app">
+      <Router></Router>
+    </div>
+	)
+}
