0c1564cab8976d7a62ef0734a8bef82e96474f38
IBarakov <ibarakov@infragistics.com>
2023-09-29 12:05:12 +0300

    chore: formatting

--- packages/cli/templates/react/igr-ts/projects/_base_with_home/files/src/App.tsx
+++ packages/cli/templates/react/igr-ts/projects/_base_with_home/files/src/App.tsx
@@ -7,20 +7,16 @@ export default function App() {
 	return (
     <div className="app">
 			<div className={styles.app__name}>{name}</div>
-		<header className={styles.header}>
-			<img src={logo} className={styles.logo} alt="logo" />
-			<p>
-			Welcome to Ignite UI for React!
-			</p>
-			<a
-			className={styles.link}
-			href="https://www.infragistics.com/products/ignite-ui-react"
-			target="_blank"
-			rel="noopener noreferrer"
-			>
-			Learn More
-			</a>
-		</header>
+      <header className={styles.header}>
+        <img src={logo} className={styles.logo} alt="logo" />
+        <p>Welcome to Ignite UI for React!</p>
+        <a className={styles.link}
+          href="https://www.infragistics.com/products/ignite-ui-react"
+          target="_blank"
+          rel="noopener noreferrer">
+          Learn More
+        </a>
+      </header>
     </div>
 	)
 }
--- packages/cli/templates/react/igr-ts/projects/top-nav/files/src/views/home/index.tsx
+++ packages/cli/templates/react/igr-ts/projects/top-nav/files/src/views/home/index.tsx
@@ -4,19 +4,15 @@ import styles from './style.module.css';
 
 export default function Home() {
 	return (
-		<header className={styles.header}>
-			<img src={logo} className={styles.logo} alt="logo" />
-			<p>
-			Welcome to Ignite UI for React!
-			</p>
-			<a
-			className={styles.link}
-			href="https://www.infragistics.com/products/ignite-ui-react"
-			target="_blank"
-			rel="noopener noreferrer"
-			>
-			Learn More
-			</a>
-		</header>
+      <header className={styles.header}>
+        <img src={logo} className={styles.logo} alt="logo" />
+        <p>Welcome to Ignite UI for React!</p>
+        <a className={styles.link}
+          href="https://www.infragistics.com/products/ignite-ui-react"
+          target="_blank"
+          rel="noopener noreferrer">
+          Learn More
+        </a>
+      </header>
 	)
 }
