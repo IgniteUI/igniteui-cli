33bc0bec35a78ea97ed49415ed07fbdb74f91688
IBarakov <ibarakov@infragistics.com>
2023-09-29 11:56:01 +0300

    chore: remove serviceWorker

--- packages/cli/templates/react/igr-ts/projects/_base/files/src/main.tsx
+++ packages/cli/templates/react/igr-ts/projects/_base/files/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import 'react-app-polyfill/ie11';
 
 /** Required in IE11 for Charts */
@@ -16,8 +15,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
