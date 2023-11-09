a7c5afaa108692ecbd8a933a0bd4b52fb525b45e
Viktor Aladzhov <valadzhov@infragistics.com>
2023-11-09 15:58:49 +0200

    CLI templates update for Angular 17 (#1169)

--- packages/igx-templates/igx-ts/custom-templates/crm-grid/index.ts
+++ packages/igx-templates/igx-ts/custom-templates/crm-grid/index.ts
@@ -41,7 +41,7 @@ class IgxGridCRMTemplate extends IgniteUIForAngularTemplate {
 			},
 			{ import: "FormsModule", from: "@angular/forms" }
 		];
-		this.packages = ["igniteui-angular-core@~16.0.0", "igniteui-angular-charts@~16.0.0"];
+		this.packages = ["igniteui-angular-core@~17.0.0-beta.0", "igniteui-angular-charts@~17.0.0-beta.0"];
 	}
 }
 module.exports = new IgxGridCRMTemplate();
