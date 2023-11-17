f34ea271f8128c0b070c6119b3b50c65ffd9e1ff
jackofdiamond5 <jackofdiamond596@gmail.com>
2023-11-17 16:35:30 +0200

    refactor(projects/_base_with_home): update to standalone components

--- packages/igx-templates/igx-ts/projects/_base_with_home/files/src/app/home/home.component.ts
+++ packages/igx-templates/igx-ts/projects/_base_with_home/files/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
