87316999a18baca69a58c18796e4bf52d21c8546
jackofdiamond5 <jackofdiamond596@gmail.com>
2023-11-20 15:44:55 +0200

    refactor(projects/_base): animations & gestures for standalone components

--- packages/igx-templates/igx-ts/projects/_base/files/src/app/app.config.ts
+++ packages/igx-templates/igx-ts/projects/_base/files/src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+	provideRouter(routes),
+	provideAnimations(),
+	importProvidersFrom(BrowserModule, HammerModule),
+	// provide the HAMMER_GESTURE_CONFIG token after the above line 
+	// to override the default settings of the HammerModule
+    // { provide: HAMMER_GESTURE_CONFIG, useClass: MyHammerConfig }
+  ]
 };
--- packages/igx-templates/igx-ts/projects/_base/files/src/main.ts
+++ packages/igx-templates/igx-ts/projects/_base/files/src/main.ts
@@ -1,5 +1,5 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
 
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
@@ -9,5 +9,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppComponent, appConfig)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
