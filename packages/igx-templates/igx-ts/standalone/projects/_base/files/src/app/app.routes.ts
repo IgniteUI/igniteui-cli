43310868c7eefd55299115db977ac0d3af880928
jackofdiamond5 <jackofdiamond596@gmail.com>
2023-11-24 15:15:14 +0200

    refactor(projects/_base): remove routing module

--- packages/igx-templates/igx-ts/standalone/projects/_base/files/src/app/app.routes.ts
+++ packages/igx-templates/igx-ts/standalone/projects/_base/files/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { PageNotFoundComponent } from './error-routing/not-found/not-found.component';
+import { UncaughtErrorComponent } from './error-routing/error/uncaught-error.component';
+
+export const routes: Routes = [
+	{ path: 'error', component: UncaughtErrorComponent },
+	{ path: '**', component: PageNotFoundComponent } // must always be last
+];
