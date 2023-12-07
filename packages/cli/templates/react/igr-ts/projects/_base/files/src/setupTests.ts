849d40cf7d0b457962055a1820f345a57d318358
onlyexeption <ibarakov@infragistics.com>
2023-12-07 14:35:13 +0200

    chore: fix indentation

--- packages/cli/templates/react/igr-ts/projects/_base/files/src/setupTests.ts
+++ packages/cli/templates/react/igr-ts/projects/_base/files/src/setupTests.ts
@@ -2,4 +2,4 @@ import '@testing-library/jest-dom'
 import 'vitest-canvas-mock'
 import ResizeObserver from 'resize-observer-polyfill'
 
-global.ResizeObserver = ResizeObserver
+global.ResizeObserver = ResizeObserver;
--- packages/cli/templates/react/igr-ts/projects/_base_with_home/files/src/app/home/home.tsx
+++ packages/cli/templates/react/igr-ts/projects/_base_with_home/files/src/app/home/home.tsx
@@ -2,23 +2,23 @@ import logo from "/logo.svg";
 import styles from "./style.module.css";
 
 export default function App() {
-  const name = "$(name)";
+    const name = '$(name)';
 
-  return (
-    <div className="app">
-      <div className={styles.app__name}>{name}</div>
-      <header className={styles.header}>
-        <img src={logo} className={styles.logo} alt="logo" />
-        <p>Welcome to Ignite UI for React!</p>
-        <a
-          className={styles.link}
-          href="https://www.infragistics.com/products/ignite-ui-react"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn More
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <div className={styles.app__name}>{name}</div>
+            <header className={styles.header}>
+                <img src={logo} className={styles.logo} alt="logo" />
+                <p>Welcome to Ignite UI for React!</p>
+                <a
+                    className={styles.link}
+                    href="https://www.infragistics.com/products/ignite-ui-react"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn More
+                </a>
+            </header>
+        </div>
+    );
 }
