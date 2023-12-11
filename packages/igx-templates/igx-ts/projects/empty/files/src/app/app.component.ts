c2ba933ff5baa02517e85564f00fe5d4dd3c78e2
jackofdiamond5 <jackofdiamond596@gmail.com>
2023-12-11 09:28:36 +0200

    refactor(projects): add router outlet to empty project

--- packages/igx-templates/igx-ts/projects/empty/files/src/app/app.component.ts
+++ packages/igx-templates/igx-ts/projects/empty/files/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [RouterOutlet]
 })
 export class AppComponent {
   title = 'Home - IgniteUI for Angular';
