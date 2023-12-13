6b8d344d513d570d2563740cd41ef6ab25a9304c
jackofdiamond5 <jackofdiamond596@gmail.com>
2023-12-13 12:22:09 +0200

    feat(cli;schematics): update prompts, add launch configs

--- packages/ng-schematics/src/migrations/update-1/index.spec.ts
+++ packages/ng-schematics/src/migrations/update-1/index.spec.ts
@@ -12,58 +12,58 @@ describe("Update 17.1.0", () => {
 
 	it("change projectType to legacy", async done => {
 		appTree.create(
-			"./igniteui-cli.json",
+			"./ignite-ui-cli.json",
 `{
-    "igPackageRegistry": "https://packages.infragistics.com/npm/js-licensed/",
-    "customTemplates": [],
-    "skipGit": false,
-    "skipAnalytic": false,
-    "version": "13.0.1",
-    "project": {
-        "defaultPort": 4200,
-        "framework": "angular",
-        "projectType": "igx-ts",
-        "projectTemplate": "side-nav",
-        "theme": "Default",
-        "themePath": "node_modules/igniteui-angular/styles/igniteui-angular.css",
-        "isBundle": false,
-        "bundleFilePath": "",
-        "igniteuiSource": "",
-        "components": [],
-        "sourceFiles": [],
-        "isShowcase": false,
-        "version": ""
-    },
-    "build": {}
+  "igPackageRegistry": "https://packages.infragistics.com/npm/js-licensed/",
+  "customTemplates": [],
+  "skipGit": false,
+  "skipAnalytic": false,
+  "version": "13.0.1",
+  "project": {
+    "defaultPort": 4200,
+    "framework": "angular",
+    "projectType": "igx-ts",
+    "projectTemplate": "side-nav",
+    "theme": "Default",
+    "themePath": "node_modules/igniteui-angular/styles/igniteui-angular.css",
+    "isBundle": false,
+    "bundleFilePath": "",
+    "igniteuiSource": "",
+    "components": [],
+    "sourceFiles": [],
+    "isShowcase": false,
+    "version": ""
+  },
+  "build": {}
 }
 `
 		);
 
 		const tree = await schematicRunner.runSchematicAsync("migration-01", { applyMigrations: true }, appTree).toPromise();
-		expect(tree.readContent("./igniteui-cli.json"))
+		expect(tree.readContent("./ignite-ui-cli.json"))
 			.toEqual(
 `{
-    "igPackageRegistry": "https://packages.infragistics.com/npm/js-licensed/",
-    "customTemplates": [],
-    "skipGit": false,
-    "skipAnalytic": false,
-    "version": "13.0.1",
-    "project": {
-        "defaultPort": 4200,
-        "framework": "angular",
-        "projectType": "igx-ts-legacy",
-        "projectTemplate": "side-nav",
-        "theme": "Default",
-        "themePath": "node_modules/igniteui-angular/styles/igniteui-angular.css",
-        "isBundle": false,
-        "bundleFilePath": "",
-        "igniteuiSource": "",
-        "components": [],
-        "sourceFiles": [],
-        "isShowcase": false,
-        "version": ""
-    },
-    "build": {}
+  "igPackageRegistry": "https://packages.infragistics.com/npm/js-licensed/",
+  "customTemplates": [],
+  "skipGit": false,
+  "skipAnalytic": false,
+  "version": "13.0.1",
+  "project": {
+    "defaultPort": 4200,
+    "framework": "angular",
+    "projectType": "igx-ts-legacy",
+    "projectTemplate": "side-nav",
+    "theme": "Default",
+    "themePath": "node_modules/igniteui-angular/styles/igniteui-angular.css",
+    "isBundle": false,
+    "bundleFilePath": "",
+    "igniteuiSource": "",
+    "components": [],
+    "sourceFiles": [],
+    "isShowcase": false,
+    "version": ""
+  },
+  "build": {}
 }`
 			);
 		done();
--- packages/ng-schematics/src/migrations/update-1/index.ts
+++ packages/ng-schematics/src/migrations/update-1/index.ts
@@ -5,15 +5,15 @@ export default function(options: Options): Rule {
 	return (host: Tree, context: SchematicContext) => {
 		if (!options.applyMigrations) { return; }
 
-		context.logger.info("Updating @igniteui/angular-schematics to 17.1.0");
+		context.logger.info("We've updated your project type to `igx-ts-legacy` to continue using `NgModule`-s. You can always change back to `igx-ts` should you want to start using Standalone Components.");
 
-		const igConfig = "./igniteui-cli.json";
+		const igConfig = "./ignite-ui-cli.json";
 
 		if (host.exists(igConfig)) {
 			const content = JSON.parse(host.read(igConfig)!.toString());
 			if (content?.project?.projectType === "igx-ts") {
 				content.project.projectType = "igx-ts-legacy";
-				host.overwrite(igConfig, JSON.stringify(content, null, 4));
+				host.overwrite(igConfig, JSON.stringify(content, null, 2));
 			}
 		}
 	};
