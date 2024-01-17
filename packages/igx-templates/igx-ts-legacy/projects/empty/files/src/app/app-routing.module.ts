578874a3087d58af23c9258ba48e1dfb1f0f5a26
Viktor Kombov <vkombov@infragistics.com>
2024-01-17 16:12:02 +0200

    Enable binding information from the Router state to the components' inputs

--- packages/igx-templates/igx-ts-legacy/projects/empty/files/src/app/app-routing.module.ts
+++ packages/igx-templates/igx-ts-legacy/projects/empty/files/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { bindToComponentInputs: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
