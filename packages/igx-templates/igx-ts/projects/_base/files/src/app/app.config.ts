1e1d1c2d6c70a110ace81e74c91bcab2a7021086
jackofdiamond5 <jackofdiamond596@gmail.com>
2024-02-20 16:23:06 +0200

    feat(_base): provide http client

--- packages/igx-templates/igx-ts/projects/_base/files/src/app/app.config.ts
+++ packages/igx-templates/igx-ts/projects/_base/files/src/app/app.config.ts
@@ -1,15 +1,17 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { BrowserModule, HammerModule } from '@angular/platform-browser';
+import { HammerModule } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
-    importProvidersFrom(BrowserModule, HammerModule),
-    provideAnimations()
+    importProvidersFrom(HammerModule),
+    provideAnimations(),
+    provideHttpClient()
     // provide the HAMMER_GESTURE_CONFIG token
     // to override the default settings of the HammerModule
     // { provide: HAMMER_GESTURE_CONFIG, useClass: MyHammerConfig }
