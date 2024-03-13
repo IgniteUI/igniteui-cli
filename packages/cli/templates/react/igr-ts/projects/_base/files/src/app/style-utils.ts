ffffa613db601b5eea09859c85fc2184607e56c2
gedinakova <gedinakova@infragistics.com>
2024-03-13 12:40:59 +0200

    fix(Lint): Fixed styles typing in style-utils

--- packages/cli/templates/react/igr-ts/projects/_base/files/src/app/style-utils.ts
+++ packages/cli/templates/react/igr-ts/projects/_base/files/src/app/style-utils.ts
@@ -2,7 +2,7 @@
  * Creates a class transformer function that maps matches from a module object
  * @param styles Object map of classes and their scoped name, normally from a CSS Module import
  */
-export default function createClassTransformer(styles: any) {
+export default function createClassTransformer(styles: CSSModuleClasses) {
   return (classes: string) => {
     return classes
       .split(' ')
