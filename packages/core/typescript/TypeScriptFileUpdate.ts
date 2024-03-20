dae649ceaef551bef594b56518f4ba93d58e6c35
jackofdiamond5 <jackofdiamond596@gmail.com>
2024-03-20 15:27:20 +0200

    fix(routing): generate default redirect routes for children

--- packages/core/typescript/TypeScriptFileUpdate.ts
+++ packages/core/typescript/TypeScriptFileUpdate.ts
@@ -78,8 +78,10 @@ export class TypeScriptFileUpdate {
 		routesVariable = DEFAULT_ROUTES_VARIABLE,
 		lazyload = false,
 		routesPath = "",
-		root = false) {
-		this.addRouteModuleEntry(filePath, linkPath, linkText, routesVariable, parentRoutePath, lazyload, routesPath, root);
+		root = false,
+		isDefault = false) {
+		this.addRouteModuleEntry(filePath, linkPath, linkText, routesVariable,
+			parentRoutePath, lazyload, routesPath, root, isDefault);
 	}
 
 	/**
@@ -95,8 +97,8 @@ export class TypeScriptFileUpdate {
 		linkPath: string,
 		linkText: string,
 		routesVariable = DEFAULT_ROUTES_VARIABLE,
-		lazyload = false, routesPath = "", root = false) {
-		this.addRouteModuleEntry(filePath, linkPath, linkText, routesVariable, null, lazyload, routesPath, root);
+		lazyload = false, routesPath = "", root = false, isDefault = false) {
+		this.addRouteModuleEntry(filePath, linkPath, linkText, routesVariable, null, lazyload, routesPath, root, isDefault);
 	}
 
 	/**
@@ -276,7 +278,8 @@ export class TypeScriptFileUpdate {
 		parentRoutePath?: string,
 		lazyload = false,
 		routesPath = "",
-		root = false
+		root = false,
+		isDefault = false
 	) {
 		let className: string;
 		const fileSource = TsUtils.getFileSource(filePath);
@@ -343,6 +346,10 @@ export class TypeScriptFileUpdate {
 							}
 							return false;
 						}
+						let defaultRoute: ts.ObjectLiteralExpression;
+						if (isDefault) {
+							defaultRoute = this.createRouteEntry("", null, null, false, routesPath, root, isDefault);
+						}
 						const newObject = this.createRouteEntry(linkPath, className, linkText);
 						const currentNode = node as ts.ObjectLiteralExpression;
 						this.createdStringLiterals.push(linkPath, linkText);
@@ -364,7 +371,12 @@ export class TypeScriptFileUpdate {
 
 						let existingProperties = syntaxList.getChildren()
 							.filter(element => element.kind !== ts.SyntaxKind["CommaToken"]) as ts.ObjectLiteralElementLike[];
-						const newArrayValues = childrenArray.elements.concat(newObject);
+						let newArrayValues: ts.Expression[];
+						if (isDefault) {
+							newArrayValues = childrenArray.elements.concat(defaultRoute, newObject);
+						} else {
+							newArrayValues = childrenArray.elements.concat(newObject);
+						}
 						if (!childrenProperty) {
 							const propertyName = "children";
 							const propertyValue = ts.factory.createArrayLiteralExpression([...newArrayValues]);
@@ -780,8 +792,15 @@ export class TypeScriptFileUpdate {
 		linkPath: string,
 		className: string,
 		linkText: string,
-		lazyload = false, routesPath = "", root = false): ts.ObjectLiteralExpression {
+		lazyload = false, routesPath = "", root = false, isDefault = false): ts.ObjectLiteralExpression {
 		const routePath = ts.factory.createPropertyAssignment("path", ts.factory.createStringLiteral(linkPath));
+		if (isDefault) {
+			const routeRedirectTo = ts.factory.createPropertyAssignment("redirectTo",
+				ts.factory.createStringLiteral(routesPath));
+			const routePathMatch = ts.factory.createPropertyAssignment("pathMatch",
+				ts.factory.createStringLiteral("full"));
+			return ts.factory.createObjectLiteralExpression([routePath, routeRedirectTo, routePathMatch]);
+		}
 		let routeComponent;
 		// TODO: we should consider using the ts.factory instead of string interpolations
 		if (lazyload) {
