e319a9946807a737dca1915dd496e325608fb1e8
Ivaylo Barakov <ibarakov@icloud.com>
2024-04-02 17:47:29 +0300

    chore: remove obsolete classes (#1231)

--- packages/cli/templates/react/igr-ts/projects/_base/files/src/app/app.tsx
+++ packages/cli/templates/react/igr-ts/projects/_base/files/src/app/app.tsx
@@ -3,10 +3,6 @@ import { Outlet } from "react-router-dom";
 export default function App() {
 
   return (
-    <div className="app"> 
-      <div className="content">
-        <Outlet />
-      </div>
-    </div>
+    <Outlet />
   )
 }
