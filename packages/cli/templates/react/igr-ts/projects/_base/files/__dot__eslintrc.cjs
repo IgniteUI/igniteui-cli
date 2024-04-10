39882be155325090c8da3fbdcae9b32801e1e208
onlyexeption <ibarakov@infragistics.com>
2024-04-10 16:25:29 +0300

    chore: use correct quote style

--- packages/cli/templates/react/igr-ts/projects/_base/files/__dot__eslintrc.cjs
+++ packages/cli/templates/react/igr-ts/projects/_base/files/__dot__eslintrc.cjs
@@ -13,13 +13,13 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': [
       'error',
       {
-        "args": "all",
-        "argsIgnorePattern": "^_",
-        "caughtErrors": "all",
-        "caughtErrorsIgnorePattern": "^_",
-        "destructuredArrayIgnorePattern": '^(_|set)',
-        "varsIgnorePattern": "^_",
-        "ignoreRestSiblings": true
+        args: 'all',
+        argsIgnorePattern: '^_',
+        caughtErrors: 'all',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^(_|set)',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true
       }
     ],
     'react-refresh/only-export-components': [
