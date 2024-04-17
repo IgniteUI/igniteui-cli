9b733ffb11985ce59bafe53c90fbdc2b47be9d59
jackofdiamond5 <jackofdiamond596@gmail.com>
2024-04-17 16:38:42 +0300

    feat(igx-templates): add route types for ng

---
+++ packages/igx-templates/types/index.ts
@@ -0,0 +1,4 @@
+export * from './enumerations/AngularDecoratorMetaTarget';
+export * from './enumerations/AngularRouteTarget';
+export * from './IAngularRouteEntry';
+export * from './IAngularRouteLike';
