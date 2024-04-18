225dbbaa8d54fc483d7dbb647e15ff157c6617ba
jackofdiamond5 <jackofdiamond596@gmail.com>
2024-04-18 09:52:56 +0300

    refactor(templates): remove redundant addClassDeclaration method

--- packages/cli/templates/webcomponents/igc-ts/card/default/index.ts
+++ packages/cli/templates/webcomponents/igc-ts/card/default/index.ts
@@ -1,4 +1,3 @@
-import { TypeScriptFileUpdate } from "@igniteui/cli-core";
 import { IgniteUIForWebComponentsTemplate } from "../../../../../lib/templates/IgniteUIForWebComponentsTemplate";
 
 class IgcCardTemplate extends IgniteUIForWebComponentsTemplate {
@@ -11,9 +10,6 @@ class IgcCardTemplate extends IgniteUIForWebComponentsTemplate {
 		this.projectType = "igc-ts";
 		this.name = "Card";
 		this.description = "basic IgcCard";
-	}
-	protected addClassDeclaration(mainModule: TypeScriptFileUpdate, projPath: string, name: string, modulePath: string) {
-		// not applicable with custom module
 	}
 }
 module.exports = new IgcCardTemplate();
