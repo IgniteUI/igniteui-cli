07904985b0c7b47f6e62621868e84999b397e3fa
jackofdiamond5 <jackofdiamond596@gmail.com>
2024-04-18 16:06:06 +0300

    refactor(core/templates); move route entry to enums col

--- packages/core/types/IRouteEntry.ts
+++ packages/core/types/IRouteEntry.ts
@@ -1,6 +1,6 @@
 import * as ts from 'typescript';
 import { IPropertyAssignment } from './IPropertyAssignment';
-import { RouteTarget } from './RouteTarget';
+import { RouteTarget } from './enumerations/RouteTarget';
 
 export interface IRouteEntry extends IPropertyAssignment {
   name: RouteTarget;
