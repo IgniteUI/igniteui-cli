18f03a0dab3bb8de2a77edf7f9a467df466d217c
jackofdiamond5 <jackofdiamond596@gmail.com>
2024-05-09 17:10:24 +0300

    feat(core/igx-templates): update types
    (cherry picked from commit 99656c9db9746d2bc883b1b4c0b9e7f1d50f9297)

--- packages/core/types/RouteLike.ts
+++ packages/core/types/RouteLike.ts
@@ -8,6 +8,8 @@ export interface RouteLike {
   modulePath?: string;
   /** If the identifier should be lazy loaded. */
   lazyload?: boolean;
+  /** A URL to redirect to when the path matches. */
+  redirectTo?: string;
   /** The children of the route. */
   children?: RouteLike[];
 }
