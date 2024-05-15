cf94f920c0a368873ee4f3c41b674f42d413f8fd
jackofdiamond5 <jackofdiamond596@gmail.com>
2024-05-15 15:41:40 +0300

    refactor(core/igx): extend base ts file update & add WC File Update
    (cherry picked from commit 687ebefcfe726bdb4a02a96791470f25a5fb536d)

--- packages/core/typescript/VisitorConditions.ts
+++ packages/core/typescript/VisitorConditions.ts
@@ -1,7 +1,6 @@
 import * as ts from 'typescript';
 import { TypeScriptASTTransformer } from './TypeScriptASTTransformer';
 import { ROUTES_VARIABLE_NAME } from '../util';
-import { RouteTarget } from '../types';
 
 /**
  * Starting from the current node, look up the AST to find the variable declaration of the routes variable.
@@ -22,15 +21,17 @@ export const RoutesVariableAsParentCondition = (
 };
 
 /**
- * Checks if the node is a property assignment with a specific string literal value.
+ * Checks if the node is a property assignment with a specific name and string literal value.
+ * @param name The name of the property assignment.
  * @param literal The string literal value to check for.
  */
-export const FindPropertyAssignmentWithStringLiteralValue = (
+export const PropertyAssignmentWithStringLiteralValueCondition = (
+  name: string,
   literal: string
 ) => {
   return (node: ts.Node | ts.ObjectLiteralElementLike) =>
     ts.isPropertyAssignment(node) &&
-    node.name.getText() === RouteTarget.Path &&
+    node.name.getText() === name &&
     ts.isStringLiteral(node.initializer) &&
     node.initializer.text === literal;
 };
--- packages/core/util/GlobalConstants.ts
+++ packages/core/util/GlobalConstants.ts
@@ -1,5 +1,18 @@
+import * as ts from 'typescript';
+import { PropertyAssignment } from '../types';
+
+// TypeScript
 export const ROUTES_VARIABLE_NAME = 'routes';
 export const THEN_IDENTIFIER_NAME = 'then';
 export const IMPORT_IDENTIFIER_NAME = 'import';
 export const SIDE_EFFECTS_IMPORT_TEMPLATE_NAME = 'side_effects_import';
 export const UNDERSCORE_TOKEN = "_";
+
+// Angular
+export const NG_MODULE_DECORATOR_NAME = 'NgModule';
+export const NG_SA_DECORATOR_NAME = 'Component';
+export const NG_FOR_ROOT_IDENTIFIER_NAME = 'forRoot';
+export const ANCHOR_ELEMENT: PropertyAssignment = {
+  name: 'path',
+  value: ts.factory.createStringLiteral('**'),
+};
