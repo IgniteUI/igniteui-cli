17b19508195c14b4fed02019c22aa4b8a74ca5c5
jackofdiamond5 <jackofdiamond596@gmail.com>
2024-06-12 15:45:06 +0300

    feat(ng): update types

--- packages/igx-templates/types/AngularRouteEntry.ts
+++ packages/igx-templates/types/AngularRouteEntry.ts
@@ -1,5 +1,5 @@
 import { RouteEntry, RouteTarget } from "@igniteui/cli-core";
-import { AngularRouteTarget } from "./enumerations/AngularRouteTarget";
+import { AngularRouteTarget } from "./index";
 
 /** Represents an Angular route entry in a routes collection. */
 export interface AngularRouteEntry extends Pick<RouteEntry, "value"> {
