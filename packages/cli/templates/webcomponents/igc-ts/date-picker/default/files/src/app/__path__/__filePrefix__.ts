d8f6dfcfce807ef64d952f886dcbb7d9983bc1e1
valadzhov <valadzhov@infragistics.com>
2024-07-04 15:41:18 +0300

    chore(WebC): Add date picker template.

--- packages/cli/templates/webcomponents/igc-ts/date-picker/default/files/src/app/__path__/__filePrefix__.ts
+++ packages/cli/templates/webcomponents/igc-ts/date-picker/default/files/src/app/__path__/__filePrefix__.ts
@@ -2,22 +2,17 @@ import { html, LitElement } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import {
   defineComponents,
-  IgcAvatarComponent,
+  IgcDatePickerComponent,
 } from 'igniteui-webcomponents';
 
-defineComponents(IgcAvatarComponent);
+defineComponents(IgcDatePickerComponent);
 
 @customElement('app-$(path)')
 export default class $(ClassName) extends LitElement {
   render() {
     return html`
-      <igc-avatar
-        size="large"
-        shape="circle"
-        src="./src/assets/avatar1.jpg"
-        alt="My avatar"
-      >
-      </igc-avatar>
+      <igc-date-picker>
+      </igc-date-picker>
     `;
   }
 }
