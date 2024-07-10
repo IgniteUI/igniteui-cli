f6fb01a4cac3fae2c7e539b477f1203415112f17
jackofdiamond5 <jackofdiamond596@gmail.com>
2024-07-10 15:43:41 +0300

    refactor(wc): extend base file update; update wc template

--- packages/cli/templates/webcomponents/WebComponentsTypeScriptFileUpdate.ts
+++ packages/cli/templates/webcomponents/WebComponentsTypeScriptFileUpdate.ts
@@ -18,34 +18,26 @@ export class WebComponentsTypeScriptFileUpdate extends TypeScriptFileUpdate {
 
 	//#region Overridden Public API
 
-	public addRoute(
+	public override addRoute(
 		route: RouteLike,
 		multiline: boolean = false,
 		prepend: boolean = true,
 		anchorElement: PropertyAssignment = WC_ANCHOR_ELEMENT
-	): ts.SourceFile {
-		return this.addRedirectOrSimpleRouteEntry(
-			route,
-			RoutesVariableAsParentCondition(this.astTransformer),
-			multiline,
-			prepend,
-			anchorElement
-		);
-	}
+	): void {
+		const routeName = Util.lowerDashed(route.name || route.path);
+		this.requestSideEffectsImportForModule(`./${routeName}/${routeName}`);
+		if (!route.lazyload) {
+			this.requestImportForRouteIdentifier(route);
+		}
 
-	public addChildRoute(
-		parentPath: string,
-		route: RouteLike,
-		asIdentifier: boolean = true,
-		multiline?: boolean,
-		prepend?: boolean,
-		anchorElement?: PropertyAssignment
-	): ts.SourceFile {
-		return super.addChildRoute(
-			parentPath,
-			route,
-			asIdentifier,
-			multiline,
+		const structure = this.buildRouteStructure(route, multiline);
+		const newRoute = this.astTransformer.createObjectLiteralExpression(
+			structure,
+			multiline
+		);
+		this.astTransformer.requestNewMembersInArrayLiteral(
+			RoutesVariableAsParentCondition(this.astTransformer),
+			[newRoute],
 			prepend,
 			anchorElement
 		);
@@ -55,114 +47,64 @@ export class WebComponentsTypeScriptFileUpdate extends TypeScriptFileUpdate {
 
 	//#region Overrides
 
-	protected addRouteEntry(
+	protected override buildRouteStructure(
 		route: RouteLike,
-		visitCondition: (node: ts.Node) => boolean,
-		multiline: boolean = false,
-		prepend: boolean = false,
-		anchorElement: PropertyAssignment
-	): ts.SourceFile {
-		const routeName = Util.lowerDashed(route.name || route.path);
-		this.requestSideEffectsImportForModule(`./${routeName}/${routeName}`);
-		if (
-			route.children &&
-			!Array.isArray(route.children) &&
-			(route.children.identifierName || route.children.aliasName)
-		) {
-			this.requestImportForRouteIdentifier(route.children);
-		}
-
-		const structure: RouteEntry[] = [
+		_multiline: boolean
+	): RouteEntry[] {
+		// default route
+		let structure: RouteEntry[] = [
 			{
 				name: RouteTarget.Path,
-				value: ts.factory.createStringLiteral(route.path),
+				value: ts.factory.createStringLiteral(
+					route.path,
+					this.formatSettings.singleQuotes
+				),
 			},
 			{
 				name: RouteTarget.Component,
 				value: ts.factory.createStringLiteral(
-					route.aliasName || route.identifierName
+					route.aliasName || route.identifierName,
+					this.formatSettings.singleQuotes
 				),
 			},
 			{
 				name: RouteTarget.Name,
-				value: ts.factory.createStringLiteral(route.name),
-			},
-		];
-		const newRoute = this.astTransformer.createObjectLiteralExpression(
-			structure,
-			multiline
-		);
-		return this.astTransformer.addMembersToArrayLiteral(
-			visitCondition,
-			[newRoute],
-			prepend,
-			anchorElement
-		);
-	}
-
-	protected addRedirectRouteEntry(
-		route: RouteLike,
-		visitCondition: (node: ts.Node) => boolean,
-		multiline: boolean = false,
-		prepend: boolean = false,
-		anchorElement: PropertyAssignment
-	): ts.SourceFile {
-		const routeName = Util.lowerDashed(route.name || route.path);
-		this.requestSideEffectsImportForModule(`./${routeName}/${routeName}`);
-		const childRoute = route.children as RouteLike;
-		if (childRoute) {
-			this.requestImportForRouteIdentifier(childRoute);
-		}
-
-		const structure: RouteEntry[] = [
-			{
-				name: RouteTarget.Path,
-				value: ts.factory.createStringLiteral(route.path || ''),
-			},
-			{
-				// vaadin/router uses the component's tag name when navigating
-				name: RouteTarget.Component,
-				value: ts.factory.createStringLiteral(route.redirectTo),
-			},
-			{
-				name: RouteTarget.Name,
-				value: ts.factory.createStringLiteral(route.name),
-			},
-		];
-		if (childRoute) {
-			structure.push({
-				// the identifier name in this case will be the alias of the routes variable
-				name: RouteTarget.Children,
-				value: ts.factory.createIdentifier(
-					childRoute.aliasName || childRoute.identifierName
+				value: ts.factory.createStringLiteral(
+					route.name,
+					this.formatSettings.singleQuotes
 				),
-			});
+			},
+		];
+
+		// redirecting route
+		if (route.redirectTo) {
+			structure = [
+				{
+					name: RouteTarget.Path,
+					value: ts.factory.createStringLiteral(
+						route.path || '',
+						this.formatSettings.singleQuotes
+					),
+				},
+				{
+					// vaadin/router uses the component's tag name when navigating
+					name: RouteTarget.Component,
+					value: ts.factory.createStringLiteral(
+						route.redirectTo,
+						this.formatSettings.singleQuotes
+					),
+				},
+				{
+					name: RouteTarget.Name,
+					value: ts.factory.createStringLiteral(
+						route.name,
+						this.formatSettings.singleQuotes
+					),
+				},
+			];
 		}
 
-		const newRoute = this.astTransformer.createObjectLiteralExpression(
-			structure,
-			multiline
-		);
-		return this.astTransformer.addMembersToArrayLiteral(
-			visitCondition,
-			[newRoute],
-			prepend,
-			anchorElement
-		);
-	}
-
-	protected addChildRouteEntry(
-		route: RouteLike,
-		asIdentifier: boolean = true,
-		multiline: boolean = false,
-		prepend: boolean = false
-	): ts.SourceFile {
-		return super.addChildRouteEntry(
-			route,
-			asIdentifier || true, // for WC the `children` member should always be added in the form { children: identifier }
-			multiline,
-			prepend
-		);
+		return structure;
 	}
 
 	//#endregion
