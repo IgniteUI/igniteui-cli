40bbccf3594bd97c11a93eadc4ea59c664cf4964
jackofdiamond5 <jackofdiamond596@gmail.com>
2024-09-04 11:43:25 +0300

    refactor(execute): provide args to cli.run

--- packages/cli/bin/execute.js
+++ packages/cli/bin/execute.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
-var fs = require("fs");
-var inquirer = require('inquirer');
-var projectConfiguration = {};
-var resolve = require('resolve');
-var path = require('path');
+const fs = require("fs");
+const resolve = require("resolve");
+const path = require("path");
+const { hideBin } = require("yargs/helpers");
+
+const args = hideBin(process.argv);
 
 //execute -g igniteui-cli in case there is no project installed, else resolve to the local one
-resolve('igniteui-cli', { basedir: process.cwd() }, function (err, res) {
-	var cli;
+resolve("igniteui-cli", { basedir: process.cwd() }, function (err, res) {
+	let cli;
 	if (res && res.indexOf(process.cwd()) > -1) {
-		var localVersion = require(path.join(process.cwd(), 'node_modules/igniteui-cli/package.json'))['version'];
-		var globalVersion = require(path.join(__dirname, '../package.json'))['version'];
+		const localVersion = require(path.join(process.cwd(), "node_modules/igniteui-cli/package.json"))["version"];
+		const globalVersion = require(path.join(__dirname, "../package.json"))["version"];
 		if (globalVersion !== localVersion) {
 			console.log("Different igniteui-cli global and local version");
 		}
@@ -18,5 +19,5 @@ resolve('igniteui-cli', { basedir: process.cwd() }, function (err, res) {
 	} else {
 		cli = require("../lib/cli");
 	}
-	cli.run();
+	cli.run(args);
 });
