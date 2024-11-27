5a9806ec8668772369c787e65e89d025a670b303
Hristo Hristov <HHristov@infragistics.com>
2024-11-27 15:36:23 +0200

    fix: remove yaml generate deployment script flag and if condition for disable the workflow

--- packages/igx-templates/igx-ts-legacy/projects/_base/index.ts
+++ packages/igx-templates/igx-ts-legacy/projects/_base/index.ts
@@ -66,8 +66,7 @@ $app-palette: palette($primary, $secondary, $surface);
 			path: name,
 			projectTemplate: this.id,
 			yamlDefaultBranch: "<%=yaml-default-branch%>", // the placeholder will be evaluated by CodeGen
-			ApplicationTitle: "<%=ApplicationTitle%>", // the placeholder will be evaluated by CodeGen
-			yamlGenerateDeploymentScript: "<%=yaml-generate-deployment-script%>" // the placeholder will be evaluated by CodeGen
+			ApplicationTitle: "<%=ApplicationTitle%>" // the placeholder will be evaluated by CodeGen
 		};
 
 		switch (theme) {
