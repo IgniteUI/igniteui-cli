9e5f34abf759f401ee657ae5d3772f50f920cbb9
lipata <nikolay.alipiev@gmail.com>
2025-01-15 09:48:12 +0200

    chore: update dv packages with v19

--- packages/igx-templates/igx-ts/custom-templates/fintech-grid/index.ts
+++ packages/igx-templates/igx-ts/custom-templates/fintech-grid/index.ts
@@ -9,7 +9,7 @@ class IgxFinTechGridTemplate extends IgniteUIForAngularTemplate {
 		this.listInCustomTemplates = true;
 		this.name = "FinTech Grid";
 		this.description = "Grid with simulated high-frequency data feed";
-		this.packages = ["igniteui-angular-core@~18.0.0", "igniteui-angular-charts@~18.0.0"];
+		this.packages = ["igniteui-angular-core@~19.0.0", "igniteui-angular-charts@~19.0.0"];
 	}
 }
 module.exports = new IgxFinTechGridTemplate();
