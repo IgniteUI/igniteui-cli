024ee5f048427c5406013f1e4d051403ae255fca
Hristo Hristov <57346540+Hristo313@users.noreply.github.com>
2025-01-23 10:12:51 +0200

    feat: replace rollup with vite configuration for wc (#1364)

---
+++ packages/cli/templates/webcomponents/igc-ts/projects/_base/files/vite.config.ts
@@ -0,0 +1,48 @@
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[hash].js',
+        chunkFileNames: '[hash].js',
+        assetFileNames: '[hash][extname]',
+      },
+      onwarn: (warning, warn) => {
+        if (warning.code === 'THIS_IS_UNDEFINED') return;
+        warn(warning);
+      },
+    },
+    target: 'es2021',
+    minify: 'terser',
+    emptyOutDir: false,
+  },
+  plugins: [
+    /** Copy static assets */
+    viteStaticCopy({
+      targets: [
+        { src: 'src/assets', dest: 'src' },
+      ],
+    }),
+    /** PWA Plugin for service worker generation */
+    VitePWA({
+      registerType: 'autoUpdate',
+      strategies: 'generateSW',
+      workbox: {
+        globDirectory: 'dist',
+        globPatterns: ['**/*.{html,js,css,webmanifest}'],
+        globIgnores: ['polyfills/*.js', 'nomodule-*.js'],
+        navigateFallback: '/index.html',
+        runtimeCaching: [
+          {
+            urlPattern: /^polyfills\/.*\.js$/,
+            handler: 'CacheFirst',
+          },
+        ],
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024
+      },
+    }),
+  ],
+});
