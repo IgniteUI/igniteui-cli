546d3e7019b38805bee9d7230fdc5333451d0c6e
lipata <nikolay.alipiev@gmail.com>
2025-09-12 16:35:40 +0300

    chore: wizard updates on react and ng

--- packages/cli/templates/react/index.ts
+++ packages/cli/templates/react/index.ts
@@ -9,8 +9,8 @@ class ReactFramework implements Framework {
 		this.id = "react";
 		this.name = "React";
 		this.projectLibraries = [];
-		this.projectLibraries.push(require("./es6") as ProjectLibrary);
-		this.projectLibraries.push(require("./igr-es6") as ProjectLibrary);
+		// this.projectLibraries.push(require("./es6") as ProjectLibrary);
+		// this.projectLibraries.push(require("./igr-es6") as ProjectLibrary);
 		this.projectLibraries.push(require("./igr-ts") as ProjectLibrary);
 	}
 }
