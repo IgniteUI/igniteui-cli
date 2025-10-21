9fe0916a7d2503556236fe0457afcb6163a91f75
lipata <nikolay.alipiev@gmail.com>
2025-10-21 16:53:05 +0300

    feat: use ng lint schematic to configure project

---
+++ packages/igx-templates/igx-ts/projects/_base/files/eslint.config.js
@@ -0,0 +1,47 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+
+module.exports = tseslint.config(
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+      // Relax rules
+      "@typescript-eslint/no-explicit-any": "off",
+      "@angular-eslint/prefer-inject": "warn",
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      // ...angular.configs.templateAccessibility,
+    ],
+    rules: {
+    },
+  }
+);
