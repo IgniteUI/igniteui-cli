a7e2be62985ce95c1301ec6ee31f6775b996fe70
Hristo Hristov <HHristov@infragistics.com>
2025-11-07 16:18:57 +0200

    fix: revert last change

--- packages/cli/lib/templates/ReactTemplate.ts
+++ packages/cli/lib/templates/ReactTemplate.ts
@@ -125,7 +125,7 @@ export class ReactTemplate implements Template {
 					igniteuiResFile = igniteuiResFile.replace("-lite", "");
 				}
 				fs.ftruncateSync(fd, 0);
-				fs.writeSync(fd, igniteuiResFile);
+				fs.writeSync(fd, igniteuiResFile, 0);
 			}
 
 			if (dvDep && !igniteuiResFile.includes(dvPath)) {
