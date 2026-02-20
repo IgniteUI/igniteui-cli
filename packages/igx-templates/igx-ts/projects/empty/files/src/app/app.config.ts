75eef022a33f0dc6bc5b90816ccee9c415d76289
Hristo Hristov <HHristov@infragistics.com>
2026-02-20 16:56:19 +0200

    fix(angular): empty template config

--- packages/igx-templates/igx-ts/projects/empty/files/src/app/app.config.ts
+++ packages/igx-templates/igx-ts/projects/empty/files/src/app/app.config.ts
@@ -1,11 +1,18 @@
-import { ApplicationConfig, EnvironmentProviders, Provider, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, EnvironmentProviders, Provider, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 
+// provide the HAMMER_GESTURE_CONFIG token
+// to override the default settings of the HammerModule
+// { provide: HAMMER_GESTURE_CONFIG, useClass: MyHammerConfig }
 const providers: (EnvironmentProviders | Provider)[] = [
   provideZoneChangeDetection({ eventCoalescing: true }),
-  provideRouter(routes)
+  provideRouter(routes),
+  importProvidersFrom(BrowserModule, HammerModule),
+  provideAnimations()
 ];
 
 export const appConfig: ApplicationConfig = { providers };
