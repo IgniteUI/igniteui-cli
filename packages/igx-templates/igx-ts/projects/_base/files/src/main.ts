9722a1cbf8d6b06050ce24e33731e2402274c0ac
Hristo Hristov <HHristov@infragistics.com>
2026-02-28 14:53:34 +0200

    fix(igx-ts): update the templates

--- packages/igx-templates/igx-ts/projects/_base/files/src/main.ts
+++ packages/igx-templates/igx-ts/projects/_base/files/src/main.ts
@@ -1,5 +1,3 @@
-/// <reference types="@angular/localize" />
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
