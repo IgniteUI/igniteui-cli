bf6c26f4db4094ced1730ad8a3d5d69398fc3529
Hristo Hristov <HHristov@infragistics.com>
2026-03-02 14:27:22 +0200

    fix(igx-ts-legacy): update error routing module

--- packages/igx-templates/igx-ts-legacy/projects/_base/files/src/app/app-module.ts
+++ packages/igx-templates/igx-ts-legacy/projects/_base/files/src/app/app-module.ts
@@ -3,14 +3,10 @@ import { BrowserModule, HammerModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { App } from './app';
-import { NotFound } from './error-routing/not-found/not-found';
-import { UncaughtError } from './error-routing/error/uncaught-error';
 
 @NgModule({
   declarations: [
-    App,
-    NotFound,
-    UncaughtError
+    App
   ],
   imports: [
     BrowserModule,
--- packages/igx-templates/igx-ts-legacy/projects/_base/files/src/app/error-routing/error-routing.module.ts
+++ packages/igx-templates/igx-ts-legacy/projects/_base/files/src/app/error-routing/error-routing.module.ts
@@ -1,8 +1,8 @@
 import { ErrorHandler, NgModule, Provider } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { GlobalErrorHandlerService } from './error/global-error-handler.service';
-import { UncaughtErrorComponent } from './error/uncaught-error.component';
-import { PageNotFoundComponent } from './not-found/not-found.component';
+import { UncaughtError } from './error/uncaught-error';
+import { NotFound } from './not-found/not-found';
 
 const providers: Provider[] = [];
 
@@ -13,8 +13,8 @@ if (environment.production) {
 
 @NgModule({
   declarations: [
-    PageNotFoundComponent,
-    UncaughtErrorComponent
+    NotFound,
+    UncaughtError
   ],
   providers
 })
