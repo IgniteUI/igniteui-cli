a4fd5620c5e6231abf66a8e1ef25031e8aeb40fd
Hristo Hristov <HHristov@infragistics.com>
2026-03-02 14:31:53 +0200

    fix(igx-ts): update error components

--- packages/igx-templates/igx-ts/projects/_base/files/src/main.ts
+++ packages/igx-templates/igx-ts/projects/_base/files/src/main.ts
@@ -1,6 +1,12 @@
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { appConfig } from './app/app.config';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
