85dd3e609295d7d801c127715c5fe8947f90f23d
Hristo Hristov <57346540+Hristo313@users.noreply.github.com>
2026-03-18 18:10:01 +0200

    feat(igx-ts): update angular project to use vitest with playwright (#1542)

--- packages/igx-templates/igx-ts-legacy/radial-gauge/default/files/src/app/__path__/__filePrefix__.spec.ts
+++ packages/igx-templates/igx-ts-legacy/radial-gauge/default/files/src/app/__path__/__filePrefix__.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { IgxRadialGaugeModule } from 'igniteui-angular-gauges';
 import { <%=ClassName%> } from './<%=filePrefix%>';
 
@@ -10,7 +9,7 @@ describe('<%=ClassName%>', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [<%=ClassName%>],
-      imports: [IgxRadialGaugeModule, NoopAnimationsModule]
+      imports: [IgxRadialGaugeModule]
     })
       .compileComponents();
   });
