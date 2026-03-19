dcec8e546cd389cd0197f38c64aa9516c13c3e00
Hristo Hristov <HHristov@infragistics.com>
2026-03-19 14:16:44 +0200

    fix(igx-ts): update the providers to fix build errors

--- packages/igx-templates/igx-ts/projects/side-nav-auth/files/src/app/authentication/providers/microsoft-provider.ts
+++ packages/igx-templates/igx-ts/projects/side-nav-auth/files/src/app/authentication/providers/microsoft-provider.ts
@@ -1,3 +1,4 @@
+import { firstValueFrom } from 'rxjs';
 import { ExternalLogin } from '../models/login';
 import { BaseOidcProvider } from './base-oidc-provider';
 
@@ -8,13 +9,13 @@ export class MicrosoftProvider extends BaseOidcProvider {
    * Format user data response from available claims:
    * https://docs.microsoft.com/en-us/azure/active-directory/develop/id-tokens#payload-claims
    */
-  protected formatUserData(userData: { [key: string]: any; }): ExternalLogin {
-    const login: ExternalLogin = {
+  protected async formatUserData(userData: { [key: string]: any; }): Promise<ExternalLogin> {
+    const token = await firstValueFrom(this.oidcSecurityService.getAccessToken(this.externalStsConfig.configId));
+    return {
       id: userData['oid'],
       name: userData['name'],
       email: userData['email'],
-      externalToken: this.oidcSecurityService.getAccessToken(this.externalStsConfig.configId)
+      externalToken: token
     };
-    return login;
   }
 }
