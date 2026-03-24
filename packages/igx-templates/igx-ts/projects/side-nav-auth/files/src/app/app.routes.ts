4f06d3f629968b167d92f84a6605a63f9322395e
Hristo Hristov <HHristov@infragistics.com>
2026-03-24 13:18:11 +0200

    fix(igx-ts): add home route

--- packages/igx-templates/igx-ts/projects/side-nav-auth/files/src/app/app.routes.ts
+++ packages/igx-templates/igx-ts/projects/side-nav-auth/files/src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { AuthGuard } from './authentication/auth.guard';
+import { Home } from './home/home';
 import { Profile } from './authentication/profile/profile';
 import { Redirect } from './authentication/redirect/redirect';
 import { ExternalAuthProvider } from './authentication/services/external-auth-configs';
 import { ExternalAuthRedirectUrl } from './authentication/services/external-auth';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: Home, data: { text: 'Home' } },
   { path: 'profile', component: Profile, canActivate: [AuthGuard] },
   { path: ExternalAuthRedirectUrl.Google, component: Redirect, data: { provider: ExternalAuthProvider.Google } },
   { path: ExternalAuthRedirectUrl.Facebook, component: Redirect, data: { provider: ExternalAuthProvider.Facebook } },
