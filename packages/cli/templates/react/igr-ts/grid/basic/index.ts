5401a1fee36cfb9dc5b8ea24d36e83a9689d99d2
damyanpetev <damyanpetev@users.noreply.github.com>
2026-03-27 14:53:00 +0200

    chore: comma

--- packages/cli/templates/react/igr-ts/grid/basic/index.ts
+++ packages/cli/templates/react/igr-ts/grid/basic/index.ts
@@ -14,7 +14,7 @@ class GridTemplate extends IgniteUIForReactTemplate {
 		this.projectType = "igr-ts";
 		this.components = ["Grid"];
 		this.controlGroup = "Data Grids";
-		this.packages = ["igniteui-react-grids@~19.6.0",];
+		this.packages = ["igniteui-react-grids@~19.6.0"];
 
 		this.hasExtraConfiguration = false;
 	}
